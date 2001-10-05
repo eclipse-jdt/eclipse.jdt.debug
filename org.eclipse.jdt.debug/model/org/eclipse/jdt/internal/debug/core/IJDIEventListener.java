a41cc3baa0d6dc918f9b6c263eee3e9d92c72529
Darin Wright <darin>
2001-10-05 14:15:38 +0000

    code review

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/IJDIEventListener.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/IJDIEventListener.java
@@ -2,14 +2,27 @@ package org.eclipse.jdt.internal.debug.core;
 
 import com.sun.jdi.event.Event;
 
+/**
+ * A jdi event listener is notified of events associated with
+ * a specific jdi event request. A listener registers/deregisters
+ * event requests with a debug target.
+ * 
+ * @see JDIDebugTarget#addJDIEventListener(IJDIEventListener, EventRequest)
+ * @see JDIDebugTarget#removeJDIEventListener(IJDIEventListener, EventRequest)
+ */
 
 public interface IJDIEventListener {
 	/**
-	 * Handles the given event that this listener has registerd for and
+	 * Handles the given event that this listener has registered for and
 	 * returns whether the thread in which the event occurred should
 	 * be resumed. All event handlers for the events in an event set
 	 * are given a chance to vote on whether the thread should be
 	 * resumed. If all agree, the thread is resumed by the event dispatcher.
+	 * If any event handler returns <code>false</code> the thread in which
+	 * the event originated is left in a suspended state.
+	 * 
+	 * @param event the event to handle
+	 * @param target the debug target in which the event occurred
 	 */
 	public boolean handleEvent(Event event, JDIDebugTarget target);
 }
