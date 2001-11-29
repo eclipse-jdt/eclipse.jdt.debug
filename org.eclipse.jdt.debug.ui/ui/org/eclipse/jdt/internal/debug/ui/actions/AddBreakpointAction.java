5e25c41854dd9283927eb91a10786d6d0cff6ca6
Darin Swanson <darins>
2001-11-29 23:56:52 +0000

    Bug 1681

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/AddBreakpointAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/AddBreakpointAction.java
@@ -5,39 +5,38 @@ package org.eclipse.jdt.internal.debug.ui.actions;
  * All Rights Reserved.
  */
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.jdt.core.IClassFile;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.debug.core.JDIDebugModel;
-import org.eclipse.jdt.internal.ui.javaeditor.BreakpointLocationVerifier;
-import org.eclipse.jdt.internal.ui.javaeditor.IClassFileEditorInput;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorActionDelegate;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IFileEditorInput;
-import org.eclipse.ui.texteditor.AddMarkerAction;
 import org.eclipse.ui.texteditor.ITextEditor;
+import org.eclipse.ui.texteditor.TextEditorAction;
 
 /**
  * Action for setting breakpoints for a given text selection.
  */
-public class AddBreakpointAction extends AddMarkerAction implements IEditorActionDelegate{
+public class AddBreakpointAction extends TextEditorAction implements IEditorActionDelegate {
+	
+	private int fLineNumber;
+	private IType fType= null;
 	
 	public AddBreakpointAction() {
-		super(ActionMessages.getResourceBundle(), "AddBreakpoint.", null, IBreakpoint.BREAKPOINT_MARKER, false); //$NON-NLS-1$
+		super(ActionMessages.getResourceBundle(), "AddBreakpoint.", null); //$NON-NLS-1$
 	}
 	
 	/**
@@ -51,56 +50,66 @@ public class AddBreakpointAction extends AddMarkerAction implements IEditorActio
 	 * Creates a breakpoint marker.
 	 */
 	protected IBreakpoint createBreakpoint(IEditorInput editorInput) {
+		if (breakpointCanBeCreated(editorInput)) {
+			try {
+				return JDIDebugModel.createLineBreakpoint(getType(), getLineNumber(), -1, -1, 0);
+			} catch (CoreException ce) {
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Creates a breakpoint marker.
+	 */
+	protected boolean breakpointCanBeCreated(IEditorInput editorInput) {
+		IType type= getType(editorInput);
+		setType(type);
+		if (type != null) {
+			try {
+				return !JDIDebugModel.lineBreakpointExists(type, getLineNumber());
+			} catch (CoreException ce) {
+				JDIDebugUIPlugin.logError(ce);
+			}
+		}
+		return false;
+	}
+	
+	protected IType getType(IEditorInput editorInput) {
+		IType type = null;
 		ISelection s= getTextEditor().getSelectionProvider().getSelection();
-		if (!s.isEmpty()) {
+		if (!s.isEmpty() && s instanceof ITextSelection) {
 			ITextSelection selection= (ITextSelection) s;
 			try {
 				IDocument document= getTextEditor().getDocumentProvider().getDocument(editorInput);
 				BreakpointLocationVerifier bv = new BreakpointLocationVerifier();
 				int lineNumber = bv.getValidBreakpointLocation(document, selection.getStartLine());
 				if (lineNumber > 0) {
+					setLineNumber(lineNumber);
+					IClassFile classFile= (IClassFile)editorInput.getAdapter(IClassFile.class);
+					if (classFile != null) {
+						type = classFile.getType();
 					
-					IRegion line= document.getLineInformation(lineNumber - 1);
-					
-					IType type = null;
-					if (editorInput instanceof IClassFileEditorInput) {
-						
-						IClassFileEditorInput input= (IClassFileEditorInput) editorInput;
-						type = input.getClassFile().getType();
-					
-					} else if (editorInput instanceof IFileEditorInput) {
-						
-						IFileEditorInput input= (IFileEditorInput) editorInput;
-						IJavaElement element= JavaCore.create(input.getFile());
-						if (element instanceof ICompilationUnit) {
-							ICompilationUnit cu = (ICompilationUnit) element;
-							IJavaElement e = cu.getElementAt(line.getOffset());
-							if (e instanceof IType)
-								type = (IType)e;
-							else if (e != null && e instanceof IMember)
-								type = ((IMember) e).getDeclaringType();
+					} else {
+						IFile file= (IFile)editorInput.getAdapter(IFile.class);
+						if (file != null) {
+							IJavaElement element= JavaCore.create(file);
+							if (element instanceof ICompilationUnit) {
+								ICompilationUnit cu = (ICompilationUnit) element;
+								IJavaElement e = cu.getElementAt(selection.getOffset());
+								if (e instanceof IType)
+									type = (IType)e;
+								else if (e != null && e instanceof IMember) {
+									type = ((IMember) e).getDeclaringType();
+								}
+							}
 						}
 					}
-					if (type != null) {
-						if (!JDIDebugModel.lineBreakpointExists(type, lineNumber)) {
-							return JDIDebugModel.createLineBreakpoint(type, lineNumber, line.getOffset(), line.getOffset() + line.getLength(), 0);
-						}
-					}
-					
 				}
-			} catch (DebugException e) {
-				Shell shell= getTextEditor().getSite().getShell();
-				ErrorDialog.openError(shell, ActionMessages.getString("AddBreakpoint.error.title1"), ActionMessages.getString("AddBreakpoint.error.message1"), e.getStatus()); //$NON-NLS-2$ //$NON-NLS-1$
-			} catch (CoreException e) {
-				Shell shell= getTextEditor().getSite().getShell();
-				ErrorDialog.openError(shell, ActionMessages.getString("AddBreakpoint.error.title1"), ActionMessages.getString("AddBreakpoint.error.message1"), e.getStatus()); //$NON-NLS-2$ //$NON-NLS-1$
-			} catch (BadLocationException e) {
-				Shell shell= getTextEditor().getSite().getShell();
-				ErrorDialog.openError(shell, ActionMessages.getString("AddBreakpoint.error.title1"), ActionMessages.getString("AddBreakpoint.error.message1"), null); //$NON-NLS-2$ //$NON-NLS-1$
+			} catch (JavaModelException jme) {
 			}
-
 		}
-		return null;
+		return type;
 	}
 	/**
 	 * @see IEditorActionDelegate#setActiveEditor(IAction, IEditorPart)
@@ -122,5 +131,24 @@ public class AddBreakpointAction extends AddMarkerAction implements IEditorActio
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
+	}
+		
+	public void update() {
+		setEnabled(getTextEditor()!= null && breakpointCanBeCreated(getTextEditor().getEditorInput()));
+	}
+	protected int getLineNumber() {
+		return fLineNumber;
+	}
+
+	protected void setLineNumber(int lineNumber) {
+		fLineNumber = lineNumber;
+	}
+
+	protected IType getType() {
+		return fType;
+	}
+
+	protected void setType(IType type) {
+		fType = type;
 	}
 }
---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/RunToLineAction.java
@@ -0,0 +1,186 @@
+package org.eclipse.jdt.internal.debug.ui.actions;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.core.model.IDebugElement;
+import org.eclipse.debug.core.model.IDebugTarget;
+import org.eclipse.debug.core.model.IThread;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.debug.core.JDIDebugModel;
+import org.eclipse.jdt.internal.debug.core.JDIDebugTarget;
+import org.eclipse.jdt.internal.debug.ui.IHelpContextIds;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Action to support run to line (i.e. where the cursor is in the active editor)
+ */
+public class RunToLineAction extends AddBreakpointAction {	
+	
+	public RunToLineAction() {
+		setText(ActionMessages.getString("RunToLine.label")); //$NON-NLS-1$
+		setToolTipText(ActionMessages.getString("RunToLine.tooltip")); //$NON-NLS-1$
+		setDescription(ActionMessages.getString("RunToLine.description")); //$NON-NLS-1$
+		
+		update();
+		setHelpContextId(IHelpContextIds.RUN_TO_LINE_ACTION );					
+	}
+
+	public void run() {
+		try {
+			IDebugTarget target= getContext();
+			if (target == null) {
+				getTextEditor().getSite().getShell().getDisplay().beep();
+				return;
+			}
+			
+			IType type= getType(getTextEditor().getEditorInput());
+			if (type == null) {
+				return;
+			}
+	
+			IBreakpoint breakpoint= null;
+			try {
+				breakpoint= JDIDebugModel.createRunToLineBreakpoint(type, getLineNumber(), -1, -1);
+			} catch (DebugException de) {
+				errorDialog(de.getStatus());
+				return;
+			} 
+			target.breakpointAdded(breakpoint);
+			IThread[] threads= target.getThreads(); 
+			for (int i= 0; i < threads.length; i++) {
+				IThread thread= threads[i];
+				if (thread.canResume()) {
+					try {
+						thread.resume();
+					} catch (DebugException de) {
+
+					}
+					break;
+				}
+			}
+		} catch(DebugException de) {
+			JDIDebugUIPlugin.logError(de);
+		}
+	}
+
+	/**
+	 * Resolves the debug target context to set the run to line
+	 */
+	protected IDebugTarget getContext() throws DebugException{
+		IDebugTarget target= getContextFromUI();
+		if (target == null) {
+			target= getContextFromModel();
+		}
+		if (target == null) {
+			return null;
+		}
+		IThread[] threads= target.getThreads();
+		boolean threadSuspended= false;
+		for (int i= 0; i < threads.length; i++) {
+			IThread thread= threads[i];
+			if (thread.canResume()) {
+				threadSuspended=true;
+				break;
+			}
+		}
+		if (threadSuspended) {
+			return target;
+		}
+		return null;
+	}
+
+	/**
+	 * Resolves a debug target context from the model
+	 */
+	protected JDIDebugTarget getContextFromModel() throws DebugException {
+		IDebugTarget[] dts= DebugPlugin.getDefault().getLaunchManager().getDebugTargets();
+		for (int i= 0; i < dts.length; i++) {
+			JDIDebugTarget dt= (JDIDebugTarget)dts[i];
+			if (getContextFromDebugTarget(dt) != null) {
+				return dt;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Resolves a debug target context from the model
+	 */
+	protected JDIDebugTarget getContextFromThread(IThread thread) throws DebugException {
+		if (thread.isSuspended()) {
+			return (JDIDebugTarget) thread.getDebugTarget();
+		}
+		return null;
+	}
+
+	/**
+	 * Resolves a stack frame context from the UI
+	 */
+	protected IDebugTarget getContextFromUI() throws DebugException {
+		IDebugElement de= DebugUITools.getDebugContext();
+		if (de != null) {
+			if (de instanceof IThread) {
+				return getContextFromThread((IThread) de);
+			}
+			
+			return de.getDebugTarget();
+		}
+		
+		return null;
+	}
+
+
+	protected void errorDialog(IStatus status) {
+		Shell shell= getTextEditor().getSite().getShell();
+		ErrorDialog.openError(shell, ActionMessages.getString("RunToLine.error.title1"), ActionMessages.getString("RunToLine.error.message1"), status); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+	
+	/**
+	 * @see IUpdate#update()
+	 */
+	public void update() {
+		try {
+			setEnabled(getContext() != null && getTextEditor() != null);
+		} catch (DebugException de) {
+			setEnabled(false);
+			JDIDebugUIPlugin.logError(de);
+		}
+	}
+	
+	/**
+	 * Resolves a stack frame context from the model
+	 */
+	protected JDIDebugTarget getContextFromDebugTarget(JDIDebugTarget dt) throws DebugException {
+		if (dt.isTerminated() || dt.isDisconnected()) {
+			return null;
+		}
+		IThread[] threads= dt.getThreads();
+		for (int i= 0; i < threads.length; i++) {
+			IThread thread= threads[i];
+			if (thread.isSuspended()) {
+				return dt;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		update();
+		action.setEnabled(isEnabled());
+	}
+}
