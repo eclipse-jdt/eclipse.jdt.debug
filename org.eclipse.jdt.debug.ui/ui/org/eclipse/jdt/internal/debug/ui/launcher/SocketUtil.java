b0af5a143c78a60266e737950a7664a10e0c8882
Darin Swanson <darins>
2002-03-01 15:09:10 +0000

    Close socket if connection established

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/SocketUtil.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/SocketUtil.java
@@ -10,25 +10,32 @@ import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Random;
 
-import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
-
 public class SocketUtil {
 	private static final Random fgRandom= new Random(System.currentTimeMillis());
 	
 	public static int findUnusedLocalPort(String host, int searchFrom, int searchTo) {
+
 		for (int i= 0; i < 10; i++) {
+			Socket s= null;
 			int port= getRandomPort(searchFrom, searchTo);
 			try {
-				new Socket(host, port);
+				s= new Socket(host, port);
 			} catch (ConnectException e) {
 				return port;
 			} catch (IOException e) {
+			} finally {
+				if (s != null) {
+					try {
+						s.close();
+					} catch (IOException ioe) {
+					}
+				}
 			}
 		}
 		return -1;
 	}
 	
 	private static int getRandomPort(int low, int high) {
-		return (int)(fgRandom.nextFloat()*(high-low))+low;
+		return (int)(fgRandom.nextFloat() * (high-low)) + low;
 	}
 }
