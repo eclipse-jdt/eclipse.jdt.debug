08c2952a847c84ebd8360904fa1afe28dc8c0f0a
Darin Swanson <darins>
2002-03-02 00:08:40 +0000

    Code cleanup - imports

--- org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/request/ExceptionRequestImpl.java
+++ org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/request/ExceptionRequestImpl.java
@@ -5,17 +5,14 @@ package org.eclipse.jdi.internal.request;
  * All Rights Reserved.
  */
 
-import com.sun.jdi.*;
-import com.sun.jdi.request.*;
-import com.sun.jdi.connect.*;
-import com.sun.jdi.event.*;
-import org.eclipse.jdi.internal.*;
-import org.eclipse.jdi.internal.event.*;
-import org.eclipse.jdi.internal.jdwp.*;
-import java.io.*;
+import org.eclipse.jdi.internal.VirtualMachineImpl;
+import org.eclipse.jdi.internal.event.ExceptionEventImpl;
+
+import com.sun.jdi.ReferenceType;
+import com.sun.jdi.request.ExceptionRequest;
 
 /**
- * this class implements the corresponding interfaces
+ * This class implements the corresponding interfaces
  * declared by the JDI specification. See the com.sun.jdi package
  * for more information.
  *
