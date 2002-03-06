0078d2b128d16100c45c73804f1d3fe584eb2afe
Darin Wright <darin>
2002-03-06 22:53:23 +0000

    disclaimer

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/VMRunner.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/VMRunner.java
@@ -24,6 +24,12 @@ import org.eclipse.jdt.core.JavaCore;
  * <p>
  * This class is intended to be subclassed.
  * </p>
+ * <p>
+ * Note: This class/interface is part of an interim API that is still under development and expected to 
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback 
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
  * @see IVMRunner
  * @see IJavaLaunchConfigurationConstants
  * @since 2.0
