33b04d515309c28019768011c5b26260544dd8eb
Darin Wright <darin>
2002-03-11 15:14:05 +0000

    bug 10894

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/IJavaExceptionBreakpoint.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/IJavaExceptionBreakpoint.java
@@ -14,8 +14,14 @@ import org.eclipse.core.runtime.CoreException;
  * the associated exception is thrown and caught.If the breakpoint
  * is an uncaught exception breakpoint, it will suspend execution
  * when the associated exception is thrown and not caught.
- * 
+ * <p>
  * Clients are not intended to implement this interface.
+ * </p>
+ * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to 
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback 
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
  */
 public interface IJavaExceptionBreakpoint extends IJavaBreakpoint {
 	/**
--- org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/IJavaFieldVariable.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/IJavaFieldVariable.java
@@ -1,15 +1,29 @@
+package org.eclipse.jdt.debug.core;
+
 /*
  * (c) Copyright IBM Corp. 2002.
  * All Rights Reserved.
  */
-package org.eclipse.jdt.debug.core;
 
 import org.eclipse.debug.core.DebugException;
-
+/**
+ * A variable containing the value of a field.
+ * <p>
+ * Clients are not intended to implement this interface.
+ * </p>
+ * <b>Note:</b> This class/interface is part of an interim API that is still under development and expected to 
+ * change significantly before reaching stability. It is being made available at this early stage to solicit feedback 
+ * from pioneering adopters on the understanding that any code that uses this API will almost certainly be broken
+ * (repeatedly) as the API evolves.
+ * </p>
+ * @since 2.0
+ */
 public interface IJavaFieldVariable extends IJavaVariable {
 
 	/**
 	 * Returns the declaring type of this field.
+	 * 
+	 * @return the declaring type of this field
 	 */
 	public IJavaType getDeclaringType();
 
