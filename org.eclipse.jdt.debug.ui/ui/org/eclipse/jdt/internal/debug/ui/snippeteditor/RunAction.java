9d605669eca7d65c24f9a467c2437a8f7fcfaa13
Darin Swanson <darins>
2002-04-29 20:26:31 +0000

    Bug 9690 - Need accelerator for "run" in snippet editor

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/snippeteditor/RunAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/snippeteditor/RunAction.java
@@ -6,11 +6,21 @@ package org.eclipse.jdt.internal.debug.ui.snippeteditor;
  */
 
 import org.eclipse.jdt.internal.debug.ui.JavaDebugImages;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 /**
  * Runs a snippet
  */
-public class RunAction extends SnippetAction {
+public class RunAction extends SnippetAction implements IWorkbenchWindowActionDelegate, IPartListener {
+	
+	private IWorkbenchWindow fWindow;
+	private IAction fAction;
 	
 	public RunAction(JavaSnippetEditor editor) {
 		super(editor);
@@ -22,10 +32,111 @@ public class RunAction extends SnippetAction {
 		setHoverImageDescriptor(JavaDebugImages.DESC_TOOL_RUNSNIPPET_HOVER);
 	}
 	
+	public RunAction() {
+		super(null);
+	}
+	
 	/**
 	 * @see IAction#run()
 	 */
 	public void run() {
 		getEditor().evalSelection(JavaSnippetEditor.RESULT_RUN);
 	} 
+	/**
+	 * @see IWorkbenchWindowActionDelegate#dispose()
+	 */
+	public void dispose() {
+		getWindow().getPartService().removePartListener(this);
+	}
+
+	/**
+	 * @see IWorkbenchWindowActionDelegate#init(IWorkbenchWindow)
+	 */
+	public void init(IWorkbenchWindow window) {
+		setWindow(window);
+		IWorkbenchPage page= window.getActivePage();
+		if (page != null) {
+			if (page.getActiveEditor() instanceof JavaSnippetEditor) {
+				setEditor((JavaSnippetEditor)page.getActiveEditor());
+			}
+		}
+		window.getPartService().addPartListener(this);
+		update();
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		run();
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		setAction(action);
+		update();
+		
+	}
+	
+	protected IWorkbenchWindow getWindow() {
+		return fWindow;
+	}
+		
+	protected void setWindow(IWorkbenchWindow window) {
+		fWindow = window;
+	}
+	
+	protected IAction getAction() {
+		return fAction;
+	}
+
+	protected void setAction(IAction action) {
+		fAction = action;
+	}
+	
+	public void setEnabled(boolean enabled) {
+		if (getAction() == null) {
+			super.setEnabled(enabled);
+		} else {
+			getAction().setEnabled(enabled);
+		}
+	}
+	/**
+	 * @see IPartListener#partActivated(IWorkbenchPart)
+	 */
+	public void partActivated(IWorkbenchPart part) {
+		if (part instanceof JavaSnippetEditor) {
+			setEditor((JavaSnippetEditor)part);
+		}
+	}
+
+	/**
+	 * @see IPartListener#partBroughtToTop(IWorkbenchPart)
+	 */
+	public void partBroughtToTop(IWorkbenchPart part) {
+	}
+
+	/**
+	 * @see IPartListener#partClosed(IWorkbenchPart)
+	 */
+	public void partClosed(IWorkbenchPart part) {
+		if (part == getEditor()) {
+			setEditor(null);
+		}
+	}
+
+	/**
+	 * @see IPartListener#partDeactivated(IWorkbenchPart)
+	 */
+	public void partDeactivated(IWorkbenchPart part) {
+	}
+
+	/**
+	 * @see IPartListener#partOpened(IWorkbenchPart)
+	 */
+	public void partOpened(IWorkbenchPart part) {
+	}
+
 }
