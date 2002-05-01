12e19e2a79c7414449f787ee6c201f3b38f25869
Darin Swanson <darins>
2002-05-01 19:22:25 +0000

    Bug 14836 - BIDI: Scrapbook editor: bad display of adjacent Hebrew or Arabic

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JDISourceViewer.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JDISourceViewer.java
@@ -5,18 +5,95 @@ package org.eclipse.jdt.internal.debug.ui;
  * All Rights Reserved.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdt.internal.ui.text.JavaPartitionScanner;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITypedRegion;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.source.IVerticalRuler;
 import org.eclipse.jface.text.source.SourceViewer;
+import org.eclipse.swt.custom.BidiSegmentEvent;
+import org.eclipse.swt.custom.BidiSegmentListener;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
 
 public class JDISourceViewer extends SourceViewer {
 
 	public JDISourceViewer(Composite parent, IVerticalRuler ruler, int styles) {
 		super(parent, ruler, styles);
+		StyledText text= this.getTextWidget();
+		text.addBidiSegmentListener(new  BidiSegmentListener() {
+			public void lineGetSegments(BidiSegmentEvent event) {
+				try {
+					event.segments= getBidiLineSegments(event.lineOffset);
+				} catch (BadLocationException x) {
+					// ignore
+				}
+			}
+		});
 	}
 
 	public IContentAssistant getContentAssistant() {
 		return fContentAssistant;
 	}
+	
+	/**
+	 * Returns a segmentation of the line of the given document appropriate for bidi rendering.
+	 * The default implementation returns only the string literals of a Java code line as segments.
+	 * 
+	 * @param document the document
+	 * @param lineOffset the offset of the line
+	 * @return the line's bidi segmentation
+	 * @throws BadLocationException in case lineOffset is not valid in document
+	 */
+	protected int[] getBidiLineSegments(int lineOffset) throws BadLocationException {
+		IDocument document= getDocument();
+		if (document == null) {
+			return null;
+		}
+		IRegion line= document.getLineInformationOfOffset(lineOffset);
+		ITypedRegion[] linePartitioning= document.computePartitioning(lineOffset, line.getLength());
+		
+		List segmentation= new ArrayList();
+		for (int i= 0; i < linePartitioning.length; i++) {
+			if (JavaPartitionScanner.JAVA_STRING.equals(linePartitioning[i].getType()))
+				segmentation.add(linePartitioning[i]);
+		}
+		
+		
+		if (segmentation.size() == 0) 
+			return null;
+			
+		int size= segmentation.size();
+		int[] segments= new int[size * 2 + 1];
+		
+		int j= 0;
+		for (int i= 0; i < size; i++) {
+			ITypedRegion segment= (ITypedRegion) segmentation.get(i);
+			
+			if (i == 0)
+				segments[j++]= 0;
+				
+			int offset= segment.getOffset() - lineOffset;
+			if (offset > segments[j - 1])
+				segments[j++]= offset;
+				
+			if (offset + segment.getLength() >= line.getLength())
+				break;
+				
+			segments[j++]= offset + segment.getLength();
+		}
+		
+		if (j < segments.length) {
+			int[] result= new int[j];
+			System.arraycopy(segments, 0, result, 0, j);
+			segments= result;
+		}
+		
+		return segments;
+	}	
 }
