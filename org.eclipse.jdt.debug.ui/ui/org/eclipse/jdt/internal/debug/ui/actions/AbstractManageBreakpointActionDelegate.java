d7cc8b07b1db03991f7e06c7b1b309bd95d60566
Darin Swanson <darins>
2002-05-07 20:22:58 +0000

    Bug 15377 - Add: Add Method Breakpoint to the Debug menu

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/AbstractManageBreakpointActionDelegate.java
@@ -0,0 +1,126 @@
+package org.eclipse.jdt.internal.debug.ui.actions;
+
+/*
+ * (c) Copyright IBM Corp. 2002.
+ * All Rights Reserved.
+ */
+
+import org.eclipse.jdt.core.IClassFile;
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.debug.core.IJavaBreakpoint;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jdt.ui.IJavaElementSearchConstants;
+import org.eclipse.jdt.ui.IWorkingCopyManager;
+import org.eclipse.jdt.ui.JavaUI;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+
+
+public abstract class AbstractManageBreakpointActionDelegate extends ManageBreakpointActionDelegate implements IObjectActionDelegate {
+
+	protected String fRemoveText;
+	protected String fRemoveDescription;
+	protected String fAddText;
+	protected String fAddDescription;
+	private IAction fAction;
+	private IMember fMember;
+	private IJavaBreakpoint fBreakpoint;
+	
+	/**
+	 * @see IObjectActionDelegate#setActivePart(IAction, IWorkbenchPart)
+	 */
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+	}
+
+	/**
+	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+		setAction(action);
+		if (selection == null) {
+			return;
+		}
+		update(selection);
+	}
+
+	protected IAction getAction() {
+		return fAction;
+	}
+
+	protected void setAction(IAction action) {
+		fAction = action;
+	}
+
+	protected IMember getMember() {
+		return fMember;
+	}
+
+	protected void setMember(IMember element) {
+		fMember = element;
+	}
+	
+	protected abstract IMember getElement(ISelection s);
+	
+	protected void update(ISelection selection) {
+		setMember(getElement(selection));
+		update();
+	}	
+	
+	protected abstract IJavaBreakpoint getBreakpoint(IMember element);
+	
+	protected void update() {
+		if(getMember() == null) {
+			setBreakpoint(null);
+		} else {
+			setBreakpoint(getBreakpoint(getMember()));
+		}
+		boolean doesNotExist= getBreakpoint() == null;
+		getAction().setText(doesNotExist ? fAddText : fRemoveText);
+		getAction().setDescription(doesNotExist ? fAddDescription : fRemoveDescription);
+	}
+	
+	protected IJavaBreakpoint getBreakpoint() {
+		return fBreakpoint;
+	}
+
+	protected void setBreakpoint(IJavaBreakpoint breakpoint) {
+		fBreakpoint = breakpoint;
+	}
+	
+	protected IMember getMember0(ITextSelection selection, IEditorInput editorInput) {
+		setLineNumber(selection.getStartLine() + 1);
+		IMember m= null;
+		try {
+			IClassFile classFile= (IClassFile)editorInput.getAdapter(IClassFile.class);
+			if (classFile != null) {
+				IJavaElement e= classFile.getElementAt(selection.getOffset());
+				if (e instanceof IMember) {
+					m= (IMember)e;
+					setMember(m);
+				}
+			} else {
+				IWorkingCopyManager manager= JavaUI.getWorkingCopyManager();
+				ICompilationUnit unit= manager.getWorkingCopy(editorInput);
+				if (unit == null) {
+					return null;
+				}
+				IJavaElement e = unit.getElementAt(selection.getOffset());
+				if (e instanceof IMember) {
+					m= (IMember)e;
+					setMember(m);
+				}
+			}
+		} catch (JavaModelException jme) {
+			JDIDebugUIPlugin.log(jme);
+		}
+		return m;
+	}
+}
---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ManageMethodBreakpointActionDelegate.java
@@ -0,0 +1,169 @@
+package org.eclipse.jdt.internal.debug.ui.actions;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved.
+ */
+ 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IBreakpointManager;
+import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.jdt.core.IMember;
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.ISourceRange;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
+import org.eclipse.jdt.debug.core.IJavaBreakpoint;
+import org.eclipse.jdt.debug.core.IJavaMethodBreakpoint;
+import org.eclipse.jdt.debug.core.JDIDebugModel;
+import org.eclipse.jdt.internal.debug.ui.BreakpointUtils;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+/**
+ * Adds a method breakpoint on a single selected element of type IMethod 
+ */
+public class ManageMethodBreakpointActionDelegate extends AbstractManageBreakpointActionDelegate {
+	
+	public ManageMethodBreakpointActionDelegate() {
+		super();
+		fAddText= ActionMessages.getString("ManageMethodBreakpointAction.&Add_Method_Breakpoint_1"); //$NON-NLS-1$
+		fAddDescription= ActionMessages.getString("ManageMethodBreakpointAction.Add_a_method_breakpoint_2"); //$NON-NLS-1$
+		fRemoveText= ActionMessages.getString("ManageMethodBreakpointAction.Remove_&Method_Breakpoint_4"); //$NON-NLS-1$
+		fRemoveDescription= ActionMessages.getString("ManageMethodBreakpointAction.Remove_a_method_breakpoint_5"); //$NON-NLS-1$
+	}
+		
+	protected void update() {
+		IMember member= getMember();
+		if (member instanceof IMethod) {
+			IMethod method= (IMethod)member;
+			if (method.isBinary()) { // only add to class files
+				getAction().setEnabled(true);
+				setBreakpoint(getBreakpoint(method));
+				boolean doesNotExist= getBreakpoint() == null;
+				getAction().setText(doesNotExist ? fAddText : fRemoveText);
+				getAction().setDescription(doesNotExist ? fAddDescription : fRemoveDescription);
+				return;
+			}
+		}
+		
+		getAction().setEnabled(false);
+	}
+	
+	protected IJavaBreakpoint getBreakpoint(IMember method) {
+		IBreakpointManager breakpointManager= DebugPlugin.getDefault().getBreakpointManager();
+		IBreakpoint[] breakpoints= breakpointManager.getBreakpoints(JDIDebugModel.getPluginIdentifier());
+		for (int i= 0; i < breakpoints.length; i++) {
+			IBreakpoint breakpoint= breakpoints[i];
+			if (breakpoint instanceof IJavaMethodBreakpoint) {
+				IMember container = null;
+				try {
+					container= BreakpointUtils.getMember((IJavaMethodBreakpoint) breakpoint);
+				} catch (CoreException e) {
+					JDIDebugUIPlugin.log(e);
+					return null;
+				}
+				if (method.equals(container)) {
+					return (IJavaBreakpoint)breakpoint;
+				}
+			}
+		}
+		return null;
+	}
+	
+	protected IMember getElement(ISelection s) {
+		if (s instanceof IStructuredSelection) {
+			IStructuredSelection ss= (IStructuredSelection) s;
+			if (ss.size() == 1) {					
+				Object o=  ss.getFirstElement();
+				if (o instanceof IMethod) {
+					return (IMethod) o;
+				}
+			}
+		} else if (s instanceof ITextSelection) {
+			ITextSelection ts= (ITextSelection)s;
+			if (getTextEditor() != null) {
+				return getMember0(ts, getTextEditor().getEditorInput());
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @see IActionDelegate#run(IAction)
+	 */
+	public void run(IAction action) {
+		if (getBreakpoint() == null) {
+			// add breakpoint
+			try {
+				IMethod method = (IMethod)getMember();
+				int start = -1;
+				int end = -1;
+				ISourceRange range = method.getNameRange();
+				if (range != null) {
+					start = range.getOffset();
+					end = start + range.getLength();
+				}
+				Map attributes = new HashMap(10);
+				BreakpointUtils.addJavaBreakpointAttributes(attributes, method);
+				String methodName = method.getElementName();
+				if (((IMethod)method).isConstructor()) {
+					methodName = "<init>"; //$NON-NLS-1$
+				}
+				setBreakpoint(JDIDebugModel.createMethodBreakpoint(BreakpointUtils.getBreakpointResource(method), 
+					method.getDeclaringType().getFullyQualifiedName(), methodName, method.getSignature(), true, false, false, -1, start, end, 0, true, attributes));
+			} catch (CoreException x) {
+				JDIDebugUIPlugin.log(x);
+				MessageDialog.openError(JDIDebugUIPlugin.getActiveWorkbenchShell(), ActionMessages.getString("ManageMethodBreakpointAction.Problems_creating_breakpoint_7"), x.getMessage()); //$NON-NLS-1$
+			}
+		} else {
+			// remove breakpoint
+			try {
+				IBreakpointManager breakpointManager= DebugPlugin.getDefault().getBreakpointManager();
+				breakpointManager.removeBreakpoint(getBreakpoint(), true);
+			} catch (CoreException x) {
+				JDIDebugUIPlugin.log(x);
+				MessageDialog.openError(JDIDebugUIPlugin.getActiveWorkbenchShell(), ActionMessages.getString("ManageMethodBreakpointAction.Problems_removing_breakpoint_8"), x.getMessage()); //$NON-NLS-1$
+			}
+		}
+		update();
+	}
+	
+	protected String resolveMethodSignature(IMethod method) {
+		try {
+			IType type= method.getDeclaringType();
+			String signature= method.getSignature();
+			String[] pTypes= Signature.getParameterTypes(signature);
+			String returnType= Signature.getReturnType(signature);
+			String[] resolvedTypes= new String[pTypes.length];
+			for (int i = 0; i < pTypes.length; i++) {
+				String unresolvedTypeName= pTypes[i];
+				unresolvedTypeName= unresolvedTypeName.substring(1, unresolvedTypeName.length() - 2);
+				String[][] resolvedTypeNames= type.resolveType(unresolvedTypeName);
+				if (resolvedTypeNames == null || resolvedTypeNames.length > 1) {
+					//cannot be uniquely resolved
+					return null;
+				}
+				StringBuffer buf= new StringBuffer();
+				for (int j= 0; j < resolvedTypeNames[0].length; j++) {
+					buf.append(resolvedTypeNames[0][j]);
+				}
+				resolvedTypes[i]= buf.toString();
+			}
+			
+			return Signature.createMethodSignature(resolvedTypes, returnType);	
+		} catch (JavaModelException jme) {
+			JDIDebugUIPlugin.log(jme);
+			return null;
+		} 
+	}
+}
