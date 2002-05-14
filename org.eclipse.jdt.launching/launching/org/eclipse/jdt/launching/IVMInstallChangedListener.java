f5d4264d51930ce8ef338c623b514ef2cf6908f9
Darin Wright <darin>
2002-05-14 20:30:17 +0000

    bug 15200

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/IVMInstallChangedListener.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/IVMInstallChangedListener.java
@@ -8,8 +8,8 @@ import org.eclipse.jdt.core.IJavaProject;
  */
 
 /**
- * A VM install changed listener is notified when the VM install assigned
- * to a project changes, or when the workspace default VM install changes.
+ * A VM install changed listener is notified when the VM install attribute 
+ * changes, or when the workspace default VM install changes.
  * Listeners register with <code>JavaRuntime</code>.
  * <p>
  * Clients may implement this interface.
@@ -17,20 +17,7 @@ import org.eclipse.jdt.core.IJavaProject;
  * @since 2.0
  */
 public interface IVMInstallChangedListener {
-	
-	/**
-	 * Notification that the VM install assigned to the given project
-	 * has changed.
-	 * 
-	 * @param project the project for which the VM install assignment
-	 *  has changed
-	 * @param previous the VM install that was previously assigned
-	 * 	to the given project, possibly <code>null</code>
-	 * @param current the VM install that is currently assigned to the
-	 * 	project, possibly <code>null</code>
-	 */
-	public void projectVMInstallChanged(IJavaProject project, IVMInstall previous, IVMInstall current);
-	
+		
 	/**
 	 * Notification that the workspace default VM install
 	 * has changed.
