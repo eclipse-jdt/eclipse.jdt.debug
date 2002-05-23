08232d858c17def84d85e291a7f3c913fc044fa4
jszursze <jszursze>
2002-05-23 18:34:31 +0000

    more tests

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/MethodBreakpointTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/MethodBreakpointTests.java
@@ -12,7 +12,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.debug.core.model.IBreakpoint;
+import org.eclipse.debug.core.model.IVariable;
 import org.eclipse.jdt.debug.core.IJavaDebugTarget;
+import org.eclipse.jdt.debug.core.IJavaMethodBreakpoint;
+import org.eclipse.jdt.debug.core.IJavaPrimitiveValue;
+import org.eclipse.jdt.debug.core.IJavaStackFrame;
 import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.tests.AbstractDebugTest;
 
@@ -64,7 +68,7 @@ public class MethodBreakpointTests extends AbstractDebugTest {
 		
 		IJavaDebugTarget debugTarget= null;
 		try {
-			debugTarget= launchAndTerminate(typeName, 3000);
+			debugTarget= launchAndTerminate(typeName, 6000);
 		} finally {
 			terminateAndRemove(debugTarget);
 			removeAllBreakpoints();
@@ -128,6 +132,60 @@ public class MethodBreakpointTests extends AbstractDebugTest {
 			assertEquals("should hit entry breakpoint first", bps.get(0),hit);
 			
 			resumeAndExit(thread);
+		} finally {
+			terminateAndRemove(thread);
+			removeAllBreakpoints();
+		}		
+	}
+
+	public void testHitCountEntryBreakpoint() throws Exception {
+		String typeName = "MethodLoop";
+		List bps = new ArrayList();
+		IJavaMethodBreakpoint bp = createMethodBreakpoint(typeName, "calculateSum", "()V", true, false);
+		bp.setHitCount(3);
+		
+		IJavaThread thread= null;
+		try {
+			thread= launch(typeName);
+			assertNotNull("Method entry breakpoint not hit within timeout period", thread);
+			
+			IJavaStackFrame frame = (IJavaStackFrame)thread.getTopStackFrame();
+			IVariable var = frame.findVariable("sum");
+			assertNotNull("Could not find variable 'sum'", var);
+			
+			IJavaPrimitiveValue value = (IJavaPrimitiveValue)var.getValue();
+			assertNotNull("variable 'sum' has no value", value);
+			int iValue = value.getIntValue();
+			assertTrue("value of 'sum' should be '3', but was " + iValue, iValue == 3);			
+			
+			bp.delete();
+		} finally {
+			terminateAndRemove(thread);
+			removeAllBreakpoints();
+		}		
+	}
+
+	public void testHitCountExitBreakpoint() throws Exception {
+		String typeName = "MethodLoop";
+		List bps = new ArrayList();
+		IJavaMethodBreakpoint bp = createMethodBreakpoint(typeName, "calculateSum", "()V", false, true);
+		bp.setHitCount(3);
+		
+		IJavaThread thread= null;
+		try {
+			thread= launch(typeName);
+			assertNotNull("Method exit breakpoint not hit within timeout period", thread);
+			
+			IJavaStackFrame frame = (IJavaStackFrame)thread.getTopStackFrame();
+			IVariable var = frame.findVariable("sum");
+			assertNotNull("Could not find variable 'sum'", var);
+			
+			IJavaPrimitiveValue value = (IJavaPrimitiveValue)var.getValue();
+			assertNotNull("variable 'sum' has no value", value);
+			int iValue = value.getIntValue();
+			assertTrue("value of 'sum' should be '6', but was " + iValue, iValue == 6);			
+			
+			bp.delete();
 		} finally {
 			terminateAndRemove(thread);
 			removeAllBreakpoints();
