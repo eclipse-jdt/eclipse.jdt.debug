5dac64d8ea156611154d6cba296e902f5777c584
Darin Wright <darin>
2002-07-23 17:41:52 +0000

    Bug 21197

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ManageBreakpointRulerActionDelegate.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ManageBreakpointRulerActionDelegate.java
@@ -1,39 +1,18 @@
 package org.eclipse.jdt.internal.debug.ui.actions;
 
 /**********************************************************************
-Copyright (c) 2000, 2002 IBM Corp. and others.
-All rights reserved. This program and the accompanying materials
-are made available under the terms of the Common Public License v0.5
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
 which accompanies this distribution, and is available at
-http://www.eclipse.org/legal/cpl-v05.html
-
-Contributors:
-    IBM Corporation - Initial implementation
+http://www.eclipse.org/legal/cpl-v10.html
 **********************************************************************/
 
-import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
-import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.texteditor.AbstractRulerActionDelegate;
 import org.eclipse.ui.texteditor.ITextEditor;
 
 public class ManageBreakpointRulerActionDelegate extends AbstractRulerActionDelegate {
-
-	/**
-	 * @see IEditorActionDelegate#setActiveEditor(IAction, IEditorPart)
-	 */
-	public void setActiveEditor(IAction callerAction, IEditorPart targetEditor) {
-		
-		// only care about compilation unit and class file editors
-		if (targetEditor != null) {
-			String id= targetEditor.getSite().getId();
-			if (!id.equals(JavaUI.ID_CU_EDITOR) && !id.equals(JavaUI.ID_CF_EDITOR))
-				targetEditor= null;
-		}
-		
-		super.setActiveEditor(callerAction, targetEditor);
-	}
 
 	/**
 	 * @see AbstractRulerActionDelegate#createAction()
