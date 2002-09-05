2c8443f8213a3073c25eb90bfacb2e7961abbb43
Darin Swanson <darins>
2002-09-05 16:22:18 +0000

    Released with verbose on...BAD

--- org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/VirtualMachineManagerImpl.java
+++ org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/VirtualMachineManagerImpl.java
@@ -92,8 +92,6 @@ public class VirtualMachineManagerImpl implements VirtualMachineManager {
 		// Get jdi.ini info.
 		URL url = getClass().getResource("/jdi.ini"); //$NON-NLS-1$
 		if (url == null) {
-			fVerbose= true;
-			fVerboseFile= "c://temp//trace.out";
 			return;
 		}
 			
