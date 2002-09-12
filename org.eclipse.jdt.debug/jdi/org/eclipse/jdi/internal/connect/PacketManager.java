ae029e5a3b6b66ff7ab0646193e176876d17cdba
Darin Swanson <darins>
2002-09-12 21:43:08 +0000

    Javadoc updates

--- org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/connect/PacketManager.java
+++ org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/connect/PacketManager.java
@@ -10,26 +10,27 @@ package org.eclipse.jdi.internal.connect;
  *
  */
 public abstract class PacketManager implements Runnable {
-	/** Flag that indicates that the VM is dosconnected. */
+	/** Flag that indicates that the VM is disconnected. */
 	private boolean fDisconnectedVM = false;
 	/** Connector that performs IO to Virtual Machine. */
 	private ConnectorImpl fConnector;
 	/** Thread that handles the communication the other way (e.g. if we are sending, the receiving thread). */
 	private Thread fPartnerThread;
 	
-	/*
+	/**
 	 * Creates new PacketManager.
 	 */
 	protected PacketManager(ConnectorImpl connector) {
 		fConnector = connector;
 	}
 
-	/*
+	/**
 	 * Used to indicate that an IO exception occurred, closes connection to Virtual Machine.
 	 */
 	public synchronized void disconnectVM() {
-		if (fDisconnectedVM)
+		if (fDisconnectedVM) {
 			return;
+		}
 
 		fDisconnectedVM = true;
 		fConnector.close();
@@ -39,15 +40,15 @@ public abstract class PacketManager implements Runnable {
 		fPartnerThread.interrupt();
 	}
 	
-	/*
+	/**
 	 * @return Returns whether an IO exception has occurred.
 	 */
 	public boolean VMIsDisconnected() {
 		return fDisconnectedVM;
 	}
 	
-	/*
-	 * Assigns thread of parter, to be notified if we have an IO exception.
+	/**
+	 * Assigns thread of partner, to be notified if we have an IO exception.
 	 */
 	public void setPartnerThread(Thread thread) {
 		fPartnerThread = thread;
