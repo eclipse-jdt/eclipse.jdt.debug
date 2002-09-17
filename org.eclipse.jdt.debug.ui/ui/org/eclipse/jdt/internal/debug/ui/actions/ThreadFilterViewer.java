cd3f7f6e2986052c0512cf4b12a6d9704b262c2c
Darin Swanson <darins>
2002-09-17 22:15:45 +0000

    Remove unused import

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ThreadFilterViewer.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ThreadFilterViewer.java
@@ -34,7 +34,6 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 /**
