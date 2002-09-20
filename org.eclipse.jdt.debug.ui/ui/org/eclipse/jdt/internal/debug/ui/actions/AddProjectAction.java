80dc71e410f9af671b24df6f6697a929fcfb0439
Darin Wright <darin>
2002-09-20 14:11:27 +0000

    bug 18627

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/AddProjectAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/AddProjectAction.java
@@ -6,27 +6,68 @@ package org.eclipse.jdt.internal.debug.ui.actions;
  */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.jdt.core.IClasspathContainer;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaModel;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.debug.ui.IJavaDebugUIConstants;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.internal.debug.ui.launcher.RuntimeClasspathViewer;
 import org.eclipse.jdt.launching.IRuntimeClasspathEntry;
 import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.ui.JavaElementLabelProvider;
 import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.jface.viewers.Viewer;
 
 /**
  * Adds a project to the runtime class path.
  */
 public class AddProjectAction extends RuntimeClasspathAction {
+	
+	class ContentProvider implements IStructuredContentProvider {
+		
+		private List fProjects;
+		
+		public ContentProvider(List projects) {
+			fProjects = projects;
+		}
+		
+		/**
+		 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+		 */
+		public Object[] getElements(Object inputElement) {
+			return fProjects.toArray();
+		}
+
+		/**
+		 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+		 */
+		public void dispose() {
+		}
+
+		/**
+		 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+		 */
+		public void inputChanged(
+			Viewer viewer,
+			Object oldInput,
+			Object newInput) {
+		}
+
+	}	
 
 	public AddProjectAction(RuntimeClasspathViewer viewer) {
 		super(ActionMessages.getString("AddProjectAction.Add_Project_1"), viewer); //$NON-NLS-1$
@@ -41,20 +82,51 @@ public class AddProjectAction extends RuntimeClasspathAction {
 		List projects = getPossibleAdditions();
 		
 		ILabelProvider labelProvider= new JavaElementLabelProvider(JavaElementLabelProvider.SHOW_DEFAULT);
-		ElementListSelectionDialog dialog= new ElementListSelectionDialog(getShell(), labelProvider);
+		IStructuredContentProvider content = new ContentProvider(projects);
+		ProjectSelectionDialog dialog= new ProjectSelectionDialog(getShell(),projects, content, labelProvider, ActionMessages.getString("AddProjectAction.Choose_&project(s)_to_add__3")); //$NON-NLS-1$
 		dialog.setTitle(ActionMessages.getString("AddProjectAction.Project_Selection_2")); //$NON-NLS-1$
-		dialog.setMessage(ActionMessages.getString("AddProjectAction.Choose_&project(s)_to_add__3")); //$NON-NLS-1$
-		dialog.setElements(projects.toArray());
-		dialog.setMultipleSelection(true);
-		
+		MultiStatus status = new MultiStatus(JDIDebugUIPlugin.getUniqueIdentifier(), IJavaDebugUIConstants.INTERNAL_ERROR, ActionMessages.getString("AddProjectAction.One_or_more_exceptions_occurred_while_adding_projects._1"), null); //$NON-NLS-1$
+				
 		if (dialog.open() == dialog.OK) {			
 			Object[] selections = dialog.getResult();
-			IRuntimeClasspathEntry[] entries = new IRuntimeClasspathEntry[selections.length];
-			for (int i = 0; i < selections.length; i++) {
-				entries[i] = JavaRuntime.newProjectRuntimeClasspathEntry((IJavaProject)selections[i]);
+			
+			List additions = new ArrayList(selections.length);
+			try {
+				for (int i = 0; i < selections.length; i++) {
+					IJavaProject jp = (IJavaProject)selections[i];
+					if (dialog.isAddRequiredProjects()) {
+						collectRequiredProjects(jp, additions);
+					} else {
+						additions.add(jp);
+					}
+				}
+			} catch (JavaModelException e) {
+				status.add(e.getStatus());
 			}
+			
+			List runtimeEntries = new ArrayList(additions.size());
+			Iterator iter = additions.iterator();
+			while (iter.hasNext()) {
+				IJavaProject jp = (IJavaProject)iter.next();
+				runtimeEntries.add(JavaRuntime.newProjectRuntimeClasspathEntry(jp));
+				if (dialog.isAddExportedEntries()) {
+					try {
+						collectExportedEntries(jp, runtimeEntries);
+					} catch (CoreException e) {
+						status.add(e.getStatus());
+					}
+				}
+			}
+			IRuntimeClasspathEntry[] entries = (IRuntimeClasspathEntry[])runtimeEntries.toArray(new IRuntimeClasspathEntry[runtimeEntries.size()]);
 			getViewer().addEntries(entries);
-		}				
+		}	
+		
+		content.dispose();
+		labelProvider.dispose();			
+		
+		if (!status.isOK()) {
+			JDIDebugUIPlugin.errorDialog(status.getMessage(), status);
+		}
 	}
 
 	/**
@@ -91,6 +163,90 @@ public class AddProjectAction extends RuntimeClasspathAction {
 		}
 		remaining.removeAll(alreadySelected);
 		return remaining;		
+	}
+	
+	/**
+	 * Adds all projects required by <code>proj</code> to the list
+	 * <code>res</code>
+	 * 
+	 * @param proj the project for which to compute required
+	 *  projects
+	 * @param res the list to add all required projects too
+	 */
+	protected void collectRequiredProjects(IJavaProject proj, List res) throws JavaModelException {
+		if (!res.contains(proj)) {
+			res.add(proj);
+			
+			IJavaModel model= proj.getJavaModel();
+			
+			IClasspathEntry[] entries= proj.getRawClasspath();
+			for (int i= 0; i < entries.length; i++) {
+				IClasspathEntry curr= entries[i];
+				if (curr.getEntryKind() == IClasspathEntry.CPE_PROJECT) {
+					IJavaProject ref= model.getJavaProject(curr.getPath().segment(0));
+					if (ref.exists()) {
+						collectRequiredProjects(ref, res);
+					}
+				}
+			}
+		}
+	}		
+	
+	/**
+	 * Adds all exported entries defined by <code>proj</code> to the list
+	 * <code>runtimeEntries</code>.
+	 * 	 * @param proj	 * @param runtimeEntries	 * @throws JavaModelException	 */
+	protected void collectExportedEntries(IJavaProject proj, List runtimeEntries) throws CoreException {
+		IClasspathEntry[] entries = proj.getRawClasspath();
+		for (int i = 0; i < entries.length; i++) {
+			IClasspathEntry entry = entries[i];
+			if (entry.isExported()) {
+				IRuntimeClasspathEntry rte = null;
+				switch (entry.getEntryKind()) {
+					case IClasspathEntry.CPE_CONTAINER:
+						IClasspathContainer container = JavaCore.getClasspathContainer(entry.getPath(), proj);
+						int kind = 0;
+						switch (container.getKind()) {
+							case IClasspathContainer.K_APPLICATION:
+								kind = IRuntimeClasspathEntry.USER_CLASSES;
+								break;
+							case IClasspathContainer.K_SYSTEM:
+								kind = IRuntimeClasspathEntry.BOOTSTRAP_CLASSES;
+								break;
+							case IClasspathContainer.K_DEFAULT_SYSTEM:
+								kind = IRuntimeClasspathEntry.STANDARD_CLASSES;
+								break;
+						}
+						rte = JavaRuntime.newRuntimeContainerClasspathEntry(entry.getPath(), kind);
+						break;
+					case IClasspathEntry.CPE_LIBRARY:
+						rte = JavaRuntime.newArchiveRuntimeClasspathEntry(entry.getPath());
+						rte.setSourceAttachmentPath(entry.getSourceAttachmentPath());
+						rte.setSourceAttachmentRootPath(entry.getSourceAttachmentRootPath());
+						break;
+					case IClasspathEntry.CPE_PROJECT:
+						String name = entry.getPath().segment(0);
+						IProject p = ResourcesPlugin.getWorkspace().getRoot().getProject(name);
+						if (p.exists()) {
+							IJavaProject jp = JavaCore.create(p);
+							if (jp.exists()) {
+								rte = JavaRuntime.newProjectRuntimeClasspathEntry(jp);
+							}
+						}
+						break;
+					case IClasspathEntry.CPE_VARIABLE:
+						rte = JavaRuntime.newVariableRuntimeClasspathEntry(entry.getPath());
+						break;
+					default:
+						break;
+				}
+				if (rte != null) {
+					if (!runtimeEntries.contains(rte)) {
+						runtimeEntries.add(rte);
+					}
+				}
+			}
+		}
 	}
 
 }
---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ProjectSelectionDialog.java
@@ -0,0 +1,86 @@
+package org.eclipse.jdt.internal.debug.ui.actions;
+
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
+import org.eclipse.jdt.internal.compiler.ast.TrueLiteral;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.dialogs.ListSelectionDialog;
+
+/**
+ * A dialog for selecting projects to add to a classpath or source
+ * lookup path. Optionally specifies whether
+ * exported entries and required projects should also be added.
+ */
+public class ProjectSelectionDialog extends ListSelectionDialog {
+	
+	private boolean fAddExportedEntries = true;
+	private boolean fAddRequiredProjects = true;
+
+	/**
+	 * @see ListSelectionDialog
+	 */
+	public ProjectSelectionDialog(
+		Shell parentShell,
+		Object input,
+		IStructuredContentProvider contentProvider,
+		ILabelProvider labelProvider,
+		String message) {
+		super(parentShell, input, contentProvider, labelProvider, message);
+	}
+
+	/**
+	 * @see org.eclipse.jface.dialogs.Dialog#createDialogArea(org.eclipse.swt.widgets.Composite)
+	 */
+	protected Control createDialogArea(Composite parent) {
+		Composite composite = (Composite)super.createDialogArea(parent);
+		
+		final Button addExported = new Button(composite, SWT.CHECK);
+		addExported.setText(ActionMessages.getString("ProjectSelectionDialog.Add_exported_entries_of_selected_projects._1")); //$NON-NLS-1$
+		addExported.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				fAddExportedEntries = addExported.getSelection();
+			}
+		});
+		addExported.setSelection(fAddExportedEntries);
+		
+		final Button addRequired = new Button(composite, SWT.CHECK);
+		addRequired.setText(ActionMessages.getString("ProjectSelectionDialog.Add_required_projects_of_selected_projects._2")); //$NON-NLS-1$
+		addRequired.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				fAddRequiredProjects = addRequired.getSelection();
+			}
+		});
+		addRequired.setSelection(fAddRequiredProjects);		
+		
+		return composite;
+	}
+	
+	/**
+	 * Returns whether the user has selected to add exported entries.
+	 * 	 * @return whether the user has selected to add exported entries	 */
+	public boolean isAddExportedEntries() {
+		return fAddExportedEntries;
+	}
+	
+	/**
+	 * Returns whether the user has selected to add required projects.
+	 * 	 * @return whether the user has selected to add required projects	 */
+	public boolean isAddRequiredProjects() {
+		return fAddRequiredProjects;
+	}
+
+}
