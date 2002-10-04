baf0111eb6f27ecaeceee92832416b4a04bdcd95
Darin Wright <darin>
2002-10-04 17:08:17 +0000

    bug 24094

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/JavaExceptionBreakpoint.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/JavaExceptionBreakpoint.java
@@ -325,8 +325,12 @@ public class JavaExceptionBreakpoint extends JavaBreakpoint implements IJavaExce
 					}
 					if (included && !excluded) {
 						return !suspend(thread);
+					} else {
+						return true;
 					}
-			}
+				} else {
+					return !suspend(thread);
+				}
 		}	
 		return true;
 	}
