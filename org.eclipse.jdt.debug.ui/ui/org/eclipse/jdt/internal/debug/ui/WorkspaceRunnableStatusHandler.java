73d94615b7b04b51553ff1fb394f65aeaec4a66d
Darin Wright <darin>
2002-10-04 21:28:38 +0000

    bug 24367

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/WorkspaceRunnableStatusHandler.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/WorkspaceRunnableStatusHandler.java
@@ -1,21 +1,14 @@
 package org.eclipse.jdt.internal.debug.ui;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.Iterator;
-import java.util.Set;
 
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRunnable;
-import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.debug.core.IStatusHandler;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.actions.WorkspaceModifyOperation;
 
@@ -37,28 +30,33 @@ public class WorkspaceRunnableStatusHandler implements IStatusHandler {
 		final IWorkspaceRunnable runnable = (IWorkspaceRunnable) source;
 		
 		// Construct a progress monitor dialog and use it to run the runnable
-		ProgressMonitorDialog dialog= new ProgressMonitorDialog(getShell());						
-		try {
-			dialog.run(true, true, new WorkspaceModifyOperation() {
-				public void execute(IProgressMonitor monitor) throws InvocationTargetException{
-					try {
-						runnable.run(monitor);
-					} catch (CoreException ce) {
-						throw new InvocationTargetException(ce);
-					}
+		Runnable r = new Runnable() {
+			public void run() {
+				ProgressMonitorDialog dialog= new ProgressMonitorDialog(getShell());						
+				try {
+					dialog.run(true, true, new WorkspaceModifyOperation() {
+						public void execute(IProgressMonitor monitor) throws InvocationTargetException{
+							try {
+								runnable.run(monitor);
+							} catch (CoreException ce) {
+								throw new InvocationTargetException(ce);
+							}
+						}
+					});
+				} catch (InterruptedException ie) {
+					// operation canceled by user
+				} catch (InvocationTargetException ite) {
+					ExceptionHandler.handle(ite, getShell(), LauncherMessages.getString("VMPreferencePage.Installed_JREs_1"), LauncherMessages.getString("VMPreferencePage.Build_failed._1")); //$NON-NLS-1$ //$NON-NLS-2$
 				}
-			});
-		} catch (InterruptedException ie) {
-			// operation canceled by user
-		} catch (InvocationTargetException ite) {
-			ExceptionHandler.handle(ite, getShell(), LauncherMessages.getString("VMPreferencePage.Installed_JREs_1"), LauncherMessages.getString("VMPreferencePage.Build_failed._1")); //$NON-NLS-1$ //$NON-NLS-2$
-		}
+			}
+		};
+		JDIDebugUIPlugin.getStandardDisplay().syncExec(r);
 		
 		return null;
 	}
 	
 	private Shell getShell() {
-		return Display.getCurrent().getActiveShell();	
+		return JDIDebugUIPlugin.getActiveWorkbenchShell();
 	}
 
 }
