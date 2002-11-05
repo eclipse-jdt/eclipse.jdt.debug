316b02c2c8c7fe7d08473cbee204730de05d0989
Darin Wright <darin>
2002-11-05 21:45:40 +0000

    bug 25746

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/AbstractVMRunner.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/AbstractVMRunner.java
@@ -6,16 +6,11 @@ package org.eclipse.jdt.launching;
  */
 
 import java.io.File;
-import java.io.IOException;
-import java.text.MessageFormat;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.DebugPlugin;
-import org.eclipse.debug.core.IStatusHandler;
-import org.eclipse.jdt.internal.launching.LaunchingMessages;
-import org.eclipse.jdt.internal.launching.LaunchingPlugin;
 
 /**
  * Abstract implementation of a VM runner.
@@ -49,47 +44,10 @@ public abstract class AbstractVMRunner implements IVMRunner {
 	protected abstract String getPluginIdentifier();
 	
 	/**
-	 * Performs a runtime exec on the given command line in the context
-	 * of the specified working directory, and returns
-	 * the resulting process. If the current runtime does not support the
-	 * specification of a working directory, the status handler for error code
-	 * <code>ERR_WORKING_DIRECTORY_NOT_SUPPORTED</code> is queried to see if the
-	 * exec should be re-executed without specifying a working directory.
-	 * 
-	 * @param cmdLine the command line
-	 * @param workingDirectory the working directory, or <code>null</code>
-	 * @return the resulting process or <code>null</code> if the exec is
-	 *  cancelled
-	 * @see Runtime
+	 * @see DebugPlugin#exec(String[], File)
 	 */
 	protected Process exec(String[] cmdLine, File workingDirectory) throws CoreException {
-		Process p= null;
-		try {
-
-			if (workingDirectory == null) {
-				p= Runtime.getRuntime().exec(cmdLine, null);
-			} else {
-				p= Runtime.getRuntime().exec(cmdLine, null, workingDirectory);
-			}
-		} catch (IOException e) {
-				if (p != null) {
-					p.destroy();
-				}
-				abort(MessageFormat.format(LaunchingMessages.getString("AbstractVMRunner.Exception_starting_process_1"), new String[]{getCmdLineAsString(cmdLine)}), e, IJavaLaunchConfigurationConstants.ERR_INTERNAL_ERROR); //$NON-NLS-1$
-		} catch (NoSuchMethodError e) {
-			//attempting launches on 1.2.* - no ability to set working directory
-			
-			IStatus status = new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IJavaLaunchConfigurationConstants.ERR_WORKING_DIRECTORY_NOT_SUPPORTED, LaunchingMessages.getString("AbstractVMRunner.Eclipse_runtime_does_not_support_working_directory_2"), e); //$NON-NLS-1$
-			IStatusHandler handler = DebugPlugin.getDefault().getStatusHandler(status);
-			
-			if (handler != null) {
-				Object result = handler.handleStatus(status, this);
-				if (result instanceof Boolean && ((Boolean)result).booleanValue()) {
-					p= exec(cmdLine, null);
-				}
-			}
-		}
-		return p;
+		return DebugPlugin.exec(cmdLine, workingDirectory);
 	}	
 	
 	protected String getCmdLineAsString(String[] cmdLine) {
