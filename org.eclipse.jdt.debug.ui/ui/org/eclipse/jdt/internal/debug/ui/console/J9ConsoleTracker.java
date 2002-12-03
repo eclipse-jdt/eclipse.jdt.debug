b52e1cf37be0ab982ed34c1db7196eb3df8924bc
Darin Wright <darin>
2002-12-03 20:20:10 +0000

    bug 24826 - support for J9 stack trace hyperlinks

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/console/J9ConsoleTracker.java
@@ -0,0 +1,68 @@
+package org.eclipse.jdt.internal.debug.ui.console;
+
+/**********************************************************************
+Copyright (c) 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
+import org.eclipse.debug.ui.console.IConsole;
+import org.eclipse.debug.ui.console.IConsoleHyperlink;
+import org.eclipse.debug.ui.console.IConsoleLineTracker;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IRegion;
+
+/**
+ * Provides links for stack traces in J9 output
+ */
+public class J9ConsoleTracker implements IConsoleLineTracker {
+	
+	/**
+	 * The console associated with this line tracker 
+	 */
+	private IConsole fConsole;
+	
+	private StringMatcher fJ9Matcher;
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#init(org.eclipse.debug.ui.console.IConsole)
+	 */
+	public void init(IConsole console) {
+		fConsole = console;
+		fJ9Matcher = new StringMatcher("*.*(*)*", false, false);
+	}
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#lineAppended(org.eclipse.jface.text.IRegion)
+	 */
+	public void lineAppended(IRegion line) {
+		try {
+			int offset = line.getOffset();
+			int length = line.getLength();
+			String text = fConsole.getDocument().get(offset, length);
+			int index = -1;
+			if (fJ9Matcher.match(text)) {
+				// find the last space in the line
+				index = text.lastIndexOf(' ');
+				if (index >= 0) {
+					int linkOffset = offset + index + 1;
+					int linkLength = length - index - 1;
+					IConsoleHyperlink link = null;
+					link = new J9StackTraceHyperlink(fConsole, linkOffset, linkLength);
+					fConsole.addLink(link);
+				}				
+			}
+		} catch (BadLocationException e) {
+		}
+	}
+
+	/**
+	 * @see org.eclipse.debug.ui.console.IConsoleLineTracker#dispose()
+	 */
+	public void dispose() {
+		fConsole = null;
+		fJ9Matcher = null;
+	}
+
+}
---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/console/J9StackTraceHyperlink.java
@@ -0,0 +1,56 @@
+package org.eclipse.jdt.internal.debug.ui.console;
+
+/**********************************************************************
+Copyright (c) 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.ui.console.IConsole;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+
+/**
+ * A hyperlink from a J9 stack trace line of the form "*.*(*)*"
+ */
+public class J9StackTraceHyperlink extends JavaStackTraceHyperlink {
+
+	/**
+     * @see JavaStackTraceHyperlink#JavaStackTraceHyperlink(IConsole, int, int)
+	 */
+	public J9StackTraceHyperlink(IConsole console, int offset, int length) {
+		super(console, offset, length);
+	}
+
+	/**
+	 * @see JavaStackTraceHyperlink#getTypeName()
+	 */
+	protected String getTypeName() throws CoreException {
+		String linkText = getLinkText();
+		int index = linkText.lastIndexOf('(');
+		if (index >= 0) {
+			String typeName = linkText.substring(0, index);
+			// remove the method name
+			index = typeName.lastIndexOf('.');
+			if (index >= 0) {
+				typeName = typeName.substring(0, index);
+			}
+			// replace slashes with dots
+			return typeName.replace('/', '.');
+		} else {
+			IStatus status = new Status(IStatus.ERROR, JDIDebugUIPlugin.getUniqueIdentifier(), 0, "Unable to parse type name from hyperlink.", null);
+			throw new CoreException(status);
+		}
+	}
+	
+	/**
+	 * @see org.eclipse.jdt.internal.debug.ui.console.JavaStackTraceHyperlink#getLineNumber()
+	 */
+	protected int getLineNumber() throws CoreException {
+		return -1;
+	}
+
+}
