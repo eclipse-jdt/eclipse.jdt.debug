8e2d43c162c8dd387490eff48a6ccb19844aef61
aweinand <>
2003-01-09 17:43:33 +0000

    launch wrapping only done for SWT apps automatic detection of all MacOS X JVMs

--- org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXDebugVMRunner.java
+++ org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXDebugVMRunner.java
@@ -18,17 +18,6 @@ public class MacOSXDebugVMRunner extends StandardVMDebugger {
 	}
 	
 	protected Process exec(String[] cmdLine, File workingDirectory) throws CoreException {
-		
-		String[] cmdLine2= new String[cmdLine.length + 2];
-		
-		String wrapper= MacOSXLaunchingPlugin.createWrapper(getClass(), "start_carbon.sh");	//$NON-NLS-1$
-		
-		int j= 0;
-		cmdLine2[j++]= "/bin/sh";	//$NON-NLS-1$
-		cmdLine2[j++]= wrapper;
-		for (int i= 0; i < cmdLine.length; i++)
-			cmdLine2[j++]= cmdLine[i];
-		
-		return super.exec(cmdLine2, workingDirectory);
+		return super.exec(MacOSXLaunchingPlugin.wrap(getClass(), cmdLine), workingDirectory);
 	}
 }
--- org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXVMInstallType.java
+++ org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXVMInstallType.java
@@ -5,22 +5,48 @@
 package org.eclipse.jdt.internal.launching.macosx;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.launching.AbstractVMInstallType;
 import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jdt.launching.LibraryLocation;
 
 /**
  * This plugins into the org.eclipse.jdt.launching.vmInstallTypes extension point
  */
 public class MacOSXVMInstallType extends AbstractVMInstallType {
-
+	
+	private static final String JVM_VERSION_LOC= "/System/Library/Frameworks/JavaVM.framework/Versions/";	//$NON-NLS-1$
+	private static final String CURRENT_JVM= "CurrentJDK";	//$NON-NLS-1$
+	private static final String JVM_SUBDIR= "Home";	//$NON-NLS-1$
+	private static final String JAVADOC_LOC= "/Developer/Documentation/Java/Reference/api";	//$NON-NLS-1$
+	
+	private String fDefaultJDKID;
+	
 	public IVMInstall doCreateVMInstall(String id) {
-		return new MacOSXVMInstall(this, id);
+		MacOSXVMInstall vm= new MacOSXVMInstall(this, id);
+		String path= JVM_VERSION_LOC + id + File.separator + JVM_SUBDIR;
+		File home = new File(path);
+		vm.setInstallLocation(home);
+		String name= "JVM " + id;
+		if (id.equals(fDefaultJDKID))
+			name +=  " (MacOS X Default)";
+		vm.setName(name);
+		vm.setLibraryLocations(getDefaultLibraryLocations(home));
+		try {
+			URL doc= new URL("file", "", JAVADOC_LOC);	//$NON-NLS-1$ //$NON-NLS-2$
+			vm.setJavadocLocation(doc);
+		} catch (MalformedURLException ex) {
+		}
+		return vm;
 	}
 	
 	public String getName() {
@@ -28,25 +54,49 @@ public class MacOSXVMInstallType extends AbstractVMInstallType {
 	}
 	
 	public IStatus validateInstallLocation(File installLocation) {
+		String id= MacOSXLaunchingPlugin.getUniqueIdentifier();
 		File java= new File(installLocation, "bin"+File.separator+"java"); //$NON-NLS-2$ //$NON-NLS-1$
-		if (!java.isFile()) {
-			return new Status(IStatus.ERROR, MacOSXLaunchingPlugin.getUniqueIdentifier(), 0, MacOSXLauncherMessages.getString("MacOSXVMType.error.notRoot"), null); //$NON-NLS-1$
-		}
-		return new Status(IStatus.OK, MacOSXLaunchingPlugin.getUniqueIdentifier(), 0, "ok", null); //$NON-NLS-1$
+		if (java.isFile())
+			return new Status(IStatus.OK, id, 0, "ok", null); //$NON-NLS-1$
+		return new Status(IStatus.ERROR, id, 0, MacOSXLauncherMessages.getString("MacOSXVMType.error.notRoot"), null); //$NON-NLS-1$
 	}
 
 	/**
 	 * @see IVMInstallType#detectInstallLocation()
 	 */
 	public File detectInstallLocation() {
+		
 		if (!"Java HotSpot(TM) Client VM".equals(System.getProperty("java.vm.name"))) //$NON-NLS-2$ //$NON-NLS-1$
 			return null;
-			
-		String javaHome= System.getProperty("java.home");	//$NON-NLS-1$
-		if (javaHome != null) {
-			File home = new File(javaHome); 
-			if (home.exists())
-				return home;
+	
+		// find all installed VMs
+		File versionDir= new File(JVM_VERSION_LOC);
+		if (versionDir.exists() && versionDir.isDirectory()) {
+			File currentJDK= new File(versionDir, CURRENT_JVM);
+			try {
+				currentJDK= currentJDK.getCanonicalFile();
+			}catch (IOException ex) {
+			}
+			File[] versions= versionDir.listFiles();
+			for (int i= 0; i < versions.length; i++) {
+				String version= versions[i].getName();
+				File home=  new File(versions[i], JVM_SUBDIR);
+				if (home.exists() && findVMInstall(version) == null && !CURRENT_JVM.equals(version)) {
+					if (currentJDK.equals(versions[i])) {
+						if (fDefaultJDKID == null)
+							fDefaultJDKID= version;
+						createVMInstall(version);							
+						IVMInstall vm= findVMInstall(version);
+						if (vm != null) {
+							try {
+								JavaRuntime.setDefaultVMInstall(vm, null);
+							} catch (CoreException e) {
+							}
+						}
+					} else
+						createVMInstall(version);
+				}
+			}
 		}
 		return null;
 	}
