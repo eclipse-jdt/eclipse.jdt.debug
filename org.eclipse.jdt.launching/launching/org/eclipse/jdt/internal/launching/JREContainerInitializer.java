d69f000c034574c1c5c1f27d0fdd1c9a6144c0de
Darin Wright <darin>
2003-01-17 21:10:57 +0000

    bug 29651 - allow source attachment changes on classpath container entries

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JREContainerInitializer.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JREContainerInitializer.java
@@ -11,6 +11,7 @@ package org.eclipse.jdt.internal.launching;
  *     IBM Corporation - initial API and implementation
  ******************************************************************************/
 
+import java.io.File;
 import java.text.MessageFormat;
 
 import org.eclipse.core.runtime.CoreException;
@@ -25,9 +26,12 @@ import org.eclipse.jdt.core.IClasspathContainer;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstallType;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.LibraryLocation;
+import org.eclipse.jdt.launching.VMStandin;
 
 /**
  * Resolves a container for a JRE classpath container entry.
@@ -148,4 +152,55 @@ public class JREContainerInitializer extends ClasspathContainerInitializer {
 	public static String getVMName(IPath path) {
 		return path.removeFirstSegments(2).toString();
 	}	
+	
+	/**
+	 * The container can be updated if it refers to an existing VM.
+	 * 
+	 * @see org.eclipse.jdt.core.ClasspathContainerInitializer#canUpdateClasspathContainer(org.eclipse.core.runtime.IPath, org.eclipse.jdt.core.IJavaProject)
+	 */
+	public boolean canUpdateClasspathContainer(IPath containerPath, IJavaProject project) {
+		if (containerPath != null && containerPath.segmentCount() > 0) {
+			if (JavaRuntime.JRE_CONTAINER.equals(containerPath.segment(0))) {
+				return resolveVM(containerPath) != null;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @see org.eclipse.jdt.core.ClasspathContainerInitializer#requestClasspathContainerUpdate(org.eclipse.core.runtime.IPath, org.eclipse.jdt.core.IJavaProject, org.eclipse.jdt.core.IClasspathContainer)
+	 */
+	public void requestClasspathContainerUpdate(IPath containerPath, IJavaProject project, IClasspathContainer containerSuggestion) throws CoreException {
+		IVMInstall vm = resolveVM(containerPath);
+		if (vm == null) { 
+			IStatus status = new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IJavaLaunchConfigurationConstants.ERR_VM_INSTALL_DOES_NOT_EXIST, MessageFormat.format("JRE referenced by classpath container {0} does not exist.", new String[]{containerPath.toString()}), null);
+			throw new CoreException(status);
+		} else {
+			// update of the vm with new library locations
+			
+			IClasspathEntry[] entries = containerSuggestion.getClasspathEntries();
+			LibraryLocation[] libs = new LibraryLocation[entries.length];
+			for (int i = 0; i < entries.length; i++) {
+				IClasspathEntry entry = entries[i];
+				if (entry.getEntryKind() == IClasspathEntry.CPE_LIBRARY) {
+					IPath path = entry.getPath();
+					File lib = path.toFile();
+					if (lib.exists() && lib.isFile()) {
+						libs[i] = new LibraryLocation(path, entry.getSourceAttachmentPath(), entry.getSourceAttachmentRootPath());
+					} else {
+						IStatus status = new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IJavaLaunchConfigurationConstants.ERR_INTERNAL_ERROR, MessageFormat.format("Classpath entry {0} does not refer to an existing library.", new String[]{entry.getPath().toString()}), null);
+						throw new CoreException(status);
+					}
+				} else {
+					IStatus status = new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IJavaLaunchConfigurationConstants.ERR_INTERNAL_ERROR, MessageFormat.format("Classpath entry {0} does not refer to a library.", new String[]{entry.getPath().toString()}), null);
+					throw new CoreException(status);
+				}
+			}
+			VMStandin standin = new VMStandin(vm);
+			standin.setLibraryLocations(libs);
+			standin.convertToRealVM();
+			JavaRuntime.saveVMConfiguration();
+		}
+	}
+
 }
