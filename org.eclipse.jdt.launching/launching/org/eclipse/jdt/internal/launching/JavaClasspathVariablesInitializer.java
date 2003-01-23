e3a4c13a5455b0a724f74fb2fe55cb5f8ccdba2f
Darin Wright <darin>
2003-01-23 15:54:42 +0000

    bug 29992 - JRE_LIB not properly resolved with JRE containers

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JavaClasspathVariablesInitializer.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JavaClasspathVariablesInitializer.java
@@ -34,30 +34,52 @@ public class JavaClasspathVariablesInitializer extends ClasspathVariableInitiali
 		if (vmInstall != null) {
 			IPath newPath= null;
 			LibraryLocation[] locations= JavaRuntime.getLibraryLocations(vmInstall);
-			if (variable.equals(JavaRuntime.JRELIB_VARIABLE)) {
-				newPath= locations[0].getSystemLibraryPath();
-			} else if (variable.equals(JavaRuntime.JRESRC_VARIABLE)) {
-				newPath= locations[0].getSystemLibrarySourcePath();
-			} else if (variable.equals(JavaRuntime.JRESRCROOT_VARIABLE)){
-				newPath= locations[0].getPackageRootPath();
+			// look for rt.jar or classes.zip (both may exist, so do exhaustive search)
+			LibraryLocation rtjar = null;
+			LibraryLocation classeszip = null;
+			for (int i = 0; i < locations.length; i++) {
+				LibraryLocation location = locations[i];
+				String name = location.getSystemLibraryPath().lastSegment();
+				if (name.equalsIgnoreCase("rt.jar")) { //$NON-NLS-1$
+					rtjar = location;
+				} else if (name.equalsIgnoreCase("classes.zip")) { //$NON-NLS-1$
+					classeszip = location;
+				}
 			}
-			if (newPath == null) {
-				return;
+			// rt.jar if present, then classes.zip, else the first library
+			LibraryLocation systemLib = rtjar;
+			if (systemLib == null) {
+				systemLib = classeszip;
 			}
-			IWorkspace workspace= ResourcesPlugin.getWorkspace();
-			IWorkspaceDescription wsDescription= workspace.getDescription();				
-			boolean wasAutobuild= wsDescription.isAutoBuilding();
-			try {
-				setAutobuild(workspace, false);
-				setJREVariable(newPath, variable);	
-			} catch (CoreException ce) {
-				LaunchingPlugin.log(ce);
-				return;
-			} finally {
+			if (systemLib == null && locations.length > 0) {
+				systemLib = locations[0];
+			}
+			if (systemLib != null) {
+				if (variable.equals(JavaRuntime.JRELIB_VARIABLE)) {
+					newPath= systemLib.getSystemLibraryPath();
+				} else if (variable.equals(JavaRuntime.JRESRC_VARIABLE)) {
+					newPath= systemLib.getSystemLibrarySourcePath();
+				} else if (variable.equals(JavaRuntime.JRESRCROOT_VARIABLE)){
+					newPath= systemLib.getPackageRootPath();
+				}
+				if (newPath == null) {
+					return;
+				}
+				IWorkspace workspace= ResourcesPlugin.getWorkspace();
+				IWorkspaceDescription wsDescription= workspace.getDescription();				
+				boolean wasAutobuild= wsDescription.isAutoBuilding();
 				try {
-					setAutobuild(workspace, wasAutobuild);
+					setAutobuild(workspace, false);
+					setJREVariable(newPath, variable);	
 				} catch (CoreException ce) {
 					LaunchingPlugin.log(ce);
+					return;
+				} finally {
+					try {
+						setAutobuild(workspace, wasAutobuild);
+					} catch (CoreException ce) {
+						LaunchingPlugin.log(ce);
+					}
 				}
 			}
 		}
