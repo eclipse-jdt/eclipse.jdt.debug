b2bdb030eb4115c89d659485685a1d29e5627502
Luc Bourlier <lbourlier>
2003-02-04 02:00:41 +0000

    Bug 28704 - Option to add variable to watch list should appear in context menu

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/WatchAction.java
@@ -0,0 +1,58 @@
+package org.eclipse.jdt.internal.debug.ui.actions;
+
+/**********************************************************************
+Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+This file is made available under the terms of the Common Public License v1.0
+which accompanies this distribution, and is available at
+http://www.eclipse.org/legal/cpl-v10.html
+**********************************************************************/
+
+import java.util.Iterator;
+
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.model.IStackFrame;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jdt.debug.core.IJavaStackFrame;
+import org.eclipse.jdt.debug.core.IJavaVariable;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jdt.internal.debug.ui.JavaWatchExpression;
+import org.eclipse.jface.viewers.IStructuredSelection;
+
+/**
+ * Create a watch item from a selected variable
+ */
+public class WatchAction extends InspectAction {
+		
+	public void run() {
+		Object selectedObject= getSelectedObject();
+		if (selectedObject instanceof IStructuredSelection) {
+			IStructuredSelection selection = (IStructuredSelection)selectedObject;
+			Iterator elements = selection.iterator();
+			while (elements.hasNext()) {
+				try {
+					createWatchExpression(((IJavaVariable)elements.next()).getName());
+				} catch (DebugException e) {
+					JDIDebugUIPlugin.log(e);
+					return;
+				}
+			}
+			if (getTargetPart().getSite().getId().equals(IDebugUIConstants.ID_EXPRESSION_VIEW)) {
+				return;
+			}
+			showExpressionView();
+		} else if (selectedObject instanceof String) {
+			createWatchExpression((String) selectedObject);
+		}
+	}
+
+	private void createWatchExpression(String snippet) {
+		JavaWatchExpression expression = new JavaWatchExpression(snippet);
+		IStackFrame frame = getStackFrameContext();
+		if (frame instanceof IJavaStackFrame) {
+			expression.evaluateExpression((IJavaStackFrame)frame);
+		}
+		DebugPlugin.getDefault().getExpressionManager().addExpression(expression);				
+	}
+
+}
