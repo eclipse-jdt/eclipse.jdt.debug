4140f6d432c3374c7d15aed379509e8eaff13bc9
aweinand <>
2003-04-12 00:26:55 +0000

    work on #23839

--- org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXLaunchingPlugin.java
+++ org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXLaunchingPlugin.java
@@ -64,33 +64,142 @@ public class MacOSXLaunchingPlugin extends Plugin {
 		for (int i= 0; i < cmdLine.length; i++) {
 			String arg= cmdLine[i];
 			if (arg.indexOf("swt.jar") >= 0 || arg.indexOf("org.eclipse.swt") >= 0 || "-ws".equals(arg)) {	//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-				String[] cmdLine2= new String[cmdLine.length + 2];
-				
-				String wrapper= "/tmp/start_carbon.sh";	//$NON-NLS-1$
-				copy(clazz, "start_carbon.sh", wrapper);	//$NON-NLS-1$
-				copy(clazz, "JavaApplicationStub", "/tmp/JavaApplicationStub");	//$NON-NLS-1$ //$NON-NLS-2$
-				
-				int j= 0;
-				cmdLine2[j++]= "/bin/sh";	//$NON-NLS-1$
-				cmdLine2[j++]= wrapper;
-				for (i= 0; i < cmdLine.length; i++)
-					cmdLine2[j++]= cmdLine[i];
-		
-				return cmdLine2;
+				try {
+					return new String[] { createBundle(clazz, cmdLine) };
+				} catch (IOException e) {
+					e.printStackTrace();
+				}		
 			}
 		}
 		return cmdLine;
 	}
+	
+	static String createBundle(Class clazz, String[] cmdLine) throws IOException {
 		
-	static void copy(Class where, String from, String to) {
+		String javaApplicationStub= "JavaApplicationStub"; //$NON-NLS-1$
+		String version= "2.1"; //$NON-NLS-1$
+		String signature= "????"; //$NON-NLS-1$
 		
-		String output= to;
-		FileOutputStream os= null;
-		try {
-			os= new FileOutputStream(output);
-		} catch (FileNotFoundException ex) {
-			return;
+		String jvmVersion= "1.3.1"; //$NON-NLS-1$
+		String vm= cmdLine[0];
+		if (vm.indexOf("1.4.1") >= 0) //$NON-NLS-1$
+			jvmVersion= "1.4.1"; //$NON-NLS-1$
+
+		String jvmOptions= ""; //$NON-NLS-1$
+		String classPath= null;
+		String mainClass= null;
+		String arguments= ""; //$NON-NLS-1$
+		
+		String workingDir= System.getProperty("user.dir"); //$NON-NLS-1$
+		if (workingDir == null)
+			workingDir= new File(".").getAbsolutePath(); //$NON-NLS-1$
+			
+		int i= 1;
+		
+		for (; i < cmdLine.length; i++) {
+			String arg= cmdLine[i];
+			
+			if ("-classpath".equals(arg) || "-cp".equals(arg)) { //$NON-NLS-1$ //$NON-NLS-2$
+				classPath= cmdLine[++i];
+				continue;
+			}
+			
+			if (arg.charAt(0) == '-') {
+				jvmOptions+= "    <string>"+arg+"</string>\n"; //$NON-NLS-1$ //$NON-NLS-2$
+				continue;
+			}
+			
+			// main class
+			mainClass= arg;
+			i++;
+			break;
 		}
+		
+		for (; i < cmdLine.length; i++)
+			arguments+= "    <string>"+cmdLine[i]+"</string>\n"; //$NON-NLS-1$ //$NON-NLS-2$
+
+		int pos= mainClass.lastIndexOf('.');
+		String appName= mainClass.substring(pos+1);
+		
+		File tmp_dir= new File("/tmp"); //$NON-NLS-1$
+		File swt_dir= createDir(tmp_dir, "swt_stubs", false); //$NON-NLS-1$
+		File app_dir= createDir(swt_dir, appName + ".app", true); //$NON-NLS-1$
+		File contents_dir= createDir(app_dir, "Contents", false); //$NON-NLS-1$
+		File macos_dir= createDir(contents_dir, "MacOS", false); //$NON-NLS-1$
+		
+		// JavaApplicationBundle
+		File stub= new File(macos_dir, javaApplicationStub);
+		copyFile(clazz, javaApplicationStub, stub);
+		Runtime.getRuntime().exec(new String[] { "/bin/chmod", "+x", stub.getAbsolutePath() }); //$NON-NLS-1$ //$NON-NLS-2$
+		
+		// Info.plist
+		File info= new File(contents_dir, "Info.plist"); //$NON-NLS-1$
+		FileOutputStream fos= new FileOutputStream(info);
+		OutputStreamWriter ow= new OutputStreamWriter(fos, "UTF8"); //$NON-NLS-1$
+		PrintWriter w= new PrintWriter(ow);
+		w.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"); //$NON-NLS-1$
+		w.println("<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"); //$NON-NLS-1$
+		w.println("<plist version=\"1.0\">"); //$NON-NLS-1$
+		w.println(" <dict>"); //$NON-NLS-1$
+		w.println("  <key>CFBundleDevelopmentRegion</key><string>English</string>"); //$NON-NLS-1$
+		w.println("  <key>CFBundleExecutable</key><string>"+javaApplicationStub+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		w.println("  <key>CFBundleGetInfoString</key><string>"+appName+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		w.println("  <key>CFBundleInfoDictionaryVersion</key><string>6.0</string>"); //$NON-NLS-1$
+		w.println("  <key>CFBundleName</key><string>"+appName+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		w.println("  <key>CFBundlePackageType</key><string>APPL</string>"); //$NON-NLS-1$
+		w.println("  <key>CFBundleShortVersionString</key><string>"+version+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		w.println("  <key>CFBundleSignature</key><string>"+signature+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		w.println("  <key>CFBundleVersion</key><string>1.0.1</string>"); //$NON-NLS-1$
+		w.println("  <key>Java</key><dict>"); //$NON-NLS-1$
+		w.println("   <key>JVMVersion</key><string>"+jvmVersion+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (jvmOptions.length() > 0) {
+			w.println("   <key>VMOptions</key><array>"); //$NON-NLS-1$
+			w.print(jvmOptions);
+			w.println("   </array>"); //$NON-NLS-1$
+		}
+		if (classPath != null)
+			w.println("   <key>ClassPath</key><string>"+classPath+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (mainClass != null)
+			w.println("   <key>MainClass</key><string>"+mainClass+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (workingDir != null)
+			w.println("   <key>WorkingDirectory</key><string>"+workingDir+"</string>"); //$NON-NLS-1$ //$NON-NLS-2$
+		if (arguments.length() > 0) {
+			w.println("   <key>Arguments</key><array>"); //$NON-NLS-1$
+			w.print(arguments);
+			w.println("   </array>"); //$NON-NLS-1$
+		}
+		w.println("  </dict>"); //$NON-NLS-1$
+		w.println(" </dict>"); //$NON-NLS-1$
+		w.println("</plist>"); //$NON-NLS-1$
+		w.close();
+		
+		return stub.getAbsolutePath();
+	}
+	
+	static void deleteDir(File dir) {
+		File[] files= dir.listFiles();
+		if (files != null) {
+			for (int i= 0; i < files.length; i++)
+				deleteDir(files[i]);
+		}
+		dir.delete();
+	}
+	
+	static File createDir(File parent_dir, String dir_name, boolean remove) throws IOException {
+		File dir= new File(parent_dir, dir_name);
+		if (dir.exists()) {
+			if (!remove)
+				return dir;
+			deleteDir(dir);
+		}
+		if (! dir.mkdir())
+			throw new IOException("cannot create dir " + dir_name); //$NON-NLS-1$
+		return dir;
+	}
+	
+	static void copyFile(Class where, String from, File to) throws IOException {
+		
+		FileOutputStream os= new FileOutputStream(to);
 
 		InputStream is= null;
 		try {
@@ -104,8 +213,6 @@ public class MacOSXLaunchingPlugin extends Plugin {
 				}
 			}
 			os.flush();
-		} catch (IOException io) {
-			return;
 		} finally {
 			if (is != null) {
 				try {
