ae1dff47c5398e28bdd51645b93638d92c2b3c02
Darin Wright <darin>
2003-04-22 14:52:10 +0000

    prevent tests from haning UI when error dialog opens

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
@@ -51,6 +51,7 @@ import org.eclipse.jdt.debug.testplugin.DebugElementKindEventWaiter;
 import org.eclipse.jdt.debug.testplugin.DebugEventWaiter;
 import org.eclipse.jdt.internal.debug.ui.IJDIPreferencesConstants;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.dialogs.ErrorDialog;
 
 
  
@@ -73,6 +74,8 @@ public abstract class AbstractDebugTest extends TestCase implements  IEvaluation
 	
 	public AbstractDebugTest(String name) {
 		super(name);
+		// set error dialog to non-blocking to avoid hanging the UI during test
+		ErrorDialog.AUTOMATED_MODE = true;
 	}
 	
 	/**
