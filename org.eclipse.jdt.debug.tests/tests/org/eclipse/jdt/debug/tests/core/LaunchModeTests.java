66d0a8d30a16169dad4bd79e8c4f55bfea60550c
Darin Wright <darin>
2003-05-15 17:26:29 +0000

    bug 34514 - Define New Launch Mode other than Run or Debug

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/LaunchModeTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/LaunchModeTests.java
@@ -15,9 +15,24 @@ import junit.framework.AssertionFailedError;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationType;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.debug.internal.ui.launchConfigurations.LaunchConfigurationPresentationManager;
+import org.eclipse.debug.internal.ui.launchConfigurations.LaunchConfigurationsDialog;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
 import org.eclipse.jdt.debug.testplugin.TestModeLaunchDelegate;
 import org.eclipse.jdt.debug.tests.AbstractDebugTest;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaJRETab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaMainTab;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaSourceLookupTab;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 
 /**
  * LaunchModeTests
@@ -111,5 +126,36 @@ public class LaunchModeTests extends AbstractDebugTest {
 		
 		assertTrue("Java application configuration should support run mode", configuration.supportsMode(ILaunchManager.RUN_MODE));
 		assertTrue("Java application type should support run mode", configuration.getType().supportsMode(ILaunchManager.RUN_MODE));		
+	}
+	
+	/**
+	 * Tests that mode specific tab group contributions work.
+	 */
+	public void testModeSpecificTabGroups() throws CoreException {
+		ILaunchConfigurationType javaType = getLaunchManager().getLaunchConfigurationType(IJavaLaunchConfigurationConstants.ID_JAVA_APPLICATION); 
+		ILaunchConfigurationTabGroup standardGroup = LaunchConfigurationPresentationManager.getDefault().getTabGroup(javaType, ILaunchManager.DEBUG_MODE);
+		ILaunchConfigurationTabGroup testGroup = LaunchConfigurationPresentationManager.getDefault().getTabGroup(javaType, "TEST_MODE");
+		ILaunchConfigurationDialog dialog = new LaunchConfigurationsDialog(DebugUIPlugin.getShell(), DebugUIPlugin.getDefault().getLaunchConfigurationManager().getLaunchGroup(IDebugUIConstants.ID_DEBUG_LAUNCH_GROUP));
+		standardGroup.createTabs(dialog, ILaunchManager.DEBUG_MODE);
+		testGroup.createTabs(dialog, "TEST_MODE");
+		
+		ILaunchConfigurationTab[] tabs = standardGroup.getTabs();
+		assertEquals("Wrong number of tabs in the standard group", 6, tabs.length);
+		assertTrue("Tab 0 should be 'Main'", tabs[0] instanceof JavaMainTab);
+		assertTrue("Tab 1 should be 'Arguments'", tabs[1] instanceof JavaArgumentsTab);
+		assertTrue("Tab 2 should be 'JRE'", tabs[2] instanceof JavaJRETab);
+		assertTrue("Tab 3 should be 'Classpath'", tabs[3] instanceof JavaClasspathTab);
+		assertTrue("Tab 4 should be 'Sourcepath'", tabs[4] instanceof JavaSourceLookupTab);
+		assertTrue("Tab 5 should be 'Common'", tabs[5] instanceof CommonTab);
+		
+		tabs = testGroup.getTabs();
+		assertEquals("Wrong number of tabs in the test group", 4, tabs.length);
+		assertTrue("Tab 0 should be 'Main'", tabs[0] instanceof JavaMainTab);
+		assertTrue("Tab 1 should be 'Arguments'", tabs[1] instanceof JavaArgumentsTab);
+		assertTrue("Tab 2 should be 'JRE'", tabs[2] instanceof JavaJRETab);
+		assertTrue("Tab 3 should be 'Classpath'", tabs[3] instanceof JavaClasspathTab);
+		
+		standardGroup.dispose();
+		testGroup.dispose();
 	}
 }
