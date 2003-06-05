d8a090cd471b1fd1a022b13680e848019931564b
Darin Swanson <darins>
2003-06-05 18:01:33 +0000

    Bug 38531 - IllegalArgumentException "Source attachment path should be absolute"

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/VMDefinitionsContainer.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/VMDefinitionsContainer.java
@@ -502,7 +502,13 @@ public class VMDefinitionsContainer {
 		String jreSrc= libLocationElement.getAttribute("jreSrc"); //$NON-NLS-1$
 		String pkgRoot= libLocationElement.getAttribute("pkgRoot"); //$NON-NLS-1$
 		if (jreJar != null && jreSrc != null && pkgRoot != null) {
-			return new LibraryLocation(new Path(jreJar), new Path(jreSrc), new Path(pkgRoot));
+			Path sourcePath;
+			if (jreSrc.length() == 0) {
+				sourcePath= Path.ROOT;
+			} else  {
+				sourcePath= new Path(jreSrc);
+			}
+			return new LibraryLocation(new Path(jreJar), sourcePath, new Path(pkgRoot));
 		} else {
 			LaunchingPlugin.log(LaunchingMessages.getString("JavaRuntime.Library_location_element_incorrectly_specified_3")); //$NON-NLS-1$
 		}
