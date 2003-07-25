43c47670d3aeedbaac6faf0662540547347883fa
Luc Bourlier <lbourlier>
2003-07-25 17:05:13 +0000

    Removed the extra tab before the copyright notice

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/SocketUtil.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/SocketUtil.java
@@ -1,4 +1,4 @@
-	/*******************************************************************************
+/*******************************************************************************
  * Copyright (c) 2000, 2003 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
