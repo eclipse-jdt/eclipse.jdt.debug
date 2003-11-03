c15899047c9caec77ea737e36f3181dd15f17b47
Darin Swanson <darins>
2003-11-03 21:15:05 +0000

    TODOs and spelling and formatting

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/RuntimeClasspathEntryLabelProvider.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/RuntimeClasspathEntryLabelProvider.java
@@ -41,154 +41,152 @@ import org.eclipse.ui.model.WorkbenchLabelProvider;
  */
 public class RuntimeClasspathEntryLabelProvider extends LabelProvider {
 		
-		private WorkbenchLabelProvider lp = new WorkbenchLabelProvider();
-		
-		/**
-		 * Context in which to render containers, or <code>null</code>
-		 */
-		private ILaunchConfiguration fLaunchConfuration;
-		
-		/**
-		 * @see ILabelProvider#getImage(Object)
-		 */
-		public Image getImage(Object element) {
-			IRuntimeClasspathEntry entry = (IRuntimeClasspathEntry)element;
-			switch (entry.getType()) {
-				case IRuntimeClasspathEntry.PROJECT:
-					// XXX: what if project not loaded?
-					IResource res = entry.getResource();
-					IJavaElement proj = JavaCore.create(res);
-					return lp.getImage(proj);
-				case IRuntimeClasspathEntry.ARCHIVE:
-					// XXX: illegal access to images
-					res = entry.getResource();
-					if (res instanceof IContainer) {
-						return lp.getImage(res);
-					}
-					boolean external = res == null;
-					boolean source = (entry.getSourceAttachmentPath() != null && !Path.EMPTY.equals(entry.getSourceAttachmentPath()));
-					String key = null;
-					if (external) {
-						if (source) {
-							key = JavaPluginImages.IMG_OBJS_EXTJAR_WSRC;
-						} else {
-							key = JavaPluginImages.IMG_OBJS_EXTJAR;
-						}	
+	private WorkbenchLabelProvider lp = new WorkbenchLabelProvider();
+	
+	/**
+	 * Context in which to render containers, or <code>null</code>
+	 */
+	private ILaunchConfiguration fLaunchConfiguration;
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ILabelProvider#getImage(java.lang.Object)
+	 */
+	public Image getImage(Object element) {
+		IRuntimeClasspathEntry entry = (IRuntimeClasspathEntry)element;
+		switch (entry.getType()) {
+			case IRuntimeClasspathEntry.PROJECT:
+				//TODO what if project not loaded?
+				IResource res = entry.getResource();
+				IJavaElement proj = JavaCore.create(res);
+				return lp.getImage(proj);
+			case IRuntimeClasspathEntry.ARCHIVE:
+				//TODO illegal access to images
+				res = entry.getResource();
+				if (res instanceof IContainer) {
+					return lp.getImage(res);
+				}
+				boolean external = res == null;
+				boolean source = (entry.getSourceAttachmentPath() != null && !Path.EMPTY.equals(entry.getSourceAttachmentPath()));
+				String key = null;
+				if (external) {
+					if (source) {
+						key = JavaPluginImages.IMG_OBJS_EXTJAR_WSRC;
 					} else {
-						if (source) {
-							key = JavaPluginImages.IMG_OBJS_JAR_WSRC;
-						} else {
-							key = JavaPluginImages.IMG_OBJS_JAR;
-						}
+						key = JavaPluginImages.IMG_OBJS_EXTJAR;
+					}	
+				} else {
+					if (source) {
+						key = JavaPluginImages.IMG_OBJS_JAR_WSRC;
+					} else {
+						key = JavaPluginImages.IMG_OBJS_JAR;
 					}
-					return JavaPluginImages.get(key);
-				case IRuntimeClasspathEntry.VARIABLE:
-					// XXX: illegal internal access
-					return JavaPluginImages.get(JavaPluginImages.IMG_OBJS_ENV_VAR);				
-				case IRuntimeClasspathEntry.CONTAINER:
-					// XXX: illegal internal access
-					return JavaPluginImages.get(JavaPluginImages.IMG_OBJS_LIBRARY);
-			}	
-			return null;
-		}
+				}
+				return JavaPluginImages.get(key);
+			case IRuntimeClasspathEntry.VARIABLE:
+				//TODO: illegal internal access
+				return JavaPluginImages.get(JavaPluginImages.IMG_OBJS_ENV_VAR);				
+			case IRuntimeClasspathEntry.CONTAINER:
+				// TODO: illegal internal access
+				return JavaPluginImages.get(JavaPluginImages.IMG_OBJS_LIBRARY);
+		}	
+		return null;
+	}
 
-		/**
-		 * @see ILabelProvider#getText(Object)
-		 */
-		public String getText(Object element) {
-			IRuntimeClasspathEntry entry = (IRuntimeClasspathEntry)element;
-			switch (entry.getType()) {
-				case IRuntimeClasspathEntry.PROJECT:
-					IResource res = entry.getResource();
-					IJavaElement proj = JavaCore.create(res);
-					return lp.getText(proj);
-				case IRuntimeClasspathEntry.ARCHIVE:
-					res = entry.getResource();
-					if (res == null) {
-						return entry.getPath().toString();
-					} else {
-						return lp.getText(res);				
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.ILabelProvider#getText(java.lang.Object)
+	 */
+	public String getText(Object element) {
+		IRuntimeClasspathEntry entry = (IRuntimeClasspathEntry)element;
+		switch (entry.getType()) {
+			case IRuntimeClasspathEntry.PROJECT:
+				IResource res = entry.getResource();
+				IJavaElement proj = JavaCore.create(res);
+				return lp.getText(proj);
+			case IRuntimeClasspathEntry.ARCHIVE:
+				res = entry.getResource();
+				if (res == null) {
+					return entry.getPath().toString();
+				} else {
+					return lp.getText(res);				
+				}
+			case IRuntimeClasspathEntry.VARIABLE:
+				IPath path = entry.getPath();
+				IPath srcPath = entry.getSourceAttachmentPath();
+				StringBuffer buf = new StringBuffer(path.toString());
+				if (srcPath != null) {
+					buf.append(" ["); //$NON-NLS-1$
+					buf.append(srcPath.toString());
+					IPath rootPath = entry.getSourceAttachmentRootPath();
+					if (rootPath != null) {
+						buf.append(IPath.SEPARATOR);
+						buf.append(rootPath.toString());
 					}
-				case IRuntimeClasspathEntry.VARIABLE:
-					IPath path = entry.getPath();
-					IPath srcPath = entry.getSourceAttachmentPath();
-					StringBuffer buf = new StringBuffer(path.toString());
-					if (srcPath != null) {
-						buf.append(" ["); //$NON-NLS-1$
-						buf.append(srcPath.toString());
-						IPath rootPath = entry.getSourceAttachmentRootPath();
-						if (rootPath != null) {
-							buf.append(IPath.SEPARATOR);
-							buf.append(rootPath.toString());
+					buf.append(']'); //$NON-NLS-1$
+				}
+				// append JRE name if we can compute it
+				if (path.equals(new Path(JavaRuntime.JRELIB_VARIABLE)) && fLaunchConfiguration != null) {
+					try {
+						IVMInstall vm = JavaRuntime.computeVMInstall(fLaunchConfiguration);					
+						buf.append(" - "); //$NON-NLS-1$
+						buf.append(vm.getName());
+					} catch (CoreException e) {
+						JDIDebugUIPlugin.log(e);
+					}
+				}
+				return buf.toString();
+			case IRuntimeClasspathEntry.CONTAINER:
+				path = entry.getPath();
+				if (fLaunchConfiguration != null) {
+					try {
+						if (path.equals(new Path(JavaRuntime.JRE_CONTAINER))) {
+							// default JRE - resolve the name for the launch config, rather than using the "workspace" default description
+							IVMInstall vm = JavaRuntime.computeVMInstall(fLaunchConfiguration);
+							return MessageFormat.format(LauncherMessages.getString("RuntimeClasspathEntryLabelProvider.JRE_System_Library_[{0}]_2"), new String[]{vm.getName()}); //$NON-NLS-1$
 						}
-						buf.append(']'); //$NON-NLS-1$
-					}
-					// append JRE name if we can compute it
-					if (path.equals(new Path(JavaRuntime.JRELIB_VARIABLE)) && fLaunchConfuration != null) {
+						IJavaProject project = null;
 						try {
-							IVMInstall vm = JavaRuntime.computeVMInstall(fLaunchConfuration);					
-							buf.append(" - "); //$NON-NLS-1$
-							buf.append(vm.getName());
+							project = JavaRuntime.getJavaProject(fLaunchConfiguration);
 						} catch (CoreException e) {
-							JDIDebugUIPlugin.log(e);
+							if (e.getStatus().getCode() != IJavaLaunchConfigurationConstants.ERR_NOT_A_JAVA_PROJECT) {
+								throw e;
+							}
 						}
-					}
-					return buf.toString();
-				case IRuntimeClasspathEntry.CONTAINER:
-					path = entry.getPath();
-					if (fLaunchConfuration != null) {
-						try {
-							if (path.equals(new Path(JavaRuntime.JRE_CONTAINER))) {
-								// default JRE - resolve the name for the launch config, rather than using the "workspace" default description
-								IVMInstall vm = JavaRuntime.computeVMInstall(fLaunchConfuration);
-								return MessageFormat.format(LauncherMessages.getString("RuntimeClasspathEntryLabelProvider.JRE_System_Library_[{0}]_2"), new String[]{vm.getName()}); //$NON-NLS-1$
-							}
-							IJavaProject project = null;
-							try {
-								project = JavaRuntime.getJavaProject(fLaunchConfuration);
-							} catch (CoreException e) {
-								if (e.getStatus().getCode() != IJavaLaunchConfigurationConstants.ERR_NOT_A_JAVA_PROJECT) {
-									throw e;
-								}
-							}
-							if (project == null) {
-								if (path.segmentCount() > 0 && path.segment(0).equals(JavaRuntime.JRE_CONTAINER)) {
-									IVMInstall vm = JREContainerInitializer.resolveVM(path);
-									if (vm != null) {
-										JREContainer container = new JREContainer(vm, path);
-										return container.getDescription();
-									}
-								}
-							} else {
-								IClasspathContainer container = JavaCore.getClasspathContainer(entry.getPath(), project);
-								if (container != null) {
+						if (project == null) {
+							if (path.segmentCount() > 0 && path.segment(0).equals(JavaRuntime.JRE_CONTAINER)) {
+								IVMInstall vm = JREContainerInitializer.resolveVM(path);
+								if (vm != null) {
+									JREContainer container = new JREContainer(vm, path);
 									return container.getDescription();
 								}
 							}
-						} catch (CoreException e) {
-							JDIDebugUIPlugin.log(e);
+						} else {
+							IClasspathContainer container = JavaCore.getClasspathContainer(entry.getPath(), project);
+							if (container != null) {
+								return container.getDescription();
+							}
 						}
+					} catch (CoreException e) {
+						JDIDebugUIPlugin.log(e);
 					}
-					return entry.getPath().toString();
-			}	
-			return ""; //$NON-NLS-1$
-		}
-		
-		/**
-		 * @see IBaseLabelProvider#dispose()
-		 */
-		public void dispose() {
-			super.dispose();
-			lp.dispose();
-		}
-		
-		/**
-		 * Sets the launch configuration context for this label provider
-		 */
-		public void setLaunchConfiguration(ILaunchConfiguration configuration) {
-			fLaunchConfuration = configuration;
-			fireLabelProviderChanged(new LabelProviderChangedEvent(this));
-		}
-
+				}
+				return entry.getPath().toString();
+		}	
+		return ""; //$NON-NLS-1$
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IBaseLabelProvider#dispose()
+	 */
+	public void dispose() {
+		super.dispose();
+		lp.dispose();
+	}
+	
+	/**
+	 * Sets the launch configuration context for this label provider
+	 */
+	public void setLaunchConfiguration(ILaunchConfiguration configuration) {
+		fLaunchConfiguration = configuration;
+		fireLabelProviderChanged(new LabelProviderChangedEvent(this));
+	}
 }
-
