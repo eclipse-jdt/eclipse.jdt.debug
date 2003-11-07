11ca4f794025b8d83916922711e4b81ab543881c
aweinand <>
2003-11-07 17:01:21 +0000

    fixed #46181

--- org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXLaunchingPlugin.java
+++ org.eclipse.jdt.launching.macosx/macosx/org/eclipse/jdt/internal/launching/macosx/MacOSXLaunchingPlugin.java
@@ -85,13 +85,20 @@ public class MacOSXLaunchingPlugin extends Plugin {
 	 * Returns path to executable.
 	 */
 	static String[] createSWTlauncher(Class clazz, String[] cmdLine, String vmVersion) throws IOException {
-		
-		URL url= BootLoader.getInstallURL();
-		String path= url.getPath() + "Eclipse.app/Contents/MacOS/java_swt"; //$NON-NLS-1$
+				
+		// the following property is defined if Eclipse is started with java_swt
+		String java_swt= System.getProperty("org.eclipse.swtlauncher");	//$NON-NLS-1$
+		if (java_swt == null) {
+			// if not defined try to guess...
+			URL url= BootLoader.getInstallURL();
+			java_swt= url.getPath() + "Eclipse.app/Contents/MacOS/java_swt"; //$NON-NLS-1$
+		}
+		if (java_swt == null)
+			return cmdLine;		// give up
 		
 		String[] newCmdLine= new String[cmdLine.length+1];
 		int argCount= 0;
-		newCmdLine[argCount++]= path;
+		newCmdLine[argCount++]= java_swt;
 		newCmdLine[argCount++]= "-XXvm=" + vmVersion; //$NON-NLS-1$
 		for (int i= 1; i < cmdLine.length; i++)
 			newCmdLine[argCount++]= cmdLine[i];
