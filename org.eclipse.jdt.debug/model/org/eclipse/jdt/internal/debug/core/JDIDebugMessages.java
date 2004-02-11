e153f1e9e7276ba543c05f37b1a09874e495e252
Darin Swanson <darins>
2004-02-11 00:48:52 +0000

    *** empty log message ***

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/JDIDebugMessages.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/JDIDebugMessages.java
@@ -11,24 +11,15 @@
 package org.eclipse.jdt.internal.debug.core;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
-/**
- * 
- */
+
 public class JDIDebugMessages {
 	private static final String BUNDLE_NAME = "org.eclipse.jdt.internal.debug.core.JDIDebugMessages";//$NON-NLS-1$
 	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
-	/**
-	 * 
-	 */
+	
 	private JDIDebugMessages() {
-		// TODO Auto-generated constructor stub
 	}
-	/**
-	 * @param key
-	 * @return
-	 */
+	
 	public static String getString(String key) {
-		// TODO Auto-generated method stub
 		try {
 			return RESOURCE_BUNDLE.getString(key);
 		} catch (MissingResourceException e) {
