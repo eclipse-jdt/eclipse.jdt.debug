4914cd014b916e8180c171fd280c893dcd1ce6f7
Kevin Barnes <krbarnes>
2004-06-10 20:34:20 +0000

    Bug 65504 - No progress feedback when changing VM installs

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/jres/JREsUpdater.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/jres/JREsUpdater.java
@@ -11,32 +11,18 @@
 package org.eclipse.jdt.internal.debug.ui.jres;
 
 import java.io.IOException;
-import java.util.List;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import org.eclipse.core.resources.IncrementalProjectBuilder;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.OperationCanceledException;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.SubProgressMonitor;
-import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jdt.core.IClasspathEntry;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jdt.internal.launching.VMDefinitionsContainer;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstallType;
 import org.eclipse.jdt.launching.JavaRuntime;
-import org.eclipse.jdt.launching.LibraryLocation;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Shell;
 
 /**
@@ -94,130 +80,13 @@ public class JREsUpdater {
 			vmContainer.addVM(jres[i]);
 		}
 		
-		// determine if a build is required
-		boolean buildRequired = false;
-		try {
-			buildRequired = isBuildRequired(fOriginalVMs, vmContainer);
-		} catch (CoreException e) {
-			JDIDebugUIPlugin.log(e);
-		} 
-		boolean build = false;
-		if (buildRequired) {
-			// prompt the user to do a full build
-			boolean autobuild = ResourcesPlugin.getWorkspace().isAutoBuilding();
-			String[] buttons = null; 
-			if (autobuild) {
-				// prompt OK/cancel if autobuild is on
-				buttons = new String[] {JREMessages.getString("JREsUpdater.0"), JREMessages.getString("JREsPreferencePage.8")};  //$NON-NLS-1$//$NON-NLS-2$
-			} else {
-				// prompt yes/no/cancel if autobuild if off
-				buttons = new String[] {JREMessages.getString("JREsPreferencePage.6"), JREMessages.getString("JREsPreferencePage.7"), JREMessages.getString("JREsPreferencePage.8")};  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-			}
-			MessageDialog messageDialog = new MessageDialog(getShell(), JREMessages.getString("JREsPreferencePage.4"), null,  //$NON-NLS-1$
-			JREMessages.getString("JREsPreferencePage.5"), //$NON-NLS-1$
-			MessageDialog.QUESTION, buttons, 0);
-			int button = messageDialog.open();
-			if (button == buttons.length - 1) {
-				// cancel
-				return false;
-			}
-			build = button == 0;			
-		}
 		
 		// Generate XML for the VM defs and save it as the new value of the VM preference
 		saveVMDefinitions(vmContainer);
 		
-		// do a build if required
-		if (build) {
-			doFullBuild();
-		}
 		
 		return true;
 	}
-	
-	/**
-	 * Returns whether a re-build is required based on the previous and current
-	 * VM definitions.
-	 * 
-	 * @param prev VMs defined in the workspace
-	 * @param curr VMs that will be defined in the workspace
-	 * @return whether the new JRE definitions required the workspace to be
-	 * built
-	 */
-	private boolean isBuildRequired(VMDefinitionsContainer prev, VMDefinitionsContainer curr) throws CoreException {
-		String prevDef = prev.getDefaultVMInstallCompositeID();
-		String currDef = curr.getDefaultVMInstallCompositeID();
-		
-		boolean defaultChanged = !isEqual(prevDef, currDef);
-		
-		IJavaProject[] projects = JavaCore.create(ResourcesPlugin.getWorkspace().getRoot()).getJavaProjects();
-		
-		//if the default VM changed, see if any projects reference it
-		if (defaultChanged) {
-			for (int i = 0; i < projects.length; i++) {
-				IJavaProject project = projects[i];
-				IClasspathEntry[] entries = project.getRawClasspath();
-				for (int j = 0; j < entries.length; j++) {
-					IClasspathEntry entry = entries[j];
-					switch (entry.getEntryKind()) {
-						case IClasspathEntry.CPE_VARIABLE:
-							IPath path = entry.getPath();
-							if (path.segmentCount() == 1 && path.segment(0).equals(JavaRuntime.JRELIB_VARIABLE)) {
-								// a project references the default JRE via JRE_LIB
-								return true;
-							}
-							break;
-						case IClasspathEntry.CPE_CONTAINER:
-							path = entry.getPath();
-							if (path.segmentCount() == 1 && path.segment(0).equals(JavaRuntime.JRE_CONTAINER)) {
-								// a project references the default JRE via JRE_CONTAIER
-								return true;
-							}
-							break;
-					}
-				}
-			}
-		}
-		
-		// otherwise, if a referenced VM is removed or there is a library
-		// change in a referenced VM, a build is required 
-		List futureVMs = curr.getVMList();
-		for (int i = 0; i < projects.length; i++) {
-			IJavaProject project = projects[i];
-			IVMInstall prevVM = JavaRuntime.getVMInstall(project);
-			if (prevVM != null) {
-				int index  = futureVMs.indexOf(prevVM);
-				if (index >= 0) {
-					IVMInstall futureVM = (IVMInstall)futureVMs.get(index);
-					// the VM still exists, see if the libraries changed
-					LibraryLocation[] prevLibs = JavaRuntime.getLibraryLocations(prevVM);
-					LibraryLocation[] newLibs = JavaRuntime.getLibraryLocations(futureVM);
-					if (prevLibs.length == newLibs.length) {
-						for (int j = 0; j < newLibs.length; j++) {
-							LibraryLocation newLib = newLibs[j];
-							LibraryLocation prevLib = prevLibs[j];
-							String newPath = newLib.getSystemLibraryPath().toOSString();
-							String prevPath = prevLib.getSystemLibraryPath().toOSString();
-							if (!newPath.equalsIgnoreCase(prevPath)) {
-								// different libs or ordering, a re-build is required
-								return true;
-							}
-						} 
-					} else {
-						// different number of libraries, a re-build is required
-						return true;
-					}
-				} else {
-					// the VM no longer exists, a re-build will be required
-					return true;
-				}
-			}
-		}
-		
-		
-		return false;
-	}
-	
 	
 	private boolean isEqual(Object a, Object b) {
 		if (a == null) {
@@ -230,22 +99,28 @@ public class JREsUpdater {
 	}	
 	
 	private void saveVMDefinitions(final VMDefinitionsContainer container) {
-		BusyIndicator.showWhile(getShell().getDisplay(), new Runnable() {
-			public void run() {
-				// Generate XML for the VM defs and save it as the new value of the VM preference
+		IRunnableWithProgress runnable = new IRunnableWithProgress() {
+			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
 				try {
+					monitor.beginTask(JREMessages.getString("JREsUpdater.0"), 100); //$NON-NLS-1$
 					String vmDefXML = container.getAsXML();
+					monitor.worked(40);
 					JavaRuntime.getPreferences().setValue(JavaRuntime.PREF_VM_XML, vmDefXML);
+					monitor.worked(30);
 					JavaRuntime.savePreferences();
+					monitor.worked(30);
 				} catch (IOException ioe) {
 					JDIDebugUIPlugin.log(ioe);
 				} catch (ParserConfigurationException e) {
 					JDIDebugUIPlugin.log(e);
 				} catch (TransformerException e) {
 					JDIDebugUIPlugin.log(e);
+				} finally {
+					monitor.done();
 				}
+				
 			}
-		});
+		};
 	}	
 	
 	private Shell getShell() {
@@ -256,34 +131,4 @@ public class JREsUpdater {
 		}
 	} 
 	
-	private boolean doFullBuild() {
-		
-		Job buildJob = new Job(JREMessages.getString("JREsUpdater.1")) { //$NON-NLS-1$
-			/* (non-Javadoc)
-			 * @see org.eclipse.core.runtime.jobs.Job#run(org.eclipse.core.runtime.IProgressMonitor)
-			 */
-			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					monitor.setTaskName(JREMessages.getString("JREsUpdater.2")); //$NON-NLS-1$
-					ResourcesPlugin.getWorkspace().build(IncrementalProjectBuilder.FULL_BUILD, new SubProgressMonitor(monitor, 2));
-				} catch (CoreException e) {
-					return e.getStatus();
-				} catch (OperationCanceledException e) {
-					return Status.CANCEL_STATUS;
-				}
-				finally {
-					monitor.done();
-				}
-				return Status.OK_STATUS;
-			}
-			public boolean belongsTo(Object family) {
-				return ResourcesPlugin.FAMILY_MANUAL_BUILD == family;
-			}
-		};
-		
-		buildJob.setRule(ResourcesPlugin.getWorkspace().getRuleFactory().buildRule());
-		buildJob.setUser(true); 
-		buildJob.schedule();
-		return true;
-	}		
 }
