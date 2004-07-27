2adc7000a363b9ccfa67c3f2f3b3132d0a6a826b
Kevin Barnes <krbarnes>
2004-07-27 17:16:10 +0000

    Bug 36669 - Make console a better shell replacement

---
+++ org.eclipse.jdt.debug.tests/console tests/org/eclipse/jdt/debug/tests/console/IOConsoleEchoActionDelegate.java
@@ -0,0 +1,91 @@
+package org.eclipse.jdt.debug.tests.console;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.IActionDelegate2;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IOConsole;
+import org.eclipse.ui.console.IOConsoleInputStream;
+import org.eclipse.ui.console.IOConsoleOutputStream;
+
+public class IOConsoleEchoActionDelegate implements IActionDelegate2, IWorkbenchWindowActionDelegate {
+
+    public void init(IAction action) {
+    }
+
+    public void dispose() {
+    }
+
+    public void runWithEvent(IAction action, Event event) {
+        run(action);
+    }
+
+    public void run(IAction action) {
+        new Thread(new Runnable() {
+            public void run() {
+                runTest();
+            }
+        }, "IOConsole Test Thread").start(); //$NON-NLS-1$
+    }
+    
+    public void runTest() {
+        final IOConsole console = new IOConsole("IO Test Console", DebugUITools.getImageDescriptor(IDebugUIConstants.IMG_ACT_RUN)); //$NON-NLS-1$
+        console.setWordWrap(true);
+        
+        final Display display = Display.getDefault();
+        
+        final IOConsoleInputStream in = (IOConsoleInputStream) console.getInputStream();
+        display.asyncExec(new Runnable() {
+            public void run() {        
+                in.setColor(display.getSystemColor(SWT.COLOR_BLUE));
+            }
+        });
+        IConsoleManager manager = ConsolePlugin.getDefault().getConsoleManager();
+        manager.addConsoles(new IConsole[] { console });
+        
+        final IOConsoleOutputStream out = console.createOutputStream("MY STREAM YAY"); //$NON-NLS-1$
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                out.setColor(Display.getDefault().getSystemColor(SWT.COLOR_GREEN));     
+                out.setFontStyle(SWT.BOLD | SWT.ITALIC);
+            }
+        });
+        
+        for(;;) {
+            PrintStream ps = new PrintStream(out);
+            byte[] b = new byte[1024];
+            int bRead = 0;
+            try {
+                bRead = in.read(b);
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+            
+            try {
+                out.write(b, 0, bRead);
+                ps.println();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void selectionChanged(IAction action, ISelection selection) {
+    }
+
+    public void init(IWorkbenchWindow window) {
+    }
+
+}
---
+++ org.eclipse.jdt.debug.tests/console tests/org/eclipse/jdt/debug/tests/console/IOConsoleOutputActionDelegate.java
@@ -0,0 +1,82 @@
+package org.eclipse.jdt.debug.tests.console;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.IDebugUIConstants;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.ui.IActionDelegate2;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IOConsole;
+
+public class IOConsoleOutputActionDelegate implements IActionDelegate2, IWorkbenchWindowActionDelegate{
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate2#init(org.eclipse.jface.action.IAction)
+     */
+    public void init(IAction action) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate2#dispose()
+     */
+    public void dispose() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate2#runWithEvent(org.eclipse.jface.action.IAction, org.eclipse.swt.widgets.Event)
+     */
+    public void runWithEvent(IAction action, Event event) {
+        run(action);
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IAction action) {
+        IOConsole console = new IOConsole("Test IOConsole", DebugUITools.getImageDescriptor(IDebugUIConstants.IMG_ACT_RUN)); //$NON-NLS-1$
+		IConsoleManager manager = ConsolePlugin.getDefault().getConsoleManager();
+		manager.addConsoles(new IConsole[]{console});
+		OutputStream out = console.createOutputStream("MyOutputStream"); //$NON-NLS-1$
+		final PrintStream stream = new PrintStream(out);
+		Runnable r = new Runnable() {
+			public void run() {
+				for (int i = 0; i < 100000;i++) {
+					stream.print(Integer.toString(i));
+					stream.print(": Testing...");
+					stream.print("one...");
+					stream.println("two.... three....");
+				}
+			}
+		};
+		new Thread(r).start();
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+     */
+    public void init(IWorkbenchWindow window) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
