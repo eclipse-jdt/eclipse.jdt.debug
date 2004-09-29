139ef16faeaab3fd39bafa484e970050a79fbf18
Darin Wright <darin>
2004-09-29 15:59:54 +0000

    Bug 51884 -  Feature request: More flexible debug hierarchy

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JavaDebugContentProvider.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.debug.ui;
+
+import org.eclipse.debug.ui.DefaultDebugViewContentProvider;
+
+/**
+ * Java debugger content provider for the debug view. Provides monitor
+ * information.
+ * 
+ * @since 3.1
+ */
+public class JavaDebugContentProvider extends DefaultDebugViewContentProvider {
+
+}
