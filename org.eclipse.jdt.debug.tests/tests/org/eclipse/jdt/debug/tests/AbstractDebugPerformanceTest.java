b0602296d5a9883330a2c6240130bdc890eb4255
Darin Swanson <darins>
2004-10-18 17:09:20 +0000

    Removed unused import

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugPerformanceTest.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugPerformanceTest.java
@@ -13,7 +13,6 @@ package org.eclipse.jdt.debug.tests;
 import org.eclipse.test.performance.Dimension;
 import org.eclipse.test.performance.Performance;
 import org.eclipse.test.performance.PerformanceMeter;
-import org.eclipse.test.performance.PerformanceTestCase;
 
 public class AbstractDebugPerformanceTest extends AbstractDebugTest {
 
