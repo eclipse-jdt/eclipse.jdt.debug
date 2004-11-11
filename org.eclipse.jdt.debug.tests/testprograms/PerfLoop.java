592f3b2f1be6f5d7816d0a1ecf826e9d62db6f45
Darin Swanson <darins>
2004-11-11 20:46:32 +0000

    Increased duration of performance test

--- org.eclipse.jdt.debug.tests/testprograms/PerfLoop.java
+++ org.eclipse.jdt.debug.tests/testprograms/PerfLoop.java
@@ -18,7 +18,7 @@ public class PerfLoop {
 
 	public static void main(String[] args) {
 		Vector v = new Vector(200);
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10000; i++) {
 			v.add(new Integer(i));
 		}
 	}
