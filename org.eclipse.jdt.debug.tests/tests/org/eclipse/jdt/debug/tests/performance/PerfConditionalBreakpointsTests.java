4626445de1c91df2e1fe4660dee1ab5cfb48dd22
Kevin Barnes <krbarnes>
2005-03-17 20:12:33 +0000

    Bug 86412 - Performance results for testConditionalBreakpoint not useful on Linux

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfConditionalBreakpointsTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfConditionalBreakpointsTests.java
@@ -1,23 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.debug.tests.performance;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import org.eclipse.debug.core.model.IVariable;
+import org.eclipse.debug.core.DebugEvent;
+import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.IDebugEventSetListener;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.debug.core.IJavaDebugTarget;
 import org.eclipse.jdt.debug.core.IJavaLineBreakpoint;
-import org.eclipse.jdt.debug.core.IJavaPrimitiveValue;
-import org.eclipse.jdt.debug.core.IJavaStackFrame;
 import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.tests.AbstractDebugPerformanceTest;
 import org.eclipse.test.performance.Dimension;
@@ -25,55 +24,88 @@ import org.eclipse.test.performance.Dimension;
 /**
  * Tests performance of conditional breakpoints.
  */
-public class PerfConditionalBreakpointsTests extends
-        AbstractDebugPerformanceTest {
+public class PerfConditionalBreakpointsTests extends AbstractDebugPerformanceTest {
+    private String fTypeName = "PerfLoop";
+    private int fHitCount = 0;
+    private IJavaLineBreakpoint fBP;
+    private IJavaDebugTarget fTarget;
+    private Exception fException;
+    
+    private boolean fConditionalBreakpointSet = false;
+    private boolean fWarmUpComplete = false;
+    private int fWarmUpRuns = 5;
+    private int fMeasuredRuns = 100;
+
+    private class BreakpointListener implements IDebugEventSetListener {
+        public void handleDebugEvents(DebugEvent[] events) {
+            for (int i = 0; i < events.length; i++) {
+                DebugEvent event = events[i];
+                if (event.getKind() == DebugEvent.SUSPEND && event.getDetail() == DebugEvent.BREAKPOINT) {
+                    IJavaThread source = (IJavaThread) event.getSource();
+                    breakpointHit(source);
+                }
+            }
+        }
+    };
 
     public PerfConditionalBreakpointsTests(String name) {
         super(name);
     }
 
     public void testConditionalBreakpoint() throws Exception {
-        String typeName = "PerfLoop";
-        IJavaLineBreakpoint bp = createConditionalLineBreakpoint(22, typeName, "i == 99", true);
+        tagAsGlobalSummary("Conditional Breakpoint Test", Dimension.CPU_TIME);
+        // just in case
+        removeAllBreakpoints();
 
-        List threads= new ArrayList();
+        fBP = createLineBreakpoint(22, fTypeName);
+
+        DebugPlugin.getDefault().addDebugEventListener(new BreakpointListener());
+        ILaunchConfiguration config = getLaunchConfiguration(fTypeName);
+        fTarget = launchAndTerminate(config, 5 * 60 * 60 * 1000);
+        
+        if(fException != null) {
+            throw fException;
+        }
+        
+        commitMeasurements();
+        assertPerformance();
+        
+        removeAllBreakpoints();
+    }
+
+    private synchronized void breakpointHit(IJavaThread thread) {
         try {
-            
-            //cold launch, open editor etc.
-            IJavaThread thread  = launchToLineBreakpoint(typeName, bp);
-            threads.add(thread);
-            tagAsGlobalSummary("Conditional Breakpoint", Dimension.CPU_TIME);
-            for (int i = 0; i < 5; i++) {
-                try {
-                    startMeasuring();
-                    thread = launchToLineBreakpoint(typeName, bp);
+            if (!fConditionalBreakpointSet) {
+                fBP.delete();
+                fBP = createConditionalLineBreakpoint(22, fTypeName, "i%100==0", true);
+                fConditionalBreakpointSet = true;
+            } else if (!fWarmUpComplete) {
+                fHitCount++;
+                if (fHitCount == fWarmUpRuns) {
+                    fWarmUpComplete = true;
+                    fHitCount =  0;
+                }
+                return;
+            } else {
+                if (fHitCount > 0) {
                     stopMeasuring();
-                } finally {
-                   threads.add(thread);
+                }
+                fHitCount++;
+                if (fHitCount <= fMeasuredRuns) {
+                    startMeasuring();
+                } else {
+                    fBP.delete();
                 }
             }
-
-            commitMeasurements();
-            assertPerformance();
-
-            //verify actually stopping at the correct location
-            IJavaStackFrame frame = (IJavaStackFrame) thread.getTopStackFrame();
-            IVariable var = frame.findVariable("i");
-            assertNotNull("Could not find variable 'i'", var);
-
-            IJavaPrimitiveValue value = (IJavaPrimitiveValue) var.getValue();
-            assertNotNull("variable 'i' has no value", value);
-            int iValue = value.getIntValue();
-            assertEquals("value of 'i' incorrect", 99, iValue);
-            bp.delete();
-        } finally {
-            Iterator iter= threads.iterator();
-            while (iter.hasNext()) {
-                IJavaThread thread = (IJavaThread) iter.next();
-                terminateAndRemove(thread);    
-            }
-            
+        } catch (Exception e) {
+            fException = e;
             removeAllBreakpoints();
+        } finally {
+            try {
+                thread.resume();
+            } catch (DebugException e) {
+                fException = e;
+            }
         }
     }
 }
