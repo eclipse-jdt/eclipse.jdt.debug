d26a010df25c5fa7329146f5cc09fcd64ade78b4
Kevin Barnes <krbarnes>
2005-03-23 19:00:56 +0000

    Bug 78167 - [performance test] breakpoints

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfBreakpointTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfBreakpointTests.java
@@ -12,6 +12,7 @@
 package org.eclipse.jdt.debug.tests.performance;
 
 import org.eclipse.core.resources.IMarkerDelta;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.DebugPlugin;
@@ -24,10 +25,7 @@ import org.eclipse.jdt.debug.core.JDIDebugModel;
 import org.eclipse.jdt.debug.tests.AbstractDebugPerformanceTest;
 import org.eclipse.test.performance.Dimension;
 
-/*
- * Create & remove * (line breakpoint|watchpoint|method entry breakpoint) -
- * small source file - large source file Enable & disable * (line
- * breakpoint|watchpoint|method entry breakpoint)
+/* TODO: watchpoint creation
  */
 public class PerfBreakpointTests extends AbstractDebugPerformanceTest implements IBreakpointListener {
 
@@ -37,36 +35,38 @@ public class PerfBreakpointTests extends AbstractDebugPerformanceTest implements
         super(name);
     }
 
-    public void testBreakpointCreation() throws Exception {
+    public void testLineBreakpointCreation() throws Exception {
         tagAsSummary("Install Line Breakpoints", Dimension.CPU_TIME);
         String typeName = "LargeSourceFile";
         IResource resource = getBreakpointResource(typeName);
 
-        createLineBreakpoint(14, typeName);
+        IJavaLineBreakpoint bp = createLineBreakpoint(14, typeName);
         IJavaThread thread = launchToBreakpoint(typeName);
+        bp.delete();
+        
         try {
             DebugPlugin.getDefault().getBreakpointManager().addBreakpointListener(this);
 
-            int[] lineNumbers = new int[50];
+            int[] lineNumbers = new int[20];
             for (int i = 0; i < lineNumbers.length; i++) {
                 lineNumbers[i] = 15 + i;
             }
-            createLineBreakpoints(resource, typeName, lineNumbers);
-            waitForBreakpointCount(lineNumbers.length);
-            removeAllBreakpoints();
-            breakpointCount = 0;
-
-            lineNumbers = new int[100];
-            for (int i = 0; i < lineNumbers.length; i++) {
-                lineNumbers[i] = 15 + i;
-            }
-
+            
             for (int i = 0; i < 5; i++) {
+                createLineBreakpoints(resource, typeName, lineNumbers);
+                waitForBreakpointCount(lineNumbers.length);
+                removeAllBreakpoints();
+                waitForBreakpointCount(0);
+                breakpointCount = 0;  
+            }
+
+            for (int i = 0; i < 100; i++) {
                 startMeasuring();
                 createLineBreakpoints(resource, typeName, lineNumbers);
                 waitForBreakpointCount(lineNumbers.length);
                 stopMeasuring();
                 removeAllBreakpoints();
+                waitForBreakpointCount(0);
                 breakpointCount = 0;
             }
             commitMeasurements();
@@ -98,6 +98,7 @@ public class PerfBreakpointTests extends AbstractDebugPerformanceTest implements
 
             for (int i = 0; i < 5; i++) {
                 createLineBreakpoints(resource, typeName, lineNumbers);
+                waitForBreakpointCount(lineNumbers.length);
                 IBreakpoint[] breakpoints = manager.getBreakpoints();
                 manager.removeBreakpoints(breakpoints, true);
                 waitForBreakpointCount(0);
@@ -125,6 +126,50 @@ public class PerfBreakpointTests extends AbstractDebugPerformanceTest implements
             terminateAndRemove(thread);
         }
     }
+    
+
+
+    public void testMethodEntryBreakpointCreation() throws Exception {
+        tagAsSummary("Install Method Entry Breakpoints", Dimension.CPU_TIME);
+        String typeName = "LargeSourceFile";
+        IProject project = getJavaProject().getProject();
+        
+        IJavaLineBreakpoint bp = createLineBreakpoint(14, typeName);
+        IJavaThread thread = launchToBreakpoint(typeName);
+        bp.delete();
+
+        IBreakpointManager manager = DebugPlugin.getDefault().getBreakpointManager();
+        try {
+            manager.addBreakpointListener(this);
+
+            String[] methods = new String[100];
+            for (int i = 0; i < methods.length; i++) {
+                methods[i] = "method"+(i+1);
+            }
+
+            for (int i = 0; i < 5; i++) {
+                createMethodEntryBreakpoints(project, typeName, methods);
+                waitForBreakpointCount(methods.length);
+                removeAllBreakpoints();
+                waitForBreakpointCount(0);
+            }
+
+            for (int i = 0; i < 50; i++) {
+                startMeasuring();
+                createMethodEntryBreakpoints(project, typeName, methods);
+                waitForBreakpointCount(methods.length);
+                stopMeasuring();
+                removeAllBreakpoints();
+                breakpointCount = 0;
+            }
+            commitMeasurements();
+            assertPerformance();
+        } finally {
+            DebugPlugin.getDefault().getBreakpointManager().removeBreakpointListener(this);
+            removeAllBreakpoints();
+            terminateAndRemove(thread);
+        }        
+    }
 
     private synchronized void waitForBreakpointCount(int i) throws Exception {
         long end = System.currentTimeMillis() + 60000;
@@ -140,6 +185,13 @@ public class PerfBreakpointTests extends AbstractDebugPerformanceTest implements
         }
     }
 
+    private void createMethodEntryBreakpoints(IProject project, String typeName, String[] methods) throws CoreException {
+        for (int i = 0; i < methods.length; i++) {
+            String methodName = methods[i];
+            JDIDebugModel.createMethodBreakpoint(project, typeName, methodName, "()V", true, false, false, -1, -1, -1, 0, true, null);
+        }
+    }
+    
     public synchronized void breakpointAdded(IBreakpoint breakpoint) {
         breakpointCount++;
         notifyAll();
