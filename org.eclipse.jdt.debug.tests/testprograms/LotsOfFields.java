2be9a019649e2ff849eef044c2661da9d987c09f
cvs2svn <platform-releng-dev@eclipse.org>
2005-03-30 22:50:58 +0000

    This commit was manufactured by cvs2svn to create branch 'perf_30'.
    Cherrypick from master 2005-03-30 22:50:56 UTC Kevin Barnes <krbarnes> 'Bug 78167 - [performance test] breakpoints':
        org.eclipse.jdt.debug.tests/testprograms/LotsOfFields.java

---
+++ org.eclipse.jdt.debug.tests/testprograms/LotsOfFields.java
@@ -0,0 +1,118 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+public class LotsOfFields {
+    
+    private int field_1;
+    private int field_2;
+    private int field_3;
+    private int field_4;
+    private int field_5;
+    private int field_6;
+    private int field_7;
+    private int field_8;
+    private int field_9;
+    private int field_10;
+    private int field_11;
+    private int field_12;
+    private int field_13;
+    private int field_14;
+    private int field_15;
+    private int field_16;
+    private int field_17;
+    private int field_18;
+    private int field_19;
+    private int field_20;
+    private int field_21;
+    private int field_22;
+    private int field_23;
+    private int field_24;
+    private int field_25;
+    private int field_26;
+    private int field_27;
+    private int field_28;
+    private int field_29;
+    private int field_30;
+    private int field_31;
+    private int field_32;
+    private int field_33;
+    private int field_34;
+    private int field_35;
+    private int field_36;
+    private int field_37;
+    private int field_38;
+    private int field_39;
+    private int field_40;
+    private int field_41;
+    private int field_42;
+    private int field_43;
+    private int field_44;
+    private int field_45;
+    private int field_46;
+    private int field_47;
+    private int field_48;
+    private int field_49;
+    private int field_50;
+    private int field_51;
+    private int field_52;
+    private int field_53;
+    private int field_54;
+    private int field_55;
+    private int field_56;
+    private int field_57;
+    private int field_58;
+    private int field_59;
+    private int field_60;
+    private int field_61;
+    private int field_62;
+    private int field_63;
+    private int field_64;
+    private int field_65;
+    private int field_66;
+    private int field_67;
+    private int field_68;
+    private int field_69;
+    private int field_70;
+    private int field_71;
+    private int field_72;
+    private int field_73;
+    private int field_74;
+    private int field_75;
+    private int field_76;
+    private int field_77;
+    private int field_78;
+    private int field_79;
+    private int field_80;
+    private int field_81;
+    private int field_82;
+    private int field_83;
+    private int field_84;
+    private int field_85;
+    private int field_86;
+    private int field_87;
+    private int field_88;
+    private int field_89;
+    private int field_90;
+    private int field_91;
+    private int field_92;
+    private int field_93;
+    private int field_94;
+    private int field_95;
+    private int field_96;
+    private int field_97;
+    private int field_98;
+    private int field_99;
+    private int field_100;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
