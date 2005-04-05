34d0143000c70319e55becc91330b6743a6d56f5
Kevin Barnes <krbarnes>
2005-04-05 22:11:38 +0000

    Bug 90351 - [performance test] Tests completing too quickly

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/PerformanceSuite.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/PerformanceSuite.java
@@ -38,13 +38,13 @@ public class PerformanceSuite extends DebugSuite {
 	 */
 	public PerformanceSuite() {
 		addTest(new TestSuite(ProjectCreationDecorator.class));
-//		addTest(new TestSuite(PerfDebugBaselineTest.class));
-//		
-//		addTest(new TestSuite(PerfContextualLaunchMenu.class));
+		addTest(new TestSuite(PerfDebugBaselineTest.class));
+		
+		addTest(new TestSuite(PerfContextualLaunchMenu.class));
 		addTest(new TestSuite(PerfBreakpointTests.class));
-//		addTest(new TestSuite(PerfConditionalBreakpointsTests.class));
-//		addTest(new TestSuite(PerfSteppingTests.class));
-//		addTest(new TestSuite(PerfConsoleTests.class));
+		addTest(new TestSuite(PerfConditionalBreakpointsTests.class));
+		addTest(new TestSuite(PerfSteppingTests.class));
+		addTest(new TestSuite(PerfConsoleTests.class));
 //		addTest(new TestSuite(PerfLaunchTests.class));
 		
 	}
