0e43fd342b78bf0a5c13a2b4cba1506e73b79a97
cvs2svn <platform-releng-dev@eclipse.org>
2005-04-12 14:22:27 +0000

    This commit was manufactured by cvs2svn to create tag 'perf213_v20050412'.
    Sprout from R2_1_maintenance 2005-04-12 13:40:31 UTC Darin Wright <darin> 'update classpath to use plug-in container/jre container'
    Cherrypick from perf_213 2005-04-12 14:22:26 UTC Darin Wright <darin> 'perf tests':
        org.eclipse.jdt.debug.tests/.classpath
        org.eclipse.jdt.debug.tests/testprograms/LargeSourceFile.java
        org.eclipse.jdt.debug.tests/testprograms/LotsOfFields.java
        org.eclipse.jdt.debug.tests/testprograms/PerfLoop.java
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugPerformanceTest.java
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/PerformanceSuite.java
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/ProjectCreationDecorator.java
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfBreakpointTests.java
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfConditionalBreakpointsTests.java
    Delete:
        org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/performance/PerfSteppingTests.java

--- org.eclipse.jdt.debug.tests/testprograms/PerfLoop.java
+++ org.eclipse.jdt.debug.tests/testprograms/PerfLoop.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2004 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -18,7 +18,7 @@ public class PerfLoop {
 
 	public static void main(String[] args) {
 		Vector v = new Vector(200);
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10000; i++) {
 			v.add(new Integer(i));
 		}
 	}
