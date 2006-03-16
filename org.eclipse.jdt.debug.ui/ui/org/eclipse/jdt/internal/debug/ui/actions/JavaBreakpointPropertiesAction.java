74726ee849b9cd430df941e237c08aef96f89cb1
Kevin Barnes <krbarnes>
2006-03-16 00:21:16 +0000

    Bug 132054 - NPE in JavaBreakpointPropertiesAction

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/JavaBreakpointPropertiesAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/JavaBreakpointPropertiesAction.java
@@ -12,12 +12,15 @@ package org.eclipse.jdt.internal.debug.ui.actions;
 
  
 import org.eclipse.jdt.debug.core.IJavaBreakpoint;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.window.IShellProvider;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.dialogs.PropertyDialogAction;
@@ -35,8 +38,18 @@ public class JavaBreakpointPropertiesAction implements IObjectActionDelegate {
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
+	    IShellProvider provider;
+	    if (getActivePart() != null) {
+	        provider = getActivePart().getSite();
+	    } else {
+	        provider = new IShellProvider() {
+	            public Shell getShell() {
+	                return JDIDebugUIPlugin.getActiveWorkbenchShell();
+	            }
+	        };
+        }
 		PropertyDialogAction propertyAction= 
-			new PropertyDialogAction(getActivePart().getSite(), new ISelectionProvider() {
+			new PropertyDialogAction(provider, new ISelectionProvider() {
 				public void addSelectionChangedListener(ISelectionChangedListener listener) {
 				}
 				public ISelection getSelection() {
