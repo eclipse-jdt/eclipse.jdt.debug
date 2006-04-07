5ba39a7ebdcb09496b4fdc59e27bff6281bf91f9
Darin Wright <darin>
2006-04-07 21:27:56 +0000

    Bug 123589 - Unexpected switch to empty editor when using scrapbook

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/threadgroups/JavaThreadEventHandler.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/threadgroups/JavaThreadEventHandler.java
@@ -14,15 +14,19 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.model.IThread;
 import org.eclipse.debug.internal.ui.viewers.provisional.AbstractModelProxy;
 import org.eclipse.debug.internal.ui.viewers.provisional.IModelDelta;
 import org.eclipse.debug.internal.ui.viewers.provisional.ModelDelta;
 import org.eclipse.debug.internal.ui.viewers.update.ThreadEventHandler;
+import org.eclipse.jdt.debug.core.IJavaStackFrame;
 import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.core.IJavaThreadGroup;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jdt.internal.debug.ui.snippeteditor.ScrapbookLauncher;
 
 /**
  * @since 3.2
@@ -62,6 +66,35 @@ public class JavaThreadEventHandler extends ThreadEventHandler {
 		} else {
 			return super.addPathToThread(delta, thread);
 		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.internal.ui.viewers.update.ThreadEventHandler#handlesEvent(org.eclipse.debug.core.DebugEvent)
+	 */
+	protected boolean handlesEvent(DebugEvent event) {
+		if (super.handlesEvent(event)) {
+			Object source = event.getSource();
+			if (source instanceof IJavaThread) {
+				IJavaThread thread = (IJavaThread) source;
+				ILaunch launch = thread.getLaunch();
+				if (launch != null) {
+					if (launch.getAttribute(ScrapbookLauncher.SCRAPBOOK_LAUNCH) != null) {
+						if (event.getKind() == DebugEvent.SUSPEND) {
+							try {
+								IJavaStackFrame frame = (IJavaStackFrame) thread.getTopStackFrame();
+								if (frame.getDeclaringTypeName().startsWith("org.eclipse.jdt.internal.debug.ui.snippeteditor.ScrapbookMain")) {
+									return false;
+								}
+							} catch (DebugException e) {
+							}
+						}
+					}
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
 	}	
 
 }
