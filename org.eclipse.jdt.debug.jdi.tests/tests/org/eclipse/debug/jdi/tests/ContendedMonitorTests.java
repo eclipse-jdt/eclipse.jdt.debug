97b381ba79a4b0ae709d425330f1c0f3fff0a115
Michael Rennie <mrennie>
2006-06-14 18:39:56 +0000

    further testing for Bug 145362 JDI test suite needs to be updated for 1.6 tests

--- org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/ContendedMonitorTests.java
+++ org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/ContendedMonitorTests.java
@@ -10,6 +10,14 @@
  *******************************************************************************/
 package org.eclipse.debug.jdi.tests;
 
+import java.util.List;
+
+import com.sun.jdi.request.EventRequestManager;
+import com.sun.jdi.request.MonitorContendedEnterRequest;
+import com.sun.jdi.request.MonitorContendedEnteredRequest;
+import com.sun.jdi.request.MonitorWaitRequest;
+import com.sun.jdi.request.MonitorWaitedRequest;
+
 /**
  * Test cases for the implementation of providing argumebnt information even if 
  * no debugging information is present in the new java 1.6 VM
@@ -18,8 +26,12 @@ package org.eclipse.debug.jdi.tests;
  */
 public class ContendedMonitorTests extends AbstractJDITest {
 
+	EventRequestManager erm = null;
+	
 	/** setup test info locally **/
-	public void localSetUp() {}
+	public void localSetUp() {
+		erm = fVM.eventRequestManager();
+	}
 
 	/**
 	 * test to see if a the 1.6 VM can get monitor events info and that 
@@ -32,6 +44,70 @@ public class ContendedMonitorTests extends AbstractJDITest {
 		else {
 			assertTrue("Should not have ability to request monitor events info", !fVM.canRequestMonitorEvents());
 		}
+	}
+	
+	/**
+	 * test getting monitor contended enter requests from the event request manager
+	 * this test is not applicable to non 1.6 VMs
+	 */
+	public void testMonitorContendedEnterRequests() {
+		if(!fVM.canRequestMonitorEvents()) {
+			return;
+		}
+		MonitorContendedEnterRequest req = erm.createMonitorContendedEnterRequest();
+		req.enable();
+		List list = erm.monitorContendedEnterRequests();
+		assertNotNull("list should not be null", list);
+		assertTrue("list should be of size 1", list.size() == 1);
+		assertTrue("req should be enabled", ((MonitorContendedEnterRequest)list.get(0)).isEnabled());
+	}
+	
+	/**
+	 * test getting monitor contended entered requests from the event request manager
+	 * this test is not applicable to non 1.6 VMs
+	 */
+	public void testMonitorContendedEnteredRequests() {
+		if(!fVM.canRequestMonitorEvents()) {
+			return;
+		}
+		MonitorContendedEnteredRequest req = erm.createMonitorContendedEnteredRequest();
+		req.enable();
+		List list = erm.monitorContendedEnteredRequests();
+		assertNotNull("list should not be null", list);
+		assertTrue("list should be of size 1", list.size() == 1);
+		assertTrue("req should be enabled", ((MonitorContendedEnteredRequest)list.get(0)).isEnabled());
+	}
+	
+	/**
+	 * test getting monitor wait requests from the event request manager
+	 * this test is not applicable to non 1.6 VMs
+	 */
+	public void testMonitorWaitRequest() {
+		if(!fVM.canRequestMonitorEvents()) {
+			return;
+		}	
+		MonitorWaitRequest req = erm.createMonitorWaitRequest();
+		req.enable();
+		List list = erm.monitorWaitRequests();
+		assertNotNull("list should not be null", list);
+		assertTrue("list should be of size 1", list.size() == 1);
+		assertTrue("req should be enabled", ((MonitorWaitRequest)list.get(0)).isEnabled());
+	}
+	
+	/**
+	 * test getting monitor waited requests from the event request manager
+	 * this test is not applicable to non 1.6 VMs
+	 */
+	public void testMonitorWaitedRequest() {
+		if(!fVM.canRequestMonitorEvents()) {
+			return;
+		}
+		MonitorWaitedRequest req = erm.createMonitorWaitedRequest();
+		req.enable();
+		List list = erm.monitorWaitedRequests();
+		assertNotNull("list should not be null", list);
+		assertTrue("list should be of size 1", list.size() == 1);
+		assertTrue("req should be enabled", ((MonitorWaitedRequest)list.get(0)).isEnabled());
 	}
 	
 }
