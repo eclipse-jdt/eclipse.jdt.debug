5313f542e87ec224895e4ecc826e10fa4b6fefda
Michael Rennie <mrennie>
2006-06-22 21:41:31 +0000

    Bug 24484 [jres] VM Properties

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/SWTUtil.java
@@ -0,0 +1,253 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.debug.ui;
+
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.preference.IPreferenceNode;
+import org.eclipse.jface.preference.IPreferencePage;
+import org.eclipse.jface.preference.PreferenceDialog;
+import org.eclipse.jface.preference.PreferenceManager;
+import org.eclipse.jface.preference.PreferenceNode;
+import org.eclipse.jface.resource.JFaceResources;
+import org.eclipse.jface.util.Assert;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+
+/**
+ * Utility class to simplify access to some SWT resources. 
+ * @since 3.3
+ */
+public class SWTUtil {
+		
+	/**
+	 * Returns a width hint for a button control.
+	 */
+	public static int getButtonWidthHint(Button button) {
+		button.setFont(JFaceResources.getDialogFont());
+		PixelConverter converter= new PixelConverter(button);
+		int widthHint= converter.convertHorizontalDLUsToPixels(IDialogConstants.BUTTON_WIDTH);
+		return Math.max(widthHint, button.computeSize(SWT.DEFAULT, SWT.DEFAULT, true).x);
+	}
+	
+	/**
+	 * Sets width and height hint for the button control.
+	 * <b>Note:</b> This is a NOP if the button's layout data is not
+	 * an instance of <code>GridData</code>.
+	 * 
+	 * @param	the button for which to set the dimension hint
+	 */		
+	public static void setButtonDimensionHint(Button button) {
+		Assert.isNotNull(button);
+		Object gd= button.getLayoutData();
+		if (gd instanceof GridData) {
+			((GridData)gd).widthHint= getButtonWidthHint(button);	
+			((GridData)gd).horizontalAlignment = GridData.FILL;	 
+		}
+	}		
+	
+	
+	/**
+	 * Creates and returns a new push button with the given
+	 * label and/or image.
+	 * 
+	 * @param parent parent control
+	 * @param label button label or <code>null</code>
+	 * @param image image of <code>null</code>
+	 * 
+	 * @return a new push button
+	 */
+	public static Button createPushButton(Composite parent, String label, Image image) {
+		Button button = new Button(parent, SWT.PUSH);
+		button.setFont(parent.getFont());
+		if (image != null) {
+			button.setImage(image);
+		}
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);	
+		SWTUtil.setButtonDimensionHint(button);
+		return button;	
+	}	
+
+	/**
+	 * Creates and returns a new radio button with the given
+	 * label.
+	 * 
+	 * @param parent parent control
+	 * @param label button label or <code>null</code>
+	 * 
+	 * @return a new radio button
+	 */
+	public static Button createRadioButton(Composite parent, String label) {
+		Button button = new Button(parent, SWT.RADIO);
+		button.setFont(parent.getFont());
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);	
+		SWTUtil.setButtonDimensionHint(button);
+		return button;	
+	}	
+	
+	/**
+	 * Creates a checked button
+	 * @param parent the parent composite to add this button to
+	 * @param label the label for the button
+	 * @param checked the button's checked state
+	 * @return a new check button
+	 */
+	public static Button createCheckButton(Composite parent, String label, boolean checked) {
+		Button button = new Button(parent, SWT.CHECK);
+		button.setFont(parent.getFont());
+		if (label != null) {
+			button.setText(label);
+		}
+		GridData gd = new GridData();
+		button.setLayoutData(gd);	
+		button.setSelection(checked);
+		return button;	
+	}
+	
+	/**
+	 * Creates a new label widget
+	 * @param parent the parent composite to add this label widget to
+	 * @param text the text for the label
+	 * @param hspan the horizontal span to take up in the parent composite
+	 * @return the new label
+	 */
+	public static Label createLabel(Composite parent, String text, int hspan) {
+		Label l = new Label(parent, SWT.NONE);
+		l.setFont(parent.getFont());
+		l.setText(text);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan = hspan;
+		l.setLayoutData(gd);
+		return l;
+	}
+	
+	/**
+	 * Creates a new text widget 
+	 * @param parent the parent composite to add this text widget to
+	 * @param hspan the horizontal span to take up on the parent composite
+	 * @return the new text widget
+	 */
+	public static Text createSingleText(Composite parent, int hspan) {
+    	Text t = new Text(parent, SWT.SINGLE | SWT.BORDER);
+    	t.setFont(parent.getFont());
+    	GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+    	gd.horizontalSpan = hspan;
+    	t.setLayoutData(gd);
+    	return t;
+    }
+	
+	/**
+	 * Creates a Group widget
+	 * @param parent the parent composite to add this group to
+	 * @param text the text for the heading of the group
+	 * @param columns the number of columns within the group
+	 * @param hspan the horizontal span the group should take up on the parent
+	 * @param fill the style for how this composite should fill into its parent
+	 * @return the new group
+	 */
+	public static Group createGroup(Composite parent, String text, int columns, int hspan, int fill) {
+    	Group g = new Group(parent, SWT.NONE);
+    	g.setLayout(new GridLayout(columns, false));
+    	g.setText(text);
+    	g.setFont(parent.getFont());
+    	GridData gd = new GridData(fill);
+		gd.horizontalSpan = hspan;
+    	g.setLayoutData(gd);
+    	return g;
+    }
+	
+	/**
+	 * This method allows us to open the preference dialog on the specific page, in this case the perspective page
+	 * @param id the id of pref page to show
+	 * @param page the actual page to show
+	 */
+	public static void showPreferencePage(String id, IPreferencePage page) {
+		final IPreferenceNode targetNode = new PreferenceNode(id, page);
+		PreferenceManager manager = new PreferenceManager();
+		manager.addToRoot(targetNode);
+		final PreferenceDialog dialog = new PreferenceDialog(JDIDebugUIPlugin.getActiveWorkbenchShell(), manager);
+		BusyIndicator.showWhile(JDIDebugUIPlugin.getStandardDisplay(), new Runnable() {
+			public void run() {
+				dialog.create();
+				dialog.setMessage(targetNode.getLabelText());
+				dialog.open();
+			}
+		});		
+	}
+	
+	/**
+	 * Creates a Composite widget
+	 * @param parent the parent composite to add this composite to
+	 * @param columns the number of columns within the composite
+	 * @param hspan the horizontal span the composite should take up on the parent
+	 * @param fill the style for how this composite should fill into its parent
+	 * @return the new group
+	 */
+	public static Composite createComposite(Composite parent, Font font, int columns, int hspan, int fill) {
+    	Composite g = new Composite(parent, SWT.NONE);
+    	g.setLayout(new GridLayout(columns, false));
+    	g.setFont(font);
+    	GridData gd = new GridData(fill);
+		gd.horizontalSpan = hspan;
+    	g.setLayoutData(gd);
+    	return g;
+    }
+	
+	/**
+	 * creates a vertical spacer for seperating components
+	 * @param comp
+	 * @param numlines
+	 */
+	public static void createVerticalSpacer(Composite comp, int numlines) {
+		Label lbl = new Label(comp, SWT.NONE);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.heightHint = numlines;
+		lbl.setLayoutData(gd);
+	}
+	
+	/**
+	 * Creates an ExpandibleComposite widget
+	 * @param parent the parent to add this widget to
+	 * @param style the style for ExpandibleComposite expanding handle, and layout
+	 * @param label the label for the widget
+	 * @param hspan how many columns to span in the parent
+	 * @param fill the fill style for the widget
+	 * @return a new ExpandibleComposite widget
+	 */
+	public static ExpandableComposite createExpandibleComposite(Composite parent, int style, String label, int hspan, int fill) {
+		ExpandableComposite ex = new ExpandableComposite(parent, SWT.NONE, style);
+		ex.setText(label);
+		ex.setFont(JFaceResources.getFontRegistry().getBold(JFaceResources.DIALOG_FONT));
+		GridData gd = new GridData(fill);
+		gd.horizontalSpan = hspan;
+		ex.setLayoutData(gd);
+		return ex;
+	}
+	
+}
---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/propertypages/VMCapabilitiesPropertyPage.java
@@ -0,0 +1,248 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jdt.internal.debug.ui.propertypages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jdi.internal.VirtualMachineImpl;
+import org.eclipse.jdt.internal.debug.core.model.JDIDebugElement;
+import org.eclipse.jdt.internal.debug.core.model.JDIDebugTarget;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jdt.internal.debug.ui.SWTUtil;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.dialogs.PropertyPage;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.eclipse.ui.forms.widgets.SharedScrolledComposite;
+
+/**
+ * This class provides a properties page displaying all of the capabilities
+ * of the VM asscoiated with the selected <code>IDebugTarget</code> or <code>IProcess</code>
+ * 
+ * @since 3.3
+ */
+public class VMCapabilitiesPropertyPage extends PropertyPage {
+
+	/**
+	 * Provides a scollable area for the expansion composites
+	 */
+	class ScrollPain extends SharedScrolledComposite {
+		public ScrollPain(Composite parent) {
+			super(parent, SWT.V_SCROLL | SWT.H_SCROLL);
+			setExpandHorizontal(true);
+			setExpandVertical(true);
+			GridLayout layout = new GridLayout();
+			layout.marginHeight = 0;
+			layout.marginWidth = 0;
+			setLayout(layout);
+			GridData gd = new GridData(GridData.FILL_BOTH);
+			gd.horizontalIndent = 0;
+			gd.verticalIndent = 0;
+			setLayoutData(gd);
+		}
+	}
+	
+	private List fExpandedComps;
+	private static final String EXPANDED_STATE = "vmc_expanded_state"; //$NON-NLS-1$
+	
+	/**
+	 * Constructor 
+	 */
+	public VMCapabilitiesPropertyPage() {
+		fExpandedComps = new ArrayList();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.preference.PreferencePage#createContents(org.eclipse.swt.widgets.Composite)
+	 */
+	protected Control createContents(Composite parent) {
+		noDefaultAndApplyButton();
+		final ScrollPain scomp = new ScrollPain(parent);
+		final Composite comp = new Composite(scomp, SWT.NONE);
+		comp.setLayout(new GridLayout(2, true));
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		comp.setLayoutData(gd);
+		scomp.setContent(comp);
+		VirtualMachineImpl vm = getVM();
+		if(vm == null) {
+			SWTUtil.createLabel(comp, PropertyPageMessages.VMCapabilitiesPropertyPage_0, 2);
+		}
+		else {
+			SWTUtil.createLabel(comp, PropertyPageMessages.VMCapabilitiesPropertyPage_1+vm.version(), 2);
+			SWTUtil.createVerticalSpacer(comp, 1);
+		//General capabilities
+			ExpandableComposite general = createExpandibleComposite(comp, ExpandableComposite.TWISTIE | ExpandableComposite.CLIENT_INDENT, PropertyPageMessages.VMCapabilitiesPropertyPage_2, 2, GridData.FILL_HORIZONTAL);
+			fExpandedComps.add(general);
+			Composite general_inner = SWTUtil.createComposite(general, comp.getFont(), 2, 2, GridData.FILL_HORIZONTAL);
+			general.setClient(general_inner);
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_3, vm.canGetSyntheticAttribute());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_4, vm.canUseInstanceFilters());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_5, vm.canGetBytecodes());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_6, vm.canGetCurrentContendedMonitor());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_7, vm.canGetMonitorInfo());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_8, vm.canGetOwnedMonitorInfo());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_9, vm.canWatchFieldModification());
+			createCapabilityEntry(general_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_10, vm.canWatchFieldAccess());
+			
+		
+		//1.4 VM capabilities
+			ExpandableComposite onefour = createExpandibleComposite(comp, ExpandableComposite.TWISTIE | ExpandableComposite.CLIENT_INDENT, PropertyPageMessages.VMCapabilitiesPropertyPage_11, 2, GridData.FILL_HORIZONTAL);
+			fExpandedComps.add(onefour);
+			Composite onefour_inner = SWTUtil.createComposite(onefour, comp.getFont(), 2, 2, GridData.FILL_HORIZONTAL);
+			onefour.setClient(onefour_inner);
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_12, vm.canAddMethod());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_13, vm.canGetSourceDebugExtension());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_14, vm.canPopFrames());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_15, vm.canRedefineClasses());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_16, vm.canUnrestrictedlyRedefineClasses());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_17, vm.canRequestVMDeathEvent());
+			createCapabilityEntry(onefour_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_18, vm.canSetDefaultStratum());
+		
+		//1.6 VM capabilities
+			ExpandableComposite onesix = createExpandibleComposite(comp, ExpandableComposite.TWISTIE | ExpandableComposite.CLIENT_INDENT, PropertyPageMessages.VMCapabilitiesPropertyPage_19, 2, GridData.FILL_HORIZONTAL);
+			fExpandedComps.add(onesix);
+			Composite onesix_inner = SWTUtil.createComposite(onesix, comp.getFont(), 2, 2, GridData.FILL_HORIZONTAL);
+			onesix.setClient(onesix_inner);
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_20, vm.canUseSourceNameFilters());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_21, vm.canForceEarlyReturn());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_22, vm.canGetMonitorFrameInfo());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_23, vm.canGetClassFileVersion());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_24, vm.canGetMethodReturnValues());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_25, vm.canRequestMonitorEvents());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_26, vm.canGetInstanceInfo());
+			createCapabilityEntry(onesix_inner, PropertyPageMessages.VMCapabilitiesPropertyPage_27, vm.canGetConstantPool());
+			
+		//restore expansion state
+			restoreExpansionState();
+		}
+		return comp;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 */
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+	}
+	
+	/**
+	 * Returns the VM from the debug target
+	 * @return the VM form the element
+	 */
+	private VirtualMachineImpl getVM() {
+		Object obj = getElement();
+		if(obj instanceof JDIDebugElement) {
+			return (VirtualMachineImpl) ((JDIDebugTarget) obj).getVM();
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns a new capabiltiy entry for a specified group
+	 * @param parent the parent group to add this entry to
+	 * @param label the text for the label
+	 * @param enabled the checked state of the check button
+	 */
+	private void createCapabilityEntry(Composite parent, String label, boolean enabled) {
+		SWTUtil.createCheckButton(parent, null, enabled).setEnabled(false);
+		SWTUtil.createLabel(parent, label, 1);
+	}
+	
+	/**
+	 * Creates an ExpandibleComposite widget
+	 * @param parent the parent to add this widget to
+	 * @param style the style for ExpandibleComposite expanding handle, and layout
+	 * @param label the label for the widget
+	 * @param hspan how many columns to span in the parent
+	 * @param fill the fill style for the widget
+	 * @return a new ExpandibleComposite widget
+	 */
+	private ExpandableComposite createExpandibleComposite(Composite parent, int style, String label, int hspan, int fill) {
+		ExpandableComposite ex = SWTUtil.createExpandibleComposite(parent, style, label, hspan, fill);
+		ex.addExpansionListener(new ExpansionAdapter() {
+			public void expansionStateChanged(ExpansionEvent e) {
+				ScrollPain sp = getParentScrollPane((ExpandableComposite) e.getSource());
+				if(sp != null) {
+					sp.reflow(true);
+				}
+			}
+		});
+		return ex;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.preference.PreferencePage#doGetPreferenceStore()
+	 */
+	protected IPreferenceStore doGetPreferenceStore() {
+		return JDIDebugUIPlugin.getDefault().getPreferenceStore();
+	}
+	
+	/**
+	 * save the expansion state for next time, this only happens when the user sleectes the OK button when closing the dialog
+	 */
+	private void persistExpansionState() {
+		IPreferenceStore store = getPreferenceStore();
+		if(store != null) {
+			for (int i = 0; i < fExpandedComps.size(); i++) {
+				store.setValue(EXPANDED_STATE+i, ((ExpandableComposite) fExpandedComps.get(i)).isExpanded());
+			}
+		}
+	}
+	
+	/**
+	 * restore the expansion state
+	 */
+	private void restoreExpansionState() {
+		IPreferenceStore store = getPreferenceStore();
+		if(store == null) {
+			((ExpandableComposite)fExpandedComps.get(0)).setExpanded(true);
+		}
+		else {
+			ExpandableComposite ex;
+			for (int i = 0; i < fExpandedComps.size(); i++) {
+				ex = (ExpandableComposite) fExpandedComps.get(i);
+				ex.setExpanded(store.getBoolean(EXPANDED_STATE+i));
+			}
+		}
+	}
+		
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.preference.PreferencePage#performOk()
+	 */
+	public boolean performOk() {
+		boolean ok = super.performOk();
+		persistExpansionState();
+		return ok;
+	}
+
+	/**
+	 * Finds the parent ScrollPain that needs to be notified that it should reFlow to show the new elements
+	 * @param comp the initial comp
+	 * @return the parent or null, in this case though, we will never return null
+	 */
+	private ScrollPain getParentScrollPane(Composite comp) {
+		Control parent = comp.getParent();
+		while(parent != null && !(parent instanceof ScrollPain)) {
+			parent = parent.getParent();
+		}
+		if(parent != null) {
+			return (ScrollPain)parent;
+		}
+		return null;
+	}
+}
