13c697554227d624d5840509be5ae4a1da00edb8
Darin Wright <darin>
2006-10-26 17:00:50 +0000

    Bug 162366 org.eclipse.jdt.debug.tests.testSimpleStepFilter/testInactiveStepFilter depend on VM behavior

--- org.eclipse.jdt.debug.tests/testprograms/StepFilterThree.java
+++ org.eclipse.jdt.debug.tests/testprograms/StepFilterThree.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,7 +16,7 @@ public class StepFilterThree {
 	}
 
 	void go() {
-		System.out.println("StepFilterThree.go()");
+		for (int i = 0; i < 10; i++);
 	}
 }
 
--- org.eclipse.jdt.debug.tests/testprograms/StepFilterTwo.java
+++ org.eclipse.jdt.debug.tests/testprograms/StepFilterTwo.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ public class StepFilterTwo {
 	}
 	
 	void test() {
-		System.out.println("StepFilterTwo.test()");
+		for (int i = 0; i < 10; i++);
 	}
 }
 
