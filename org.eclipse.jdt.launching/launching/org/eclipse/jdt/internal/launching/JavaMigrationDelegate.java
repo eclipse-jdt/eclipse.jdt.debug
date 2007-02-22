4b16a89f657cc8d23c5a355432f1ca3e6bddf7e4
Darin Wright <darin>
2007-02-22 22:06:57 +0000

    Bug 175202 Java launch config migration delegate

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JavaMigrationDelegate.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/JavaMigrationDelegate.java
@@ -10,8 +10,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.launching;
 
-import java.util.ArrayList;
-
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -47,32 +45,54 @@ public class JavaMigrationDelegate implements ILaunchConfigurationMigrationDeleg
 	 * @see org.eclipse.debug.core.ILaunchConfigurationMigrationDelegate#isCandidate()
 	 */
 	public boolean isCandidate(ILaunchConfiguration candidate) throws CoreException {
-		if(candidate.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING).equals(EMPTY_STRING)) {
+		String pName = candidate.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING);
+		if(pName.equals(EMPTY_STRING)) {
 			return false;
 		}
-		if(!candidate.getAttribute(IJavaLaunchConfigurationConstants.ATTR_CONTEXT_LAUNCHING_MIGRATED, false)) {
-			return true;
-		}
-		IResource[] mappedResources = candidate.getMappedResources();
-		if(mappedResources != null && mappedResources.length > 0) {
+		if (!isAvailable(pName)) {
 			return false;
 		}
-		return true;
+		IResource[] mapped = candidate.getMappedResources();
+		IResource target = getResource(candidate);
+		if (target == null) {
+			return mapped == null;
+		} else {
+			if (mapped == null) {
+				return true;
+			} else {
+				if (mapped.length != 1) {
+					return true;
+				} else {
+					return !target.equals(mapped[0]);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Returns whether the given project is available.
+	 * 
+	 * @param projectName project name
+	 * @return whether the project exists and is open
+	 */
+	private boolean isAvailable(String projectName) {
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		return project.exists() && project.isOpen();
 	}
 
 	/**
-	 * Returns an array of the associated <code>IResource</code>s for the specified launch configuration candidate
+	 * Returns the associated <code>IResource</code> for the specified launch configuration
+	 * or <code>null</code> if none.
+	 * 
 	 * @param candidate the candidate to get the backing resource for
-	 * @return an array of associated <code>IResource</code>s or an empty array, never <code>null</code>
+	 * @return associated <code>IResource</code> or <code>null</code>
 	 * 
 	 * @since 3.3
-	 * EXPERIMENTAL
-	 * CONTEXTLAUNCHING 
 	 * 
 	 * @throws CoreException
 	 */
-	protected IResource[] getResourcesForCandidate(ILaunchConfiguration candidate) throws CoreException {
-		ArrayList res = new ArrayList();
+	protected IResource getResource(ILaunchConfiguration candidate) throws CoreException {
+		IResource resource = null;
 		String pname = candidate.getAttribute(IJavaLaunchConfigurationConstants.ATTR_PROJECT_NAME, EMPTY_STRING);
 		if(!EMPTY_STRING.equals(pname)) {
 			String tname = candidate.getAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, EMPTY_STRING);
@@ -84,41 +104,26 @@ public class JavaMigrationDelegate implements ILaunchConfigurationMigrationDeleg
 						tname = tname.replace('$', '.');
 						IType type = jproject.findType(tname);
 						if(type != null) {
-							Object o = type.getUnderlyingResource();
-							if(o != null) {
-								res.add(o);
-							}
-							else {
-								o = type.getAdapter(IResource.class);
-								if(o != null) {
-									res.add(o);
-								}
+							resource = type.getUnderlyingResource();
+							if(resource == null) {
+								resource = (IResource) type.getAdapter(IResource.class);
 							}
 						}
 					}
 				}
 			}
 		}
- 		return (IResource[]) res.toArray(new IResource[res.size()]);
+		return resource;
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.eclipse.debug.core.ILaunchConfigurationMigrationDelegate#migrate(org.eclipse.debug.core.ILaunchConfiguration)
 	 */
 	public void migrate(ILaunchConfiguration candidate) throws CoreException {
-		IResource[] resources = getResourcesForCandidate(candidate);
-		if(resources.length > 0) {
-			ArrayList mappings = new ArrayList();
-			for(int i = 0; i < resources.length; i++) {
-				if(!mappings.contains(resources[i])) {
-					mappings.add(resources[i]);
-				}
-			}
-			ILaunchConfigurationWorkingCopy wc = candidate.getWorkingCopy();
-			wc.setMappedResources((IResource[])mappings.toArray(new IResource[mappings.size()]));
-			wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_CONTEXT_LAUNCHING_MIGRATED, true);
-			wc.doSave();
-		}
+		IResource resource = getResource(candidate);
+		ILaunchConfigurationWorkingCopy wc = candidate.getWorkingCopy();
+		wc.setMappedResources(new IResource[]{resource});
+		wc.doSave();
 	}
 
 }
