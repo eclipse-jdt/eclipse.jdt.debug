9cbb8fca2ceed8b769ef7da3aed2e2fd8f290485
Curtis Windatt <cwindatt>
2007-04-27 19:24:58 +0000

    Bug 167426 - InstructionPointerContext keeps reference to closed editor

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/InstructionPointerTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/InstructionPointerTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -30,7 +30,7 @@ public class InstructionPointerTests extends AbstractDebugTest {
 	 * This test should be run last in the test suite.
 	 */
 	public void testInstructionPointerLeaks() {
-		assertEquals("Leaking instruction pointers", 0, InstructionPointerManager.getDefault().getCachedTargetCount());
+		assertEquals("Leaking instruction pointers", 0, InstructionPointerManager.getDefault().getInstructionPointerCount());
 	}
 
 }
