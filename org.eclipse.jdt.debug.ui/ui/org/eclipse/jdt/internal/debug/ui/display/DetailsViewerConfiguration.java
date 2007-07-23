347f59128219ca26ce3f9c9917829be0388251f5
Curtis Windatt <cwindatt>
2007-07-23 16:33:39 +0000

    Bug 174020 - Background colour makes variable value unreadable in variables view

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/display/DetailsViewerConfiguration.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/display/DetailsViewerConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,17 +10,29 @@
  *******************************************************************************/
 package org.eclipse.jdt.internal.debug.ui.display;
 
-
 import org.eclipse.jdt.internal.debug.ui.contentassist.CurrentValueContext;
 import org.eclipse.jdt.internal.debug.ui.contentassist.JavaDebugContentAssistProcessor;
-import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.ui.editors.text.TextSourceViewerConfiguration;
 
-public class DetailsViewerConfiguration extends DisplayViewerConfiguration {
+/**
+ * Java configuration for the source viewer in the detail pane.  Uses standard text editor
+ * settings, but adds the java content assist proposals.
+ *
+ */
+public class DetailsViewerConfiguration extends TextSourceViewerConfiguration {
 
-	/**
-	 * @see JDIViewerConfiguration#getContentAssistantProcessor()
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getContentAssistant(org.eclipse.jface.text.source.ISourceViewer)
 	 */
-	public IContentAssistProcessor getContentAssistantProcessor() {
-		return new JavaDebugContentAssistProcessor(new CurrentValueContext());
+	public IContentAssistant getContentAssistant(ISourceViewer sourceViewer) {
+		ContentAssistant assistant = new ContentAssistant();
+		assistant.setContentAssistProcessor(new JavaDebugContentAssistProcessor(new CurrentValueContext()),IDocument.DEFAULT_CONTENT_TYPE);
+		assistant.setContextInformationPopupOrientation(IContentAssistant.CONTEXT_INFO_ABOVE);
+		assistant.setInformationControlCreator(getInformationControlCreator(sourceViewer));
+		return assistant;
 	}
 }
