1566e9970f846119dfbcc9be93328c0b4f953346
Michael Rennie <mrennie>
2007-09-12 15:31:48 +0000

    Bug 201440 Improve Debug Manual Test Suite

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/ManualSuite.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/ManualSuite.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -45,6 +45,10 @@ public class ManualSuite extends TestSuite {
 	public ManualSuite() {
 		addTest(new TestSuite(ProjectCreationDecorator.class));
 		
+		/**
+		 * This test appears in the manual suite as wee need to be able to specify ports
+		 * and security settings to make sure the client can connect
+		 */
 		addTest(new TestSuite(RemoteJavaApplicationTests.class));
 
 	}
