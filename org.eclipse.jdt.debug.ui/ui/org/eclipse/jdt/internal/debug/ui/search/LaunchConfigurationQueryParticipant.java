a77eade6a48fbb8a8355b46239e7608d6cf3f0fd
Michael Rennie <mrennie>
2007-10-16 17:21:59 +0000

    Bug 57515 [search] java search participant for launch configs

---
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/search/LaunchConfigurationQueryParticipant.java
@@ -0,0 +1,271 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Ecliptical Software Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Ecliptical Software Inc. - initial API and implementation
+ *     IBM Canada - review initial contribution and commit
+ *******************************************************************************/
+package org.eclipse.jdt.internal.debug.ui.search;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubProgressMonitor;
+import org.eclipse.core.variables.VariablesPlugin;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.internal.ui.DebugUIPlugin;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.debug.ui.ILaunchGroup;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IType;
+import org.eclipse.jdt.core.search.IJavaSearchConstants;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jdt.ui.search.ElementQuerySpecification;
+import org.eclipse.jdt.ui.search.IMatchPresentation;
+import org.eclipse.jdt.ui.search.IQueryParticipant;
+import org.eclipse.jdt.ui.search.ISearchRequestor;
+import org.eclipse.jdt.ui.search.PatternQuerySpecification;
+import org.eclipse.jdt.ui.search.QuerySpecification;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.search.ui.text.Match;
+import org.eclipse.ui.PartInitException;
+
+/**
+ * This class provides a search participant to find class references in the 
+ * {@linkplain IJavaLaunchConfigurationConstants#ATTR_MAIN_TYPE_NAME} attribute
+ * of Java launch configurations
+ * 
+ * @since 3.4.0
+ */
+public class LaunchConfigurationQueryParticipant implements IQueryParticipant {
+
+	/**
+	 * Singleton instance of the UI participant for the configuration
+	 * search matches
+	 */
+	private UIParticipant uiParticipant;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.ui.search.IQueryParticipant#estimateTicks(org.eclipse.jdt.ui.search.QuerySpecification)
+	 */
+	public int estimateTicks(QuerySpecification query) {
+		if (isValid(query)) {
+			return 50;
+		}
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.ui.search.IQueryParticipant#getUIParticipant()
+	 */
+	public synchronized IMatchPresentation getUIParticipant() {
+		if (uiParticipant == null) {
+			uiParticipant = new UIParticipant();
+		}
+		return uiParticipant;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jdt.ui.search.IQueryParticipant#search(org.eclipse.jdt.ui.search.ISearchRequestor, org.eclipse.jdt.ui.search.QuerySpecification, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	public void search(ISearchRequestor requestor, QuerySpecification query, IProgressMonitor monitor) throws CoreException {
+		if (!isValid(query)) {
+			return;
+		}
+		if(monitor == null) {
+			monitor = new NullProgressMonitor();
+		}
+		monitor.beginTask("", 11); //$NON-NLS-1$
+		try {
+			Pattern pattern = null;
+			if (query instanceof ElementQuerySpecification) {
+				ElementQuerySpecification elementQuery = (ElementQuerySpecification) query;
+				IType type = (IType) elementQuery.getElement();
+				pattern = Pattern.compile(quotePattern(type.getFullyQualifiedName('$')));
+			}
+			if (query instanceof PatternQuerySpecification) {
+				PatternQuerySpecification patternQuery = (PatternQuerySpecification) query;
+				int flags = patternQuery.isCaseSensitive() ? 0
+						: Pattern.CASE_INSENSITIVE;
+				String quotedPattern = quotePattern(patternQuery.getPattern());
+				pattern = Pattern.compile(quotedPattern, flags);
+			}
+			if(monitor.isCanceled()) {
+				return;
+			}
+			monitor.worked(1);
+			searchLaunchConfigurations(requestor, pattern, new SubProgressMonitor(monitor, 7));
+		} finally {
+			monitor.done();
+		}
+	}
+
+	/**
+	 * Creates an adjusted pattern from the specified quote pattern
+	 * @param pattern
+	 * @return the original pattern adjusted to escape special chars in Java,and change Eclipse
+	 * search chars to Java regex chars
+	 */
+	private String quotePattern(String pattern) {
+		StringTokenizer t = new StringTokenizer(pattern, ".?*$", true); //$NON-NLS-1$
+		StringBuffer buf = new StringBuffer();
+		String token = null;
+		while (t.hasMoreTokens()) {
+			token = t.nextToken();
+			switch (token.charAt(0)) {
+				case '.': {
+					buf.append('\\');
+					break;
+				}
+				case '?':
+				case '*': {
+					buf.append('.');
+					break;
+				}
+				case '$': {
+					buf.append('\\');
+					break;
+				}
+			}
+			buf.append(token);
+		}
+		return buf.toString();
+	}
+
+	/**
+	 * Determines if the current query should be considered or not
+	 * @param query the current query
+	 * @return true if the query should be considered, false otherwise
+	 */
+	private boolean isValid(QuerySpecification query) {
+		switch(query.getLimitTo()) {
+			case IJavaSearchConstants.REFERENCES:
+			case IJavaSearchConstants.ALL_OCCURRENCES: {
+				return true;
+			}
+		}
+		if (query instanceof ElementQuerySpecification) {
+			return ((ElementQuerySpecification) query).getElement().getElementType() == IJavaElement.TYPE;
+		}
+		if (query instanceof PatternQuerySpecification) {
+			PatternQuerySpecification patternQuery = (PatternQuerySpecification) query;
+			switch (patternQuery.getSearchFor()) {
+				case IJavaSearchConstants.UNKNOWN:
+				case IJavaSearchConstants.TYPE:
+				case IJavaSearchConstants.CLASS:
+				case IJavaSearchConstants.CLASS_AND_INTERFACE:
+				case IJavaSearchConstants.CLASS_AND_ENUM: {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the success of the the pattern matching 
+	 * @param scope
+	 * @param config
+	 * @param workspaceRoot
+	 * @param pattern
+	 * @return true if there were matching elements for the pattern, false otherwise
+	 * @throws CoreException
+	 */
+	private boolean matches(ILaunchConfiguration config, Pattern pattern) throws CoreException {
+		if(!config.exists() || !config.getType().isPublic() || !DebugUIPlugin.doLaunchConfigurationFiltering(config)) {
+			return false;
+		}
+		String mainTypeName = config.getAttribute(IJavaLaunchConfigurationConstants.ATTR_MAIN_TYPE_NAME, (String) null);
+		if (mainTypeName == null) {
+			return false;
+		}
+		mainTypeName = VariablesPlugin.getDefault().getStringVariableManager().performStringSubstitution(mainTypeName);
+		return pattern.matcher(mainTypeName).matches();
+	}
+
+	/**
+	 * Searches for configurations matching the specified pattern
+	 * @param requestor
+	 * @param scope
+	 * @param pattern
+	 * @param monitor
+	 * @throws CoreException
+	 */
+	private void searchLaunchConfigurations(ISearchRequestor requestor, Pattern pattern, IProgressMonitor monitor) throws CoreException {
+		ILaunchConfiguration[] configs = DebugPlugin.getDefault().getLaunchManager().getLaunchConfigurations();
+		monitor.beginTask("Searching for launch configurations", configs.length); //$NON-NLS-1$
+		try {
+			for (int i = 0; i < configs.length; ++i) {
+				if (monitor.isCanceled()) {
+					return;
+				}
+				monitor.worked(1);
+				if (matches(configs[i], pattern)) {
+					requestor.reportMatch(new Match(configs[i], 0, 0));
+				}
+			}
+		} finally {
+			monitor.done();
+		}
+	}
+
+	/**
+	 * The UI participant for showing configuration matches in the search view
+	 * 
+	 * @since 3.4.0
+	 */
+	private static class UIParticipant implements IMatchPresentation {
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jdt.ui.search.IMatchPresentation#createLabelProvider()
+		 */
+		public ILabelProvider createLabelProvider() {
+			return DebugUITools.newDebugModelPresentation();
+		}
+
+		/* (non-Javadoc)
+		 * @see org.eclipse.jdt.ui.search.IMatchPresentation#showMatch(org.eclipse.search.ui.text.Match, int, int, boolean)
+		 */
+		public void showMatch(Match match, int currentOffset, int currentLength, boolean activate) throws PartInitException {
+			Object o = match.getElement();
+			if (o instanceof ILaunchConfiguration) {
+				if (activate) {
+					try {
+						ILaunchConfiguration config = (ILaunchConfiguration) o;
+						Set modes =  config.getType().getSupportedModeCombinations();
+						ILaunchGroup group = null;
+						Set mode = null;
+						for (Iterator iter = modes.iterator(); iter.hasNext();) {
+							mode = (Set) iter.next();
+							if(mode.size() == 1) {
+								group = DebugUITools.getLaunchGroup(config, (String) mode.iterator().next());
+								if (group != null) {
+									break;
+								}
+							}
+						}
+						if (group == null || !config.exists()) {
+							return;
+						}
+						DebugUITools.openLaunchConfigurationDialog(JDIDebugUIPlugin.getActiveWorkbenchShell(), config, group.getIdentifier(), Status.OK_STATUS);
+					}
+					catch(CoreException ce) {
+						JDIDebugUIPlugin.log(ce);
+					}
+				}
+			}
+		}
+	}
+}
