641de741cbc16ab5ab611829d098269fbf9a5cc5
Darin Wright <darin>
2008-01-14 16:54:46 +0000

    [r323] Bug 214581 - Library detection timeout should be longer

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -504,8 +504,8 @@ public class StandardVMType extends AbstractVMInstallType {
 			try {
 				p = Runtime.getRuntime().exec(cmdLine);
 				IProcess process = DebugPlugin.newProcess(new Launch(null, ILaunchManager.RUN_MODE, null), p, "Library Detection"); //$NON-NLS-1$
-				for (int i= 0; i < 200; i++) {
-					// Wait no more than 10 seconds (200 * 50 mils)
+				for (int i= 0; i < 600; i++) {
+					// Wait no more than 30 seconds (600 * 50 mils)
 					if (process.isTerminated()) {
 						break;
 					}
