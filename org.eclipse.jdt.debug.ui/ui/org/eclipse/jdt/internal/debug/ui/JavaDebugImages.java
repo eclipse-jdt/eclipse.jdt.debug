2a7414212bad0c21474037c65a3f40002bb3b8d5
Michael Rennie <mrennie>
2008-07-07 16:36:55 +0000

    Bug 231790 NPE when opening Run Configuration Dialog on linux/gtk

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JavaDebugImages.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JavaDebugImages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -224,7 +224,9 @@ public class JavaDebugImages {
         URL url = null;
         if (bundle != null){
             url = FileLocator.find(bundle, new Path(path), null);
-            desc = ImageDescriptor.createFromURL(url);
+            if(url != null) {
+            	desc = ImageDescriptor.createFromURL(url);
+            }
         }
         fgImageRegistry.put(key, desc);
     }
