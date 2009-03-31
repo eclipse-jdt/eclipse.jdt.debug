7360bfc266c341623e1e6cd3ab39826d01942b0d
Michael Rennie <mrennie>
2009-03-31 20:46:04 +0000

    updated copyrights

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/LauncherMessages.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/LauncherMessages.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- * IBM - Initial API and implementation
- * BEA - Daniel R Somerfield - Bug 88939
- * Remy Chi Jian Suen - Bug 221973
+ *  Copyright (c) 2000, 2009 IBM Corporation and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ * 
+ *  Contributors:
+ *  IBM - Initial API and implementation
+ *  BEA - Daniel R Somerfield - Bug 88939
+ *  Remy Chi Jian Suen - Bug 221973
  *******************************************************************************/
 package org.eclipse.jdt.internal.debug.ui.launcher;
 
