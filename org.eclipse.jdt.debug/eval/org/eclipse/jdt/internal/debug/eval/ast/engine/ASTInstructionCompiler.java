5b0a5d2c3f7393cf01f5fa3135f383980c22094f
Michael Rennie <mrennie>
2009-05-11 18:13:54 +0000

    Bug 275486 NPE while debugging.

--- org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/engine/ASTInstructionCompiler.java
+++ org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/engine/ASTInstructionCompiler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2008 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -473,6 +473,9 @@ public class ASTInstructionCompiler extends ASTVisitor {
 	 * Returns true if a storeInstruction() is needed after visiting the expression
 	 */
 	private boolean checkAutoBoxing(ITypeBinding valueBinding, ITypeBinding requestedBinding) {
+		if (valueBinding == null) {
+			return false; // unresolved
+		}
 		if (valueBinding.isPrimitive() == requestedBinding.isPrimitive()) {
 			return false;
 		}
