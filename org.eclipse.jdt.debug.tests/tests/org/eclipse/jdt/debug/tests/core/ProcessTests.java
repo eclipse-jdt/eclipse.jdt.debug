d1ae5c37b28df81f13184745d619ffdf5a0b799b
Darin Wright <darin>
2009-09-08 20:29:45 +0000

    Bug 261837 - RuntimeProcess should synchronize access to fTerminated and fExitValue

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/ProcessTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/ProcessTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,9 @@
 package org.eclipse.jdt.debug.tests.core;
 
 import org.eclipse.debug.core.DebugException;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.Launch;
 import org.eclipse.debug.core.model.ILineBreakpoint;
 import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.jdt.debug.core.IJavaThread;
@@ -49,5 +52,30 @@ public class ProcessTests extends AbstractDebugTest {
 			terminateAndRemove(thread);
 			removeAllBreakpoints();
 		}		
+	}
+	
+	/**
+	 * Tests that an already terminated java.lang.Process behaves properly when
+	 * wrapped in an IProcess.
+	 * 
+	 * @throws Exception
+	 */
+	public void testAlreadyTerminatedProcess() throws Exception {
+		Process process = DebugPlugin.exec(new String[]{"java"}, null);
+		boolean terminated = false;
+		int value = -1;
+		while (!terminated) {
+			try {
+				value = process.exitValue();
+				terminated = true;
+			} catch (IllegalThreadStateException e) {
+				process.getInputStream().read(new byte[1000], 0, 1000);
+				Thread.sleep(500);
+			}
+		}
+		Launch launch = new Launch(null, ILaunchManager.RUN_MODE, null);
+		IProcess iProcess = DebugPlugin.newProcess(launch, process, "Testing123");
+		assertTrue("Process should be terminated", iProcess.isTerminated());
+		assertEquals("Wrong exit value", value, iProcess.getExitValue());
 	}
 }
