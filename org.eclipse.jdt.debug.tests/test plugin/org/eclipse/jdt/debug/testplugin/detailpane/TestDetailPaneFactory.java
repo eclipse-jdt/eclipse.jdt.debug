c69d2585761b86d3a9193332943ea055bc3c662d
Michael Rennie <mrennie>
2010-01-21 20:16:24 +0000

    Bug 300406 -  Multi-select breakpoints causes strange detail pane

--- org.eclipse.jdt.debug.tests/test plugin/org/eclipse/jdt/debug/testplugin/detailpane/TestDetailPaneFactory.java
+++ org.eclipse.jdt.debug.tests/test plugin/org/eclipse/jdt/debug/testplugin/detailpane/TestDetailPaneFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -52,7 +52,7 @@ public class TestDetailPaneFactory implements IDetailPaneFactory {
 			if (selection.size() == 1 && selection.getFirstElement() instanceof IJavaVariable){
 				possibleIDs.add(SimpleDetailPane.ID);
 			}
-			if (selection.size() > 1){
+			if (selection.size() > 1 && selection.getFirstElement() instanceof String){
 				possibleIDs.add(TableDetailPane.ID);
 			}
 		}
