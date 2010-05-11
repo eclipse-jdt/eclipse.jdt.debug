9008d2714229a18eeb578cf2be68ff0294289245
Michael Rennie <mrennie>
2010-05-11 17:57:07 +0000

    Bug 311848 -  return statement missing in JavaBreakpointListenerProxy#installingBreakpoint

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/BreakpointListenerManager.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/BreakpointListenerManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -163,7 +163,7 @@ public class BreakpointListenerManager {
 		public int installingBreakpoint(IJavaDebugTarget target, IJavaBreakpoint breakpoint, IJavaType type) {
 			IJavaBreakpointListener delegate = getDelegate();
 			if (delegate != null) {
-				delegate.installingBreakpoint(target, breakpoint, type);
+				return delegate.installingBreakpoint(target, breakpoint, type);
 			}
 			return IJavaBreakpointListener.DONT_CARE;
 		}
