e8b2306f40d8759174d31cbcd304cfb57d5748ef
Dani Megert <dmegert>
2011-01-24 11:34:33 +0000

    Replaced wrong usage of "it's" with "its".

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/model/JDIReferenceListVariable.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/model/JDIReferenceListVariable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
+ * Copyright (c) 2007, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,12 +11,14 @@
 package org.eclipse.jdt.internal.debug.core.model;
 
 import org.eclipse.jdt.debug.core.IJavaObject;
+
 import org.eclipse.jdt.internal.debug.core.logicalstructures.JDIPlaceholderVariable;
 
 /**
- * A variable that stores a list of references.  Used to display reference information collected from the vm
- * using the 'all references' utility in Java 6.0.  This variable uses a <code>JDIReferenceListValue</code>
- * as its value.  It's children will be <code>JDIReferenceListEntryVariable</code>.
+ * A variable that stores a list of references. Used to display reference information collected from
+ * the vm using the 'all references' utility in Java 6.0. This variable uses a
+ * <code>JDIReferenceListValue</code> as its value. Its children will be
+ * <code>JDIReferenceListEntryVariable</code>.
  * 
  * @since 3.3
  * @see JDIReferenceListValue
