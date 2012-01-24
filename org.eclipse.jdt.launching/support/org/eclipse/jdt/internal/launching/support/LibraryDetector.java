326c18fbedc10203bac95c6399a022b8ea04e1a0
Dani Megert <dmegert>
2012-01-24 14:13:12 +0100

    Fixed bug 369480: launchingsupport.jar must be 1.1 compatible (was:1.4.2 JRE is not recognized in the latest M5 builds)

--- org.eclipse.jdt.launching/support/org/eclipse/jdt/internal/launching/support/LibraryDetector.java
+++ org.eclipse.jdt.launching/support/org/eclipse/jdt/internal/launching/support/LibraryDetector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -47,7 +47,7 @@ public class LibraryDetector {
 			System.out.print(System.getProperty("java.version")); //$NON-NLS-1$
 			System.out.print("|"); //$NON-NLS-1$
 			// get the boot class path - the property may vary for different vendors
-			Enumeration<Object> keys = System.getProperties().keys();
+			Enumeration keys = System.getProperties().keys();
 			boolean found = false;
 			while (keys.hasMoreElements()) {
 				String key = (String) keys.nextElement();
