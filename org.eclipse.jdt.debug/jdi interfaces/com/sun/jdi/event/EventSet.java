c68a0c1157263e5c96f3e96bcf84b331c54f3f1d
Mike Rennie <Michael_Rennie@ca.ibm.com>
2012-07-23 11:22:32 -0500

    Bug 383625 - JDT version of com.sun.jdi.event.EventSet doesn't extend java.util.set

--- org.eclipse.jdt.debug/jdi interfaces/com/sun/jdi/event/EventSet.java
+++ org.eclipse.jdt.debug/jdi interfaces/com/sun/jdi/event/EventSet.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,13 +10,13 @@
  *******************************************************************************/
 package com.sun.jdi.event;
 
-import java.util.Collection;
+import java.util.Set;
 
 import com.sun.jdi.Mirror;
 /**
  * See http://docs.oracle.com/javase/6/docs/jdk/api/jpda/jdi/com/sun/jdi/event/EventSet.html
  */
-public interface EventSet extends Mirror, Collection<Event> {
+public interface EventSet extends Mirror, Set<Event> {
 	public EventIterator eventIterator();
 	public void resume();
 	public int suspendPolicy();
