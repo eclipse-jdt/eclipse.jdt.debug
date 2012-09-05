b1c4d1bf43987166eb09b71cb584335481ef878e
Markus Keller <markus_keller@ch.ibm.com>
2012-09-05 11:01:09 -0500

    Bug 387504 - Bugs in program argument parsing (compared to command line)

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMRunner.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMRunner.java
@@ -84,32 +84,7 @@ public class StandardVMRunner extends AbstractVMRunner {
 	 * @return the command line label
 	 */
 	protected String renderCommandLine(String[] commandLine) {
-		if (commandLine.length < 1)
-			return ""; //$NON-NLS-1$
-		StringBuffer buf= new StringBuffer();
-		for (int i= 0; i < commandLine.length; i++) {
-			buf.append(' ');
-			char[] characters= commandLine[i].toCharArray();
-			StringBuffer command= new StringBuffer();
-			boolean containsSpace= false;
-			for (int j = 0; j < characters.length; j++) {
-				char character= characters[j];
-				if (character == '\"') {
-					command.append('\\');
-				} else if (character == ' ') {
-					containsSpace = true;
-				}
-				command.append(character);
-			}
-			if (containsSpace) {
-				buf.append('\"');
-				buf.append(command.toString());
-				buf.append('\"');
-			} else {
-				buf.append(command.toString());
-			}
-		}	
-		return buf.toString();
+		return DebugPlugin.renderArguments(commandLine, null);
 	}	
 	
 	/**
