5470662974f617a8a7e84478a71969e27c7971f7
Mike Rennie <Michael_Rennie@ca.ibm.com>
2013-04-01 13:01:43 -0500

    Bug 403700 - Selecting Ant Build launch configuration in External Tools Configuration dialog causes NPE

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/debug/ui/launchConfigurations/JavaJRETab.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/debug/ui/launchConfigurations/JavaJRETab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -411,7 +411,7 @@ public class JavaJRETab extends JavaLaunchTab {
 	}
 	
 	protected void selectJRE(String typeID, String vmName) {
-		if (typeID == null) {
+		if (typeID == null || vmName == null) {
 			fJREBlock.setPath(JavaRuntime.newDefaultJREContainerPath());
 		} else {
 			fJREBlock.setPath(JavaRuntime.newJREContainerPath(typeID, vmName));
