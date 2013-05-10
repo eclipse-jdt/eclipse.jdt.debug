b9d2b71ddad26f5c6a7feabeeba00b9686c9583d
Michael Rennie <Michael_Rennie@ca.ibm.com>
2013-05-10 15:13:08 -0500

    Bug 407666 - Remote debug label is missing space

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/SocketAttachConnector.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/SocketAttachConnector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -182,6 +182,9 @@ public class SocketAttachConnector implements IVMConnector {
 			}
 		}
 		StringBuffer buffer = new StringBuffer(name);
+		if(!"".equals(name)) { //$NON-NLS-1$
+			buffer.append(' ');
+		}
 		buffer.append('['); 
 		buffer.append(host);
 		buffer.append(':'); 
