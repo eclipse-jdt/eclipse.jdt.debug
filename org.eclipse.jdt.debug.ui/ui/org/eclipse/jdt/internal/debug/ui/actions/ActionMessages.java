d4649484ee4000714f673b5f272aeb14ed781f7e
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-02-13 15:09:27 -0600

    Bug 427075 - [1.8] Unable to set breakpoint in interface methods
    Change-Id: Ic291be9d89f0a5555b8663fdeedbfac8db985a25
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ActionMessages.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/ActionMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -126,7 +126,6 @@ public class ActionMessages extends NLS {
 	public static String ToggleBreakpointAdapter_4;
 	public static String ToggleBreakpointAdapter_5;
 	public static String ToggleBreakpointAdapter_6;
-	public static String ToggleBreakpointAdapter_7;
 	public static String ToggleBreakpointAdapter_9;
 	public static String ToggleBreakpointAdapter_confirmDeleteTitle;
 	public static String ToggleBreakpointAdapter_confirmDeleteMessage;
