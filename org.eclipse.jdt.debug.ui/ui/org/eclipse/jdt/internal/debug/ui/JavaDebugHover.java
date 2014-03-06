611d0b2c9ead4ce27bd4ce6ea8e9d0b5f4b5b46b
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-03-06 09:44:16 -0600

    Bug 427082 - [1.8] Hover does not show variable's value while debugging in lambda expression
    Change-Id: Ibec0605931dbf6f27f5f2f67953ead4c692c0e4f
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JavaDebugHover.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/JavaDebugHover.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -389,7 +389,8 @@ public class JavaDebugHover implements IJavaEditorTextHover, ITextHoverExtension
             						equal = true;
             					}
             				}
-            				if (equal) {
+            				// find variable if equal or method is a Lambda Method
+            				if (equal || method.isLambdaMethod()) {
             					return findLocalVariable(frame, var.getElementName());
             				}
             			}
