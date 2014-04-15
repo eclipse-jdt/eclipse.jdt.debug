0fb6a99346503e5e2c103954f6051f88c76f8c79
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-04-15 14:21:10 +0530

    Bug 385486 - problems when there are two classes with the same name in different projects

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/JavaDebugUtils.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/JavaDebugUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2013 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -340,10 +340,7 @@ public class JavaDebugUtils {
 		ISourceLocator sourceLocator = launch.getSourceLocator();
 		if (sourceLocator instanceof ISourceLookupDirector) {
 			ISourceLookupDirector director = (ISourceLookupDirector) sourceLocator;
-			Object[] objects = director.findSourceElements(object);
-			if (objects.length > 0) {
-				return objects[0];
-			}
+			return director.getSourceElement(object);
 		}
 		return null;
 	}
