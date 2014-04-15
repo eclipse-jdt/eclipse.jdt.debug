dfa0be7ae7f13fc371c9160a451c7afab4e982b5
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-04-15 14:35:57 +0530

    Bug 413605 - Random failures in Rename*Tests

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/refactoring/AbstractRefactoringDebugTest.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/refactoring/AbstractRefactoringDebugTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2013 IBM Corporation and others.
+ * Copyright (c) 2005, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -68,6 +68,7 @@ public class AbstractRefactoringDebugTest extends AbstractDebugTest {
 			ResourcesPlugin.getWorkspace().run(op, new NullProgressMonitor());
 			waitForBuild();
 			assertEquals(true, op.getValidationStatus().isOK());
+			assertEquals(true, op.getConditionStatus().isOK());
 		}
 		catch(ResourceException re) {
 			//try the test again - the tests reset the workspace to remove any half-moved / change files
