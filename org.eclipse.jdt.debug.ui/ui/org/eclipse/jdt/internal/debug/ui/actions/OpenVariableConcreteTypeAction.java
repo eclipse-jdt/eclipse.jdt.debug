b92d7407a8b4eeac055248d6215c27a52d877d6b
Sarika Sinha <sarika.sinha@in.ibm.com>
2014-05-12 11:36:22 -0500

    Bug 427110 - [1.8] Open Actual Type / Hierarchy in Variables view for lambda and method reference
    Change-Id: Id55c52a69f874b6060f5fb34f26e01cd39ef1990
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/OpenVariableConcreteTypeAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/OpenVariableConcreteTypeAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,11 +11,22 @@
 package org.eclipse.jdt.internal.debug.ui.actions;
 
 
+import java.util.Iterator;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.debug.core.model.IDebugElement;
 import org.eclipse.jdt.debug.core.IJavaType;
 import org.eclipse.jdt.debug.core.IJavaValue;
 import org.eclipse.jdt.debug.core.IJavaVariable;
+import org.eclipse.jdt.debug.ui.IJavaDebugUIConstants;
+import org.eclipse.jdt.internal.debug.core.model.JDIInterfaceType;
+import org.eclipse.jdt.internal.debug.core.model.JDIObjectValue;
+import org.eclipse.jdt.internal.debug.core.model.JDIVariable;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * Opens the concrete type of variable - i.e. it's value's actual type.
@@ -34,4 +45,38 @@ public class OpenVariableConcreteTypeAction extends OpenVariableTypeAction {
 		return null;
 	}
 	
+
+	@Override
+	public void run(IAction action) {
+		IStructuredSelection selection = getCurrentSelection();
+		if (selection == null) {
+			return;
+		}
+		Iterator<?> itr = selection.iterator();
+		try {
+			while (itr.hasNext()) {
+				Object element = itr.next();
+				if (element instanceof JDIVariable && ((JDIVariable) element).getJavaType() instanceof JDIInterfaceType) {
+					JDIObjectValue val = (JDIObjectValue) ((JDIVariable) element).getValue();
+					if (val.getJavaType().toString().contains("$$Lambda$")) { //$NON-NLS-1$
+						OpenVariableDeclaredTypeAction declaredAction = new OpenVariableDeclaredTypeAction();
+						declaredAction.setActivePart(action, getPart());
+						declaredAction.run(action);
+						return;
+					}
+				}
+				Object sourceElement = resolveSourceElement(element);
+				if (sourceElement != null) {
+						openInEditor(sourceElement);
+				} else {
+						IStatus status = new Status(IStatus.INFO, IJavaDebugUIConstants.PLUGIN_ID, IJavaDebugUIConstants.INTERNAL_ERROR, "Source not found", null); //$NON-NLS-1$
+						throw new CoreException(status);
+				}
+			}
+		}
+		catch (CoreException e) {
+			JDIDebugUIPlugin.statusDialog(e.getStatus());
+		}
+	}
+
 }
--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/OpenVariableConcreteTypeHierarchyAction.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/actions/OpenVariableConcreteTypeHierarchyAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,19 @@
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jdt.internal.debug.ui.actions;
+
+import java.util.Iterator;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jdt.debug.ui.IJavaDebugUIConstants;
+import org.eclipse.jdt.internal.debug.core.model.JDIInterfaceType;
+import org.eclipse.jdt.internal.debug.core.model.JDIObjectValue;
+import org.eclipse.jdt.internal.debug.core.model.JDIVariable;
+import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * Opens the concrete type hierarhcy of variable - i.e. it's value's actual type.
@@ -22,4 +35,37 @@ public class OpenVariableConcreteTypeHierarchyAction extends OpenVariableConcret
 	protected boolean isHierarchy() {
 		return true;
 	}	
+	
+	@Override
+	public void run(IAction action) {
+		IStructuredSelection selection = getCurrentSelection();
+		if (selection == null) {
+			return;
+		}
+		Iterator<?> itr = selection.iterator();
+		try {
+			while (itr.hasNext()) {
+				Object element = itr.next();
+				if (element instanceof JDIVariable && ((JDIVariable) element).getJavaType() instanceof JDIInterfaceType) {
+					JDIObjectValue val = (JDIObjectValue) ((JDIVariable) element).getValue();
+					if (val.getJavaType().toString().contains("$$Lambda$")) { //$NON-NLS-1$
+						OpenVariableDeclaredTypeAction declaredAction = new OpenVariableDeclaredTypeHierarchyAction();
+						declaredAction.setActivePart(action, getPart());
+						declaredAction.run(action);
+						return;
+					}
+				}
+				Object sourceElement = resolveSourceElement(element);
+				if (sourceElement != null) {
+					openInEditor(sourceElement);
+				} else {
+					IStatus status = new Status(IStatus.INFO, IJavaDebugUIConstants.PLUGIN_ID, IJavaDebugUIConstants.INTERNAL_ERROR, "Source not found", null); //$NON-NLS-1$
+					throw new CoreException(status);
+				}
+			}
+		}
+		catch (CoreException e) {
+			JDIDebugUIPlugin.statusDialog(e.getStatus());
+		}
+	}
 }
