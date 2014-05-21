27bcbe095fd456ea110ba07fb2f0d723d1140009
Jacob Saoumi <xman.augs@gmx.de>
2014-05-21 10:03:17 -0500

    Bug 434722 - error in ConditionalJump Instruction
    Signed-off-by: Jacob Saoumi <xman.augs@gmx.de>

--- org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/instructions/ConditionalJump.java
+++ org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/instructions/ConditionalJump.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,11 +7,14 @@
  * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Jacob Saoumi - Bug 434722 error in ConditionalJump Instruction
  *******************************************************************************/
 package org.eclipse.jdt.internal.debug.eval.ast.instructions;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.debug.core.IJavaObject;
 import org.eclipse.jdt.debug.core.IJavaPrimitiveValue;
+import org.eclipse.jdt.debug.core.IJavaValue;
 
 public class ConditionalJump extends Jump {
 	private boolean fJumpOnTrue;
@@ -25,7 +28,15 @@ public class ConditionalJump extends Jump {
 	 */
 	@Override
 	public void execute() throws CoreException {
-		IJavaPrimitiveValue condition = (IJavaPrimitiveValue) popValue();
+		IJavaValue conditionValue = popValue();
+		IJavaPrimitiveValue condition = null;
+		if (conditionValue instanceof IJavaPrimitiveValue) {
+			condition = (IJavaPrimitiveValue) conditionValue;
+		} else if (conditionValue instanceof IJavaObject) {
+			if (((IJavaObject) conditionValue).getJavaType().getName().equals("java.lang.Boolean")) { //$NON-NLS-1$
+				condition = (IJavaPrimitiveValue) ((IJavaObject) conditionValue).getField("value", false).getValue(); //$NON-NLS-1$
+			}
+		}
 
 		if (!(fJumpOnTrue ^ condition.getBooleanValue())) {
 			jump(fOffset);
