77ef36de6d2e9af03f0d318b0b0ba458aceefbb2
Markus Keller <markus_keller@ch.ibm.com>
2014-11-10 15:49:09 +0100

    Bug 450841: Update default URLs for attached Javadoc for JREs to https

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
@@ -757,19 +757,19 @@ public class StandardVMType extends AbstractVMInstallType {
 	public static URL getDefaultJavadocLocation(String version) {
 		try {
 			if (version.startsWith(JavaCore.VERSION_1_8)) {
-				return new URL("http://docs.oracle.com/javase/8/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/8/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_1_7)) {
-				return new URL("http://docs.oracle.com/javase/7/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/7/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_1_6)) {
-				return new URL("http://docs.oracle.com/javase/6/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/6/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_1_5)) {
-				return new URL("http://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_1_4)) {
 				// archived: http://download.oracle.com/javase/1.4.2/docs/api/
-				return new URL("http://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_1_3)) {
 				// archived: http://download.oracle.com/javase/1.3/docs/api/
-				return new URL("http://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/javase/1.5.0/docs/api/"); //$NON-NLS-1$
 			}
 		} catch (MalformedURLException e) {
 		}
