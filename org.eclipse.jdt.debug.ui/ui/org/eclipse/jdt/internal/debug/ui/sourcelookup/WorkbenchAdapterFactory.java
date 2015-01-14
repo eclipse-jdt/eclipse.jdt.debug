e644421b4528ce8a3c5bad32d7f02f37050a42bd
Sarika Sinha <sarika.sinha@in.ibm.com>
2015-01-14 22:25:13 +0100

    Fixed bug 56062: [source lookup] Duplicate source lookup should indicate full location of duplicate

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/sourcelookup/WorkbenchAdapterFactory.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/sourcelookup/WorkbenchAdapterFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,6 +11,7 @@
 package org.eclipse.jdt.internal.debug.ui.sourcelookup;
 
 import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.ui.model.IWorkbenchAdapter;
 
 /**
@@ -22,6 +23,9 @@ public class WorkbenchAdapterFactory implements IAdapterFactory {
 	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
 	 */
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
+		if (adaptableObject instanceof IJavaElement && adapterType.equals(IWorkbenchAdapter.class)) {
+			return new JavaDebugWorkbenchAdapter();
+		}
 		if (adapterType.equals(IWorkbenchAdapter.class)) {
 			return new WorkbenchAdapter();
 		}
