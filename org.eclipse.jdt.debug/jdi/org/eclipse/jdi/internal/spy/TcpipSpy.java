e502a8d96432694e51c5cf83bef1de7eef93c459
Sarika Sinha <sarika.sinha@in.ibm.com>
2015-03-14 08:17:34 +0530

    Bug 462118 - Compile log warning in JDT Debug build after 1.7 BREE change
    Change-Id: I9c0f5cd45799eedaddf6ff0ceebd657840da86c3

--- org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/spy/TcpipSpy.java
+++ org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/spy/TcpipSpy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -97,11 +97,10 @@ public class TcpipSpy extends Thread {
 			}
 		}
 		out.println();
-		try {
-			ServerSocket serverSock = new ServerSocket(inPort);
+		try (ServerSocket serverSock = new ServerSocket(inPort);
 			Socket inSock = serverSock.accept();
 			Socket outSock = new Socket(InetAddress.getByName(serverHost),
-					outPort);
+					outPort);){
 			new TcpipSpy(false, inSock.getInputStream(),
 					outSock.getOutputStream()).start();
 			new TcpipSpy(true, outSock.getInputStream(),
