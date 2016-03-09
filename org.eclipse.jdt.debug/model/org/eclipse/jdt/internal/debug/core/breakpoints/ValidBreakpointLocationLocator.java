ab2e1cfb7766ab0154afe58699f9a8b893149160
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-03-09 10:30:40 +0530

    Bug 485741 - [Breakpoints] Can't remove method breakpoint in second
    Change-Id: I63217a2a87c70c297aa13ad73b407d6d2f17eab7

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/ValidBreakpointLocationLocator.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/ValidBreakpointLocationLocator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2003, 2014 IBM Corporation and others.
+ * Copyright (c) 2003, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -994,6 +994,11 @@ public class ValidBreakpointLocationLocator extends ASTVisitor {
 	@Override
 	public boolean visit(MethodDeclaration node) {
 		if (visit(node, false)) {
+			if (node.getParent() instanceof AnonymousClassDeclaration){
+				fLocationType = LOCATION_NOT_FOUND;
+				fLocationFound = true;
+				return false;
+			}
 			if (fBestMatch) {
 				// check if we are on the line which contains the method name
 				int nameOffset = node.getName().getStartPosition();
