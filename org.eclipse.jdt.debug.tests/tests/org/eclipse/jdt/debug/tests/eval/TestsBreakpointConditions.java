9cf488b905d0fb7224e6d4e4c8ab8140fd5a7eef
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-07-13 10:13:58 +0530

    Bug 497509 - Cannot execute "throw new Exception()" expression during debug
    Change-Id: I4e80af93f37e0d544275f2da6d7262d78a1a029a

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/eval/TestsBreakpointConditions.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/eval/TestsBreakpointConditions.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBM Corporation and others.
+ * Copyright (c) 2015, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -56,6 +56,21 @@ public class TestsBreakpointConditions extends Tests {
 		try {
 			init();
 			IValue value = eval("System.out.println(\"test\");return true");
+			System.out.println(value);
+
+		}
+		finally {
+			end();
+		}
+
+	}
+	/*
+	 * To test throw as a last statement
+	 */
+	public void testCondition4() throws Throwable {
+		try {
+			init();
+			IValue value = eval("System.out.println(\"test\");throw new Exception(\"test\")");
 			System.out.println(value);
 
 		}
