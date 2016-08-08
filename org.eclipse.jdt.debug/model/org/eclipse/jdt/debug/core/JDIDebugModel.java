23ac170f3b1ad104b9d4d3f3a27baf1209eb385a
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-08-08 09:54:18 +0530

    Bug 40912 - [java debug] Debugger should show value of "last returned method"
    Change-Id: Ie62dace91ee9c60a97aa308ee33f54a8e43536bb

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/JDIDebugModel.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/debug/core/JDIDebugModel.java
@@ -104,6 +104,7 @@ public class JDIDebugModel {
 	/**
 	 * Preference key for specifying if the value returned or thrown should be displayed as variable after a "step return" or "step over" (if
 	 * supported by the vm)
+	 * @since 3.11
 	 */
 	public static final String PREF_SHOW_STEP_RESULT = getPluginIdentifier()
 			+ ".PREF_SHOW_STEP_RESULT"; //$NON-NLS-1$
