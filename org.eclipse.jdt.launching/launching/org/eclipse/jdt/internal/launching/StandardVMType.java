4d358bdb462ad5362a71631f494375891320b7f4
Sarika Sinha <sarika.sinha@in.ibm.com>
2016-10-21 15:06:52 +0530

    Bug 506252 - [9] Source attachment not added automatically for JDK 9
    Change-Id: I37754cd573f45a7422fb7886810ec381ad5199f5

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -214,6 +214,7 @@ public class StandardVMType extends AbstractVMInstallType {
 				String version = readReleaseVersion(javaHome);
 				if (JavaCore.compareJavaVersions(version, JavaCore.VERSION_1_8) > 0) {
 					info = new LibraryInfo(version, new String[0], new String[0], new String[0]);
+					LaunchingPlugin.setLibraryInfo(installPath, info);
 				} else {
 					info = generateLibraryInfo(javaHome, javaExecutable);
 					if (info == null) {
