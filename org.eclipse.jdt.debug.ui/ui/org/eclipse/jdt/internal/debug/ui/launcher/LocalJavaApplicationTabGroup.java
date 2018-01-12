a9bc530db65cc69e24565798fc3f6070e529693b
Axel Richard <axel.richard@obeo.fr>
2018-01-12 15:47:08 +0100

    Bug 529738 - Fix PrototypeTab typo
    Change-Id: Icf7919b3e5ccc8ae34c8ea5d9ac38557a55e46ae
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/LocalJavaApplicationTabGroup.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/launcher/LocalJavaApplicationTabGroup.java
@@ -21,7 +21,7 @@ import org.eclipse.debug.ui.EnvironmentTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
 import org.eclipse.debug.ui.ILaunchConfigurationTabGroup;
-import org.eclipse.debug.ui.ProtoypeTab;
+import org.eclipse.debug.ui.PrototypeTab;
 import org.eclipse.debug.ui.sourcelookup.SourceLookupTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaArgumentsTab;
 import org.eclipse.jdt.debug.ui.launchConfigurations.JavaClasspathTab;
@@ -62,7 +62,7 @@ public class LocalJavaApplicationTabGroup extends AbstractLaunchConfigurationTab
 			new SourceLookupTab(),
 			new EnvironmentTab(),
 			new CommonTab(),
-			new ProtoypeTab()
+			new PrototypeTab()
 		};
 		setTabs(tabs);
 	}
