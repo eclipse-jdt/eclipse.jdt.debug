c042fff17877059457f7fdd4d345c8743eba3938
Sarika Sinha <sarika.sinha@in.ibm.com>
2018-04-16 14:23:58 +0530

    Bug 509095 - Remote Java Application page: missing mnemonics
    Change-Id: I5bf0cd1fc81eb87c2648e3bde880829c40b2100f

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/debug/ui/launchConfigurations/JavaConnectTab.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/debug/ui/launchConfigurations/JavaConnectTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,6 +32,7 @@ import org.eclipse.jdt.internal.debug.ui.launcher.LauncherMessages;
 import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
 import org.eclipse.jdt.launching.IVMConnector;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jface.action.LegacyActionTools;
 import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditor;
@@ -362,7 +363,11 @@ public class JavaConnectTab extends AbstractJavaMainTab implements IPropertyChan
 			if (editor instanceof StringFieldEditor) {
 				String value = ((StringFieldEditor)editor).getStringValue();
 				if (!arg.isValid(value)) {
-					setErrorMessage(arg.label() + LauncherMessages.JavaConnectTab__is_invalid__5);
+					StringBuffer label = new StringBuffer(LegacyActionTools.removeMnemonics(arg.label()));
+					if (label.lastIndexOf(":") == label.length() - 1) { //$NON-NLS-1$
+						label = label.deleteCharAt(label.length() - 1);
+					}
+					setErrorMessage(label.toString() + LauncherMessages.JavaConnectTab__is_invalid__5);
 					return false;
 				}
 			}
