31566e2eed2ecae13a8e4b49f81dc77d098b72ba
Igor Fedorenko <igor@ifedorenko.com>
2018-10-05 23:34:21 -0400

    Bug 538636 - disable javaagent based on Object.class major_version
    ... also upgraded to ASM 6.2.1 (see bug 539082)
    
    Change-Id: I5fe1457929f8d6416a6f0434ee8414eb177eef8e
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- org.eclipse.jdt.launching.javaagent/src/main/java/org/eclipse/jdt/launching/internal/weaving/ClassfileTransformer.java
+++ org.eclipse.jdt.launching.javaagent/src/main/java/org/eclipse/jdt/launching/internal/weaving/ClassfileTransformer.java
@@ -23,12 +23,18 @@ public class ClassfileTransformer {
 	// must match JDIHelpers.STRATA_ID
 	private static final String STRATA_ID = "jdt"; //$NON-NLS-1$
 
+	/** max supported java class format major version, must match {@link #ASM_API} below **/
+	public static final int MAX_CLASS_MAJOR = Opcodes.V10;
+
+	/** supported ASM API version, must match {@link #MAX_CLASS_MAJOR} above */
+	private static final int ASM_API = Opcodes.ASM6;
+
 	public byte[] transform(byte[] classfileBuffer, final String location) {
 
 		final ClassReader r = new ClassReader(classfileBuffer, 0, classfileBuffer.length);
 		final ClassWriter w = new ClassWriter(0);
 
-		r.accept(new ClassVisitor(Opcodes.ASM5, w) {
+		r.accept(new ClassVisitor(ASM_API, w) {
 			@Override
 			public void visitSource(String source, String debug) {
 				String javaSource = source;
