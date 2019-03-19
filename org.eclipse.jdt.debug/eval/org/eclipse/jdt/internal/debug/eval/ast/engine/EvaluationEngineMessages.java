35add75462e80b5aee4a5c7bc86737eee389de08
Sarika Sinha <sarika.sinha@in.ibm.com>
2019-03-19 11:27:31 +0530

    Bug 545475 - [12] Remove JCP disclaimers from code, UI and marketplace
    Change-Id: Ia51855f1c73ef99979fbf12357ea739ee485529a

--- org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/engine/EvaluationEngineMessages.java
+++ org.eclipse.jdt.debug/eval/org/eclipse/jdt/internal/debug/eval/ast/engine/EvaluationEngineMessages.java
@@ -8,10 +8,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0
  *
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
- *
  * Contributors:
  * IBM - Initial API and implementation
  * Jesper Steen Moller - 427089: [1.8] Change value in Variables view with lambda or method reference
