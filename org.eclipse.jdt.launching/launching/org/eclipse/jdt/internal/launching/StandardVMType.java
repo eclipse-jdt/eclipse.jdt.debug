e7bdf3674d07f11a38a8d78825edaaa5d89f8760
Sarika Sinha <sarika.sinha@in.ibm.com>
2019-09-18 08:56:51 +0530

    Bug 547716 - [13] Java 13 API doc to be added for JRE library
    Change-Id: I802c6b87e9a13f29f93d3a5c87f3712cbaf8859f
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMType.java
@@ -845,8 +845,7 @@ public class StandardVMType extends AbstractVMInstallType {
 	public static URL getDefaultJavadocLocation(String version) {
 		try {
 			if (version.startsWith(JavaCore.VERSION_13)) {
-				// No URL yet for 13
-				return new URL("https://docs.oracle.com/en/java/javase/12/docs/api/"); //$NON-NLS-1$
+				return new URL("https://docs.oracle.com/en/java/javase/13/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_12)) {
 				return new URL("https://docs.oracle.com/en/java/javase/12/docs/api/"); //$NON-NLS-1$
 			} else if (version.startsWith(JavaCore.VERSION_11)) {
