de1307688f9b2671e8aeaa08e48569c665d34665
Sarika Sinha <sarika.sinha@in.ibm.com>
2020-03-17 10:08:59 +0530

    Bug 551500 - [14] Remove disclaimers from JDT Debug code base and merge Beta branch to Master Remove JCP Discalimers
    Change-Id: I9e8e8a2fab3646fcd14a0b38f5ad770a91f9212d

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/EECompilationParticipant.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/EECompilationParticipant.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2019 IBM Corporation and others.
+ * Copyright (c) 2008, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
- *
- * This is an implementation of an early-draft specification developed under the Java
- * Community Process (JCP) and is made available for testing and evaluation purposes
- * only. The code is not compatible with any specification of the JCP.
  *
  * SPDX-License-Identifier: EPL-2.0
  *
