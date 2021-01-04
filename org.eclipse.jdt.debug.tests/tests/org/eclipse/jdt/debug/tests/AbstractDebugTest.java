7bb5baf572c75ff3ea9c0951e499baa555ace24b
Sarika Sinha <sarika.sinha@in.ibm.com>
2021-01-04 23:51:18 +0530

    Bug 569914 - JDT Debug tests log has uncompleted AutoRegisterSchemeHandlersJob jobs
    Change-Id: I0128ae2c1723e64d322c47b32939ee96beaf8dbf

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -318,6 +318,11 @@ public abstract class AbstractDebugTest extends TestCase implements  IEvaluation
 	        	node.putBoolean(ResourcesPlugin.PREF_LIGHTWEIGHT_AUTO_REFRESH, true);
 	        	node.flush();
 	        }
+
+			IEclipsePreferences prefs = InstanceScope.INSTANCE.getNode("org.eclipse.urischeme");
+			if (prefs != null) {
+				prefs.putBoolean("skipAutoRegistration", true);
+			}
 	        loadedPrefs = true;
 		}
     }
