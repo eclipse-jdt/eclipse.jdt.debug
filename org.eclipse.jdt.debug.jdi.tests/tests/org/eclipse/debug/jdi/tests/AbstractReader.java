758aa89360e4c9eb222af7914c67d764107c408c
Thomas Wolf <thomas.wolf@paranor.ch>
2021-10-30 14:50:57 +0200

    Bug 576979 - Use LF as line endings
    AbstractReader used CR (not CRLF!) as line endings. Replace all CRs
    by LFs.
    
    Change-Id: I32c9ceca7886dbaa656c8aa222026fd52944ad9c
    Signed-off-by: Thomas Wolf <thomas.wolf@paranor.ch>
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.debug/+/187192
    Tested-by: JDT Bot <jdt-bot@eclipse.org>
    Reviewed-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/AbstractReader.java
+++ org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/AbstractReader.java
@@ -10,4 +10,50 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *******************************************************************************/package org.eclipse.debug.jdi.tests;/** * An abstract reader that continuously reads. */abstract public class AbstractReader {	protected String fName;	protected Thread fReaderThread;	protected boolean fIsStopping = false;	/**	 * Constructor	 * @param name	 */	public AbstractReader(String name) {		fName = name;	}	/**	 * Continuously reads. Note that if the read involves waiting	 * it can be interrupted and a InterruptedException will be thrown.	 */	abstract protected void readerLoop();	/**	 * Start the thread that reads events.	 *	 */	public void start() {		fReaderThread = new Thread(new Runnable() {			@Override			public void run() {				readerLoop();			}		}, fName);		fReaderThread.setDaemon(true);		fReaderThread.start();	}	/**	 * Tells the reader loop that it should stop.	 */	public void stop() {		fIsStopping = true;		if (fReaderThread != null) {			fReaderThread.interrupt();		}	}}
+ *******************************************************************************/
+package org.eclipse.debug.jdi.tests;
+
+/**
+ * An abstract reader that continuously reads.
+ */
+abstract public class AbstractReader {
+	protected String fName;
+	protected Thread fReaderThread;
+	protected boolean fIsStopping = false;
+
+	/**
+	 * Constructor
+	 * @param name
+	 */
+	public AbstractReader(String name) {
+		fName = name;
+	}
+	/**
+	 * Continuously reads. Note that if the read involves waiting
+	 * it can be interrupted and a InterruptedException will be thrown.
+	 */
+	abstract protected void readerLoop();
+	/**
+	 * Start the thread that reads events.
+	 *
+	 */
+	public void start() {
+		fReaderThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				readerLoop();
+			}
+		}, fName);
+		fReaderThread.setDaemon(true);
+		fReaderThread.start();
+	}
+	/**
+	 * Tells the reader loop that it should stop.
+	 */
+	public void stop() {
+		fIsStopping = true;
+		if (fReaderThread != null) {
+			fReaderThread.interrupt();
+		}
+	}
+}
