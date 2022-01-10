f5f784d8f97c4ff82bf2903e99fe4c4738f8653a
Sarika Sinha <sarika.sinha@in.ibm.com>
2022-01-10 16:20:57 +0530

    Bug 574831 - Computation of module name in Java launch use wrong module
    Change-Id: I587e95d6c30f270bdf22778140dc884e8ce947bc
    Reviewed-on: https://git.eclipse.org/r/c/jdt/eclipse.jdt.debug/+/189428
    Tested-by: JDT Bot <jdt-bot@eclipse.org>
    Reviewed-by: Sarika Sinha <sarika.sinha@in.ibm.com>

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/JavaLaunchDelegate.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/JavaLaunchDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corporation and others.
+ * Copyright (c) 2000, 2021 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -111,7 +111,7 @@ public class JavaLaunchDelegate extends AbstractJavaLaunchConfigurationDelegate 
 					if (modName != null && modName.length() > 0) {
 						String defaultModuleName = null;
 						String moduleName = configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_MODULE_NAME, defaultModuleName);
-						if (moduleName != null) {
+						if (moduleName != null && !moduleName.isEmpty()) {
 							runConfig.setModuleDescription(moduleName);
 						} else {
 							runConfig.setModuleDescription(modName);
