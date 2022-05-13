d6eb2e13363611b80c891f10d2cb69a0d01a3597
Sarika Sinha <sarika.sinha@in.ibm.com>
2022-05-13 20:44:05 +0530

    Add JavaSE-19 fixes https://github.com/eclipse-jdt/eclipse.jdt.debug/issues/49

--- org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/JavaRuntime.java
+++ org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/JavaRuntime.java
@@ -7,6 +7,9 @@
  * https://www.eclipse.org/legal/epl-2.0/
  *
  * SPDX-License-Identifier: EPL-2.0
+ * This is an implementation of an early-draft specification developed under the Java
+ * Community Process (JCP) and is made available for testing and evaluation purposes
+ * only. The code is not compatible with any specification of the JCP.
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
@@ -3364,8 +3367,11 @@ public final class JavaRuntime {
 				} else if (javaVersion.startsWith(JavaCore.VERSION_18)
 						&& (javaVersion.length() == JavaCore.VERSION_18.length() || javaVersion.charAt(JavaCore.VERSION_18.length()) == '.')) {
 					compliance = JavaCore.VERSION_18;
+				} else if (javaVersion.startsWith(JavaCore.VERSION_19)
+						&& (javaVersion.length() == JavaCore.VERSION_19.length() || javaVersion.charAt(JavaCore.VERSION_19.length()) == '.')) {
+					compliance = JavaCore.VERSION_18;
 				} else {
-					compliance = JavaCore.VERSION_18; // use latest by default
+					compliance = JavaCore.VERSION_19; // use latest by default
 				}
 
             	Hashtable<String, String> options= JavaCore.getOptions();
