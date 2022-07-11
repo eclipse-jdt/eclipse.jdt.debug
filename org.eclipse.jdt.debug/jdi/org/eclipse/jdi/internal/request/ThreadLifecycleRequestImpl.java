5fd32670165f95da6b8b8d00a16d2048717133c8
Jinbo Wang <jinbwan@microsoft.com>
2022-07-11 18:24:03 +0800

    Add test cases for Virtual Threads JDI capability
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>
    Change-Id: I5554cc7a6f8639d080a6a05e031443e9f33027c2
    
    Add pom.xml to run jdi.tests for Java 19
    
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>
    Change-Id: I13de4cf1dbc3535d4ca12b042d6fba92cfe917c8

--- org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/request/ThreadLifecycleRequestImpl.java
+++ org.eclipse.jdt.debug/jdi/org/eclipse/jdi/internal/request/ThreadLifecycleRequestImpl.java
@@ -17,25 +17,9 @@
  *******************************************************************************/
 package org.eclipse.jdi.internal.request;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 import org.eclipse.jdi.internal.VirtualMachineImpl;
 
 public abstract class ThreadLifecycleRequestImpl extends EventRequestImpl {
-	/**
-	 * PlatformThreadsOnly is a preview API of the Java platform. Preview features may be removed in a future release, or upgraded to permanent
-	 * features of the Java platform. Since JDWP version 19. For thread start and thread end events, restrict the events so they are only sent for
-	 * platform threads.
-	 *
-	 * @since 3.20
-	 */
-	public static final byte MODIF_KIND_PLATFORMTHREADSONLY = 13;
-
-	/**
-	 * Platform threads filter
-	 */
-	protected boolean fPlatformThreadsFilter = false;
 
 	protected ThreadLifecycleRequestImpl(String description, VirtualMachineImpl vmImpl) {
 		super(description, vmImpl);
@@ -49,22 +33,5 @@ public abstract class ThreadLifecycleRequestImpl extends EventRequestImpl {
 	public void addPlatformThreadsOnlyFilter() {
 		checkDisabled();
 		fPlatformThreadsFilter = true;
-	}
-
-	@Override
-	protected void writeModifiers(DataOutputStream outData) throws IOException {
-		super.writeModifiers(outData);
-		if (fPlatformThreadsFilter && supportsPlatformThreadsFilter()) {
-			writeByte(MODIF_KIND_PLATFORMTHREADSONLY, "modifier", modifierKindMap(), outData); //$NON-NLS-1$
-		}
-	}
-
-	/**
-	 * Returns whether JDWP supports platform threads filter (a 19 preview feature).
-	 *
-	 * @return whether JDWP supports platform threads filter
-	 */
-	private boolean supportsPlatformThreadsFilter() {
-		return ((VirtualMachineImpl) virtualMachine()).mayCreateVirtualThreads();
 	}
 }
