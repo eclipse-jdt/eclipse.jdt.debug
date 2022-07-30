eclipse-jdt/eclipse.jdt.debug

commit 91ca97311c9424031826c33bac43fcb9fbaac274

Bug 565902 - Support for argument files during launch
Adds a new setting to the arguments tab for writing arguments into a
file. This allows for endless long command lines when launching Java
applications.

Change-Id: I63702eaaee565cd87fa881d80d4719371920432e
Bug: 565902

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/CommandLineShortener.java
@@ -1,0 +1,224 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Gunnar Wagenknecht and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - copied from ClasspathShortener and simplified to shorten all arguments
+ *******************************************************************************/
+package org.eclipse.jdt.internal.launching;
+
+import static org.eclipse.jdt.internal.launching.LaunchingPlugin.LAUNCH_TEMP_FILE_PREFIX;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+import org.eclipse.jdt.launching.IVMInstall;
+import org.eclipse.jdt.launching.IVMInstall2;
+
+/**
+ * Shortens the command line by writing all commands into an arguments file.
+ */
+public class CommandLineShortener {
+	public static String getJavaVersion(IVMInstall vmInstall) {
+		if (vmInstall instanceof IVMInstall2) {
+			IVMInstall2 install = (IVMInstall2) vmInstall;
+			return install.getJavaVersion();
+		}
+		return null;
+	}
+
+	private final String javaVersion;
+	private final ILaunch launch;
+	private final String[] cmdLine;
+	private File processTempFilesDir;
+
+	private final List<File> processTempFiles = new ArrayList<>();
+
+	/**
+	 *
+	 * @param vmInstall
+	 *            the vm installation
+	 * @param launch
+	 *            the launch
+	 * @param cmdLine
+	 *            the command line (java executable + VM arguments + program arguments)
+	 * @param lastJavaArgumentIndex
+	 *            the index of the last java argument in cmdLine (next arguments if any are program arguments)
+	 * @param workingDir
+	 *            the working dir to use for the launched VM or null
+	 * @param envp
+	 *            array of strings, each element of which has environment variable settings in the format name=value, or null if the subprocess should
+	 *            inherit the environment of the current process.
+	 */
+	public CommandLineShortener(IVMInstall vmInstall, ILaunch launch, String[] cmdLine, File workingDir) {
+		this(getJavaVersion(vmInstall), launch, cmdLine, workingDir);
+	}
+
+	protected CommandLineShortener(String javaVersion, ILaunch launch, String[] cmdLine, File workingDir) {
+		Assert.isNotNull(javaVersion);
+		Assert.isNotNull(launch);
+		Assert.isNotNull(cmdLine);
+		this.javaVersion = javaVersion;
+		this.launch = launch;
+		this.cmdLine = cmdLine;
+		this.processTempFilesDir = workingDir != null ? workingDir : Paths.get(".").toAbsolutePath().normalize().toFile(); //$NON-NLS-1$
+	}
+
+	protected void addProcessTempFile(File file) {
+		processTempFiles.add(file);
+	}
+
+	protected File createArgumentFile(String[] cmdLine) throws CoreException {
+		try {
+			String timeStamp = getLaunchTimeStamp();
+			File argumentsFile = new File(processTempFilesDir, String.format(LAUNCH_TEMP_FILE_PREFIX
+					+ "%s-args-%s.txt", getLaunchConfigurationName(), timeStamp)); //$NON-NLS-1$
+
+			cmdLine = quoteForArgfile(cmdLine);
+
+			Files.write(argumentsFile.toPath(), Arrays.asList(cmdLine));
+			return argumentsFile;
+		} catch (IOException e) {
+			throw new CoreException(new Status(IStatus.ERROR, LaunchingPlugin.getUniqueIdentifier(), IStatus.ERROR, "Cannot create argument file", e)); //$NON-NLS-1$
+		}
+	}
+
+	String[] quoteForArgfile(String[] cmdLine) {
+		String[] quotedCmdLine = new String[cmdLine.length];
+		for (int i = 0; i < cmdLine.length; i++) {
+			String arg = cmdLine[i];
+			if (CommandLineQuoting.needsQuoting(arg)) {
+				StringBuilder escapedArg = new StringBuilder();
+				for (int j = 0; j < arg.length(); j++) {
+					char c = arg.charAt(j);
+					if (c == '\\') {
+						escapedArg.append('\\');
+					} else if (c == '\"') {
+						escapedArg.append('\\');
+					}
+					escapedArg.append(c);
+				}
+				arg = "\"" + escapedArg.toString() + "\""; //$NON-NLS-1$ //$NON-NLS-2$
+			}
+			quotedCmdLine[i] = arg;
+		}
+		return quotedCmdLine;
+	}
+
+	protected String getLaunchConfigurationName() {
+		return launch.getLaunchConfiguration().getName();
+	}
+
+	protected String getLaunchTimeStamp() {
+		String timeStamp = launch.getAttribute(DebugPlugin.ATTR_LAUNCH_TIMESTAMP);
+		if (timeStamp == null) {
+			timeStamp = Long.toString(System.currentTimeMillis());
+		}
+		return timeStamp;
+	}
+
+	/**
+	 * The files that were created while shortening the path. They can be deleted once the process is terminated
+	 *
+	 * @return created files
+	 */
+	public List<File> getProcessTempFiles() {
+		return new ArrayList<>(processTempFiles);
+	}
+
+	public File getProcessTempFilesDir() {
+		return processTempFilesDir;
+	}
+
+	/**
+	 * @return the original, unshortened command line
+	 */
+	public String[] getOriginalCmdLine() {
+		return cmdLine;
+	}
+
+	/**
+	 * @return <code>true</code> if the JVM supports launching with argument files, <code>false</code> otherwise
+	 */
+	protected boolean isArgumentFileSupported() {
+		return JavaCore.compareJavaVersions(javaVersion, JavaCore.VERSION_9) >= 0;
+	}
+
+	/**
+	 * The directory to use to create temp files needed when shortening the classpath. By default, the working directory is used
+	 *
+	 * The java.io.tmpdir should not be used on MacOs (does not work for classpath-only jars)
+	 *
+	 * @param processTempFilesDir
+	 */
+	public void setProcessTempFilesDir(File processTempFilesDir) {
+		this.processTempFilesDir = processTempFilesDir;
+	}
+
+	/**
+	 * Writes the command line into an arguments file and returns the shortened command line.
+	 *
+	 * @return a shortened command line
+	 * @throws CoreException
+	 */
+	public String[] shortenCommandLine() throws CoreException {
+		List<String> fullCommandLine = new ArrayList<>(Arrays.asList(cmdLine));
+		List<String> shortCommandLine = new ArrayList<>();
+
+		shortCommandLine.add(fullCommandLine.remove(0));
+
+		File argumentFile = createArgumentFile(fullCommandLine.toArray(new String[fullCommandLine.size()]));
+		addProcessTempFile(argumentFile);
+		shortCommandLine.add("@" + argumentFile.getAbsolutePath());//$NON-NLS-1$
+
+		return shortCommandLine.toArray(new String[shortCommandLine.size()]);
+	}
+
+	/**
+	 * Indicates if the command line {@link #isArgumentFileSupported() can} and should be shortened.
+	 * <p>
+	 * The command line should only be shortened if at least Java 9 is used and the launch is configured to do so.
+	 * </p>
+	 *
+	 * @return <code>true</code> if {@link #isArgumentFileSupported()} returns <code>true</code> and command line should be shortened,
+	 *         <code>false</code> otherwise
+	 * @throws CoreException
+	 */
+	public boolean shouldShortenCommandLine() throws CoreException {
+		if (!isArgumentFileSupported()) {
+			return false;
+		}
+
+		if (cmdLine.length < 2) {
+			// no need to shorten if it's just the program argument
+			return false;
+		}
+
+		ILaunchConfiguration configuration = launch.getLaunchConfiguration();
+		if (configuration != null) {
+			return configuration.getAttribute(IJavaLaunchConfigurationConstants.ATTR_USE_ARGFILE, false);
+		}
+
+		return false;
+	}
+}

commit 38d95929cbf43e347817909154f805343fb23c78

Don't use nested argfiles to launch Java processes (#84)
This is fix for regression from
91ca97311c9424031826c33bac43fcb9fbaac274, see [1].

The Oracle's tool guide [2] explicitly states:

> Use of the at sign (@) to recursively interpret files isn’t supported.

That was obviously not considered while bug 565902 patching [1].

Changed the code in the way that is either uses explicit argfiles option
if enabled in the launch config, or tries to shorten command line with
more sophisticated approach that also supports Java versions without
@argfiles support (Java < 9). Regression tests added.

[1] https://bugs.eclipse.org/bugs/show_bug.cgi?id=565902
[2] https://docs.oracle.com/javase/9/tools/java.htm#JSWOR-GUID-4856361B-8BFD-4964-AE84-121F5F6CF111

Fixes https://github.com/eclipse-jdt/eclipse.jdt.debug/issues/84

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/ClasspathShortener.java
@@ -54,7 +54,7 @@ import org.eclipse.jdt.launching.IVMInstall2;
  * env variable. The modulepath is replaced by an argument file if necessary.
  *
  */
-public class ClasspathShortener {
+public class ClasspathShortener implements IProcessTempFileCreator {
 	private static final String CLASSPATH_ENV_VAR_PREFIX = "CLASSPATH="; //$NON-NLS-1$
 	public static final int ARG_MAX_LINUX = 2097152;
 	public static final int ARG_MAX_WINDOWS = 32767;
@@ -136,11 +136,7 @@ public class ClasspathShortener {
 		return cmdLine.toArray(new String[cmdLine.size()]);
 	}
 
-	/**
-	 * The files that were created while shortening the path. They can be deleted once the process is terminated
-	 *
-	 * @return created files
-	 */
+	@Override
 	public List<File> getProcessTempFiles() {
 		return new ArrayList<>(processTempFiles);
 	}

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/CommandLineShortener.java
@@ -41,7 +41,7 @@ import org.eclipse.jdt.launching.IVMInstall2;
 /**
  * Shortens the command line by writing all commands into an arguments file.
  */
-public class CommandLineShortener {
+public class CommandLineShortener implements IProcessTempFileCreator {
 	public static String getJavaVersion(IVMInstall vmInstall) {
 		if (vmInstall instanceof IVMInstall2) {
 			IVMInstall2 install = (IVMInstall2) vmInstall;
@@ -153,11 +153,7 @@ public class CommandLineShortener {
 		return timeStamp;
 	}
 
-	/**
-	 * The files that were created while shortening the path. They can be deleted once the process is terminated
-	 *
-	 * @return created files
-	 */
+	@Override
 	public List<File> getProcessTempFiles() {
 		return new ArrayList<>(processTempFiles);
 	}

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/IProcessTempFileCreator.java
@@ -1,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Andrey Loskutov (loskutov@gmx.de) and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Andrey Loskutov (loskutov@gmx.de) - extracted interface
+ *******************************************************************************/
+package org.eclipse.jdt.internal.launching;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Internal interface to access process temp files
+ */
+interface IProcessTempFileCreator {
+
+	/**
+	 * Necessary files that were created for starting the process. They can be deleted once the process is terminated
+	 *
+	 * @return created files
+	 */
+	List<File> getProcessTempFiles();
+
+}

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMDebugger.java
@@ -25,7 +25,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
@@ -272,29 +271,28 @@ public class StandardVMDebugger extends StandardVMRunner {
 			return null;
 		}
 		File workingDir = getWorkingDir(config);
-		ClasspathShortener classpathShortener = new ClasspathShortener(fVMInstance, launch, cmdLine, lastVMArgumentIndex, workingDir, envp);
-		if (classpathShortener.shortenCommandLineIfNecessary()) {
-			cmdLine = classpathShortener.getCmdLine();
-			envp = classpathShortener.getEnvp();
+		CommandDetails cmd = new CommandDetails();
+		CommandLineShortener commandLineShortener = new CommandLineShortener(fVMInstance, launch, cmdLine, workingDir);
+		if (commandLineShortener.shouldShortenCommandLine()) {
+			cmdLine = commandLineShortener.shortenCommandLine();
+			cmd.setCommandLineShortener(commandLineShortener);
+		} else {
+			ClasspathShortener classpathShortener = new ClasspathShortener(fVMInstance, launch, cmdLine, lastVMArgumentIndex, workingDir, envp);
+			if (classpathShortener.shortenCommandLineIfNecessary()) {
+				cmdLine = classpathShortener.getCmdLine();
+				envp = classpathShortener.getEnvp();
+			}
+			cmd.setCommandLineShortener(classpathShortener);
 		}
-
 		String[] newCmdLine = validateCommandLine(launch.getLaunchConfiguration(), cmdLine);
 		if (newCmdLine != null) {
 			cmdLine = newCmdLine;
 		}
 		subMonitor.worked(1);
 
-		CommandLineShortener commandLineShortener = new CommandLineShortener(fVMInstance, launch, newCmdLine, workingDir);
-		if (commandLineShortener.shouldShortenCommandLine()) {
-			cmdLine = commandLineShortener.shortenCommandLine();
-		}
-
-		CommandDetails cmd = new CommandDetails();
 		cmd.setCommandLine(cmdLine);
 		cmd.setEnvp(envp);
 		cmd.setWorkingDir(workingDir);
-		cmd.setClasspathShortener(classpathShortener);
-		cmd.setCommandLineShortener(commandLineShortener);
 		cmd.setPort(port);
 		return cmd;
 
@@ -366,9 +364,9 @@ public class StandardVMDebugger extends StandardVMRunner {
 					}
 					process.setAttribute(DebugPlugin.ATTR_ENVIRONMENT, buff.toString());
 				}
-				if (!cmdDetails.getClasspathShortener().getProcessTempFiles().isEmpty()
-						|| !cmdDetails.getCommandLineShortener().getProcessTempFiles().isEmpty()) {
-					String tempFiles = Stream.concat(cmdDetails.getClasspathShortener().getProcessTempFiles().stream(), cmdDetails.getCommandLineShortener().getProcessTempFiles().stream()).map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator));
+				List<File> processTempFiles = cmdDetails.getCommandLineShortener().getProcessTempFiles();
+				if (!processTempFiles.isEmpty()) {
+					String tempFiles = processTempFiles.stream().map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator));
 					process.setAttribute(LaunchingPlugin.ATTR_LAUNCH_TEMP_FILES, tempFiles);
 				}
 				subMonitor.worked(1);

org.eclipse.jdt.launching/launching/org/eclipse/jdt/internal/launching/StandardVMRunner.java
@@ -24,7 +24,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -367,8 +366,7 @@ public class StandardVMRunner extends AbstractVMRunner {
 		private String[] commandLine;
 		private String[] envp;
 		private File workingDir;
-		private ClasspathShortener classpathShortener;
-		private CommandLineShortener commandLineShortener;
+		private IProcessTempFileCreator commandLineShortener;
 		private int port;
 
 		public String[] getEnvp() {
@@ -395,19 +393,11 @@ public class StandardVMRunner extends AbstractVMRunner {
 			this.workingDir = workingDir;
 		}
 
-		public ClasspathShortener getClasspathShortener() {
-			return classpathShortener;
-		}
-
-		public void setClasspathShortener(ClasspathShortener classpathShortener) {
-			this.classpathShortener = classpathShortener;
-		}
-
-		public CommandLineShortener getCommandLineShortener() {
+		public IProcessTempFileCreator getCommandLineShortener() {
 			return commandLineShortener;
 		}
 
-		public void setCommandLineShortener(CommandLineShortener commandLineShortener) {
+		public void setCommandLineShortener(IProcessTempFileCreator commandLineShortener) {
 			this.commandLineShortener = commandLineShortener;
 		}
 
@@ -494,25 +484,27 @@ public class StandardVMRunner extends AbstractVMRunner {
 		String[] cmdLine= new String[arguments.size()];
 		arguments.toArray(cmdLine);
 		File workingDir = getWorkingDir(config);
-		ClasspathShortener classpathShortener = new ClasspathShortener(fVMInstance, launch, cmdLine, lastVMArgumentIndex, workingDir, envp);
-		if (classpathShortener.shortenCommandLineIfNecessary()) {
-			cmdLine = classpathShortener.getCmdLine();
-			envp = classpathShortener.getEnvp();
+		CommandDetails cmd = new CommandDetails();
+		CommandLineShortener commandLineShortener = new CommandLineShortener(fVMInstance, launch, cmdLine, workingDir);
+		if (commandLineShortener.shouldShortenCommandLine()) {
+			cmdLine = commandLineShortener.shortenCommandLine();
+			cmd.setCommandLineShortener(commandLineShortener);
+		} else {
+			ClasspathShortener classpathShortener = new ClasspathShortener(fVMInstance, launch, cmdLine, lastVMArgumentIndex, workingDir, envp);
+			if (classpathShortener.shortenCommandLineIfNecessary()) {
+				cmdLine = classpathShortener.getCmdLine();
+				envp = classpathShortener.getEnvp();
+			}
+			cmd.setCommandLineShortener(classpathShortener);
 		}
 		String[] newCmdLine = validateCommandLine(launchConfiguration, cmdLine);
 		if (newCmdLine != null) {
 			cmdLine = newCmdLine;
 		}
-		CommandLineShortener commandLineShortener = new CommandLineShortener(fVMInstance, launch, newCmdLine, workingDir);
-		if (commandLineShortener.shouldShortenCommandLine()) {
-			cmdLine = commandLineShortener.shortenCommandLine();
-		}
-		CommandDetails cmd = new CommandDetails();
+
 		cmd.setCommandLine(cmdLine);
 		cmd.setEnvp(envp);
 		cmd.setWorkingDir(workingDir);
-		cmd.setClasspathShortener(classpathShortener);
-		cmd.setCommandLineShortener(commandLineShortener);
 		subMonitor.worked(1);
 		return cmd;
 	}
@@ -555,9 +547,9 @@ public class StandardVMRunner extends AbstractVMRunner {
 			Arrays.sort(envp);
 			process.setAttribute(DebugPlugin.ATTR_ENVIRONMENT, String.join(String.valueOf('\n'), envp));
 		}
-		if (!cmdDetails.getClasspathShortener().getProcessTempFiles().isEmpty()
-				|| !cmdDetails.getCommandLineShortener().getProcessTempFiles().isEmpty()) {
-			String tempFiles = Stream.concat(cmdDetails.getClasspathShortener().getProcessTempFiles().stream(), cmdDetails.getCommandLineShortener().getProcessTempFiles().stream()).map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator));
+		List<File> processTempFiles = cmdDetails.getCommandLineShortener().getProcessTempFiles();
+		if (!processTempFiles.isEmpty()) {
+			String tempFiles = processTempFiles.stream().map(File::getAbsolutePath).collect(Collectors.joining(File.pathSeparator));
 			process.setAttribute(LaunchingPlugin.ATTR_LAUNCH_TEMP_FILES, tempFiles);
 		}
 		subMonitor.worked(1);

org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/launching/LongClassPathTests.java
@@ -30,6 +30,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
@@ -38,6 +39,8 @@ import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.debug.core.IJavaThread;
+import org.eclipse.jdt.debug.testplugin.DebugElementKindEventWaiter;
+import org.eclipse.jdt.debug.testplugin.DebugEventWaiter;
 import org.eclipse.jdt.debug.testplugin.JavaProjectHelper;
 import org.eclipse.jdt.debug.tests.AbstractDebugTest;
 import org.eclipse.jdt.internal.launching.LaunchingPlugin;
@@ -55,11 +58,11 @@ import junit.framework.TestSuite;
  *
  */
 public class LongClassPathTests extends AbstractDebugTest {
-	private static final String MAIN_TYPE_NAME = "test.classpath.Main";
-	private static final IPath CLASSPATH_PROJECT_CONTENT_PATH = new Path("testresources/classpathProject");
-	private IJavaProject javaProject;
-	private ILaunchConfiguration launchConfiguration;
-	private IJavaThread thread;
+	protected static final String MAIN_TYPE_NAME = "test.classpath.Main";
+	protected static final IPath CLASSPATH_PROJECT_CONTENT_PATH = new Path("testresources/classpathProject");
+	protected IJavaProject javaProject;
+	protected ILaunchConfiguration launchConfiguration;
+	protected IJavaThread thread;
 
 	public LongClassPathTests(String name) {
 		super(name);
@@ -100,7 +103,7 @@ public class LongClassPathTests extends AbstractDebugTest {
 	 */
 	public void testVeryLongClasspathWithClasspathOnlyJar() throws Exception {
 		// Given
-		javaProject = createJavaProjectClone("testVeryLongClasspathWithClasspathOnlyJar", CLASSPATH_PROJECT_CONTENT_PATH.toString(), JavaProjectHelper.JAVA_SE_1_6_EE_NAME, true);
+		javaProject = createJavaProjectClone("test ä VeryLongClasspathWithClasspathOnlyJar", CLASSPATH_PROJECT_CONTENT_PATH.toString(), JavaProjectHelper.JAVA_SE_1_6_EE_NAME, true);
 		launchConfiguration = createLaunchConfigurationStopInMain(javaProject, MAIN_TYPE_NAME);
 		int minClasspathLength = 300000;
 		setLongClasspath(javaProject, minClasspathLength);
@@ -182,7 +185,7 @@ public class LongClassPathTests extends AbstractDebugTest {
 		resumeAndExit(thread);
 	}
 
-	private Optional<File> getTempFile(ILaunch launch) {
+	protected Optional<File> getTempFile(ILaunch launch) {
 		IProcess process = launch.getProcesses()[0];
 		String tempFile = process.getAttribute(LaunchingPlugin.ATTR_LAUNCH_TEMP_FILES);
 		if (tempFile == null) {
@@ -191,7 +194,7 @@ public class LongClassPathTests extends AbstractDebugTest {
 		return Optional.of(new File(tempFile));
 	}
 
-	private boolean isArgumentFileSupported(ILaunchConfiguration launchConfiguration) throws CoreException {
+	protected boolean isArgumentFileSupported(ILaunchConfiguration launchConfiguration) throws CoreException {
 		IVMInstall vmInstall = JavaRuntime.computeVMInstall(launchConfiguration);
 		if (vmInstall instanceof AbstractVMInstall) {
 			AbstractVMInstall install = (AbstractVMInstall) vmInstall;
@@ -209,7 +212,7 @@ public class LongClassPathTests extends AbstractDebugTest {
 		return configurationWorkingCopy.doSave();
 	}
 
-	private ILaunchConfiguration createLaunchConfigurationStopInMain(IJavaProject javaProject, String mainTypeName) throws Exception, CoreException {
+	protected ILaunchConfiguration createLaunchConfigurationStopInMain(IJavaProject javaProject, String mainTypeName) throws Exception, CoreException {
 		ILaunchConfiguration launchConfiguration;
 		launchConfiguration = createLaunchConfiguration(javaProject, mainTypeName);
 		ILaunchConfigurationWorkingCopy wc = launchConfiguration.getWorkingCopy();
@@ -218,7 +221,7 @@ public class LongClassPathTests extends AbstractDebugTest {
 		return launchConfiguration;
 	}
 
-	private void setLongClasspath(IJavaProject javaProject, int minClassPathLength) throws Exception {
+	protected void setLongClasspath(IJavaProject javaProject, int minClassPathLength) throws Exception {
 		StringBuilder sb = new StringBuilder();
 		List<IClasspathEntry> classpathEntries = new ArrayList<>();
 		int i = 0;
@@ -240,4 +243,17 @@ public class LongClassPathTests extends AbstractDebugTest {
 		javaProject.setRawClasspath(classpathEntries.toArray(new IClasspathEntry[classpathEntries.size()]), null);
 	}
 
+	/**
+	 * Increased timeout
+	 *
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected IJavaThread launchAndSuspend(ILaunchConfiguration config) throws Exception {
+		DebugEventWaiter waiter = new DebugElementKindEventWaiter(DebugEvent.SUSPEND, IJavaThread.class);
+		waiter.setTimeout(DEFAULT_TIMEOUT * 2);
+		waiter.setEnableUIEventLoopProcessing(enableUIEventLoopProcessingInWaiter());
+		Object suspendee = launchAndWait(config, waiter);
+		return (IJavaThread) suspendee;
+	}
 }

org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/launching/LongCommandLineTests.java
@@ -1,0 +1,112 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Andrey Loskutov (loskutov@gmx.de) and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Andrey Loskutov (loskutov@gmx.de) - initial implementation
+ *******************************************************************************/
+package org.eclipse.jdt.debug.tests.launching;
+
+import static org.junit.Assume.assumeTrue;
+
+import java.io.File;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.debug.testplugin.JavaProjectHelper;
+import org.eclipse.jdt.launching.IJavaLaunchConfigurationConstants;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Test JVM startup with long classpath / system properties by explicitly enabling {@link IJavaLaunchConfigurationConstants.ATTR_USE_ARGFILE} option
+ * in the "Arguments" part of the process launch configuration
+ */
+public class LongCommandLineTests extends LongClassPathTests {
+
+	static final int MIN_CLASSPATH_LENGTH = 300000;
+	boolean createLongArguments;
+
+	public LongCommandLineTests(String name) {
+		super(name);
+	}
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite();
+		suite.addTest(new LongCommandLineTests("testVeryLongClasspathWithArgumentFile"));
+		suite.addTest(new LongCommandLineTests("testVeryLongSystemPropertiesWithArgumentFile"));
+		return suite;
+	}
+
+	/*
+	 * Test will create lot of "-Dfoo=12345" system arguments for the started JVM and check if the startup works with argument file
+	 */
+	public void testVeryLongSystemPropertiesWithArgumentFile() throws Exception {
+		createLongArguments = true;
+
+		javaProject = createJavaProjectClone("test ä VeryLongSystemPropertiesWithArgumentFile", CLASSPATH_PROJECT_CONTENT_PATH.toString(), JavaProjectHelper.JAVA_SE_9_EE_NAME, true);
+		launchConfiguration = createLaunchConfigurationStopInMain(javaProject, MAIN_TYPE_NAME);
+		assumeTrue(isArgumentFileSupported(launchConfiguration));
+
+		// Given
+		waitForBuild();
+
+		// When
+		thread = launchAndSuspend(launchConfiguration);
+
+		// Then
+		File tempFile = getTempFile(thread.getLaunch()).orElseThrow(() -> new RuntimeException("No temp file"));
+		assertTrue("No temp file created: " + tempFile, tempFile.exists());
+		assertTrue("Unexpected temp file name: " + tempFile, tempFile.getName().endsWith(".txt"));
+		String valueString = doEval(thread, "java.lang.management.ManagementFactory.getRuntimeMXBean().getInputArguments().toString()").getValueString();
+		assertTrue("Unexpected small system arguments list: " + valueString, valueString.length() >= MIN_CLASSPATH_LENGTH);
+
+		// When
+		resumeAndExit(thread);
+
+		// Then
+		if (!Platform.getOS().equals(Platform.OS_WIN32)) {
+			// On windows, temp file deletion may fail
+			assertFalse(tempFile.exists());
+		}
+	}
+
+	private void setLongVmArgumentsList(ILaunchConfigurationWorkingCopy wc, int minArgumentsLength) throws Exception {
+		final String keyArgs = IJavaLaunchConfigurationConstants.ATTR_VM_ARGUMENTS;
+		String args = wc.getAttribute(keyArgs, "");
+		StringBuilder sb = new StringBuilder(args);
+		while (sb.length() < minArgumentsLength) {
+			sb.append(" ");
+			sb.append("-Dfoo=1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890");
+		}
+		args += sb.toString();
+		wc.setAttribute(keyArgs, args);
+	}
+
+	@Override
+	protected ILaunchConfiguration createLaunchConfigurationStopInMain(IJavaProject javaProject, String mainTypeName) throws Exception, CoreException {
+		ILaunchConfiguration launchConfiguration;
+		launchConfiguration = createLaunchConfiguration(javaProject, mainTypeName);
+		ILaunchConfigurationWorkingCopy wc = launchConfiguration.getWorkingCopy();
+		wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_STOP_IN_MAIN, true);
+		// Always use argfile option
+		wc.setAttribute(IJavaLaunchConfigurationConstants.ATTR_USE_ARGFILE, true);
+		// Only set if required by particular test
+		if (createLongArguments) {
+			setLongVmArgumentsList(wc, MIN_CLASSPATH_LENGTH);
+		}
+		launchConfiguration = wc.doSave();
+		return launchConfiguration;
+	}
+
+}