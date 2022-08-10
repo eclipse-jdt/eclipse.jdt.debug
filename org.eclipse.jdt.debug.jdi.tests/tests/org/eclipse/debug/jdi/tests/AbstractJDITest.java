629d79b36ac76b4058990afb18307e8f62e8bcf9
Sarika Sinha <sarika.sinha@in.ibm.com>
2022-08-10 19:06:26 +0530

    Support Eclipse as Java Vendor fixes https://github.com/eclipse-jdt/eclipse.jdt.debug/issues/103

--- org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/AbstractJDITest.java
+++ org.eclipse.jdt.debug.jdi.tests/tests/org/eclipse/debug/jdi/tests/AbstractJDITest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -935,7 +935,8 @@ public abstract class AbstractJDITest extends TestCase {
 		String targetAddress = System.getProperty("java.home");
 		String vmLauncherName;
 		if (vmVendor != null
-			&& (vmVendor.indexOf("Sun") > -1 || vmVendor.indexOf("Oracle") > -1 || vmVendor.indexOf("Apple") > -1)
+				&& (vmVendor.indexOf("Sun") > -1 || vmVendor.indexOf("Oracle") > -1 || vmVendor.indexOf("Apple") > -1
+						|| vmVendor.indexOf("Eclipse") > -1)
 			&& vmVersion != null) {
 			vmLauncherName = "SunVMLauncher";
 		} else if (
