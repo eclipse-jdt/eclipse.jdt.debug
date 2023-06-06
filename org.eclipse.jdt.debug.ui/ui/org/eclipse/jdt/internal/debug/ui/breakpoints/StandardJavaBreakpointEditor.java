50ba72a78d26e14809ea6d1ea7d3a7b6ab502e33
Jörg Kubitz <jkubitz-eclipse@gmx.de>
2023-06-06 13:09:24 +0200

    Avoid wrapping primitives for toString()
    see https://rules.sonarsource.com/java/RSPEC-2131

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/breakpoints/StandardJavaBreakpointEditor.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/breakpoints/StandardJavaBreakpointEditor.java
@@ -192,7 +192,7 @@ public class StandardJavaBreakpointEditor extends AbstractJavaBreakpointEditor {
 			enabled = true;
 			int hitCount = breakpoint.getHitCount();
 			if (hitCount > 0) {
-				text = Integer.valueOf(hitCount).toString();
+				text = Integer.toString(hitCount);
 				hasHitCount = true;
 			}
 			suspendThread= breakpoint.getSuspendPolicy() == IJavaBreakpoint.SUSPEND_THREAD;
