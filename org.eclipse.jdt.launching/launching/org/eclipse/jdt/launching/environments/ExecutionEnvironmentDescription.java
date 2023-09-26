eclipse-jdt/eclipse.jdt.debug

commit cc2c11c5c55636a71ab20d1d0805dbd7c5baece5

Add a ExecutionEnvironmentDescription(Map) constructor
This allows creating ExecutionEnvironmentDescription directly from a
Map, without the need to write the Map content to a file.

org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/environments/ExecutionEnvironmentDescription.java
@@ -195,7 +195,19 @@ public final class ExecutionEnvironmentDescription {
 	 * @throws CoreException if unable to read or parse the file
 	 */
 	public ExecutionEnvironmentDescription(File eeFile) throws CoreException {
-		this.fProperties = loadProperties(eeFile);
+		this.fProperties = resolveEEHome(loadProperties(eeFile));
+	}
+
+	/**
+	 * Creates an execution environment description based on the given execution environment description properties. The contained properties are
+	 * defined in <code>https://wiki.eclipse.org/Execution_Environment_Descriptions</code>.
+	 *
+	 * @param eeProperties execution environment description properties
+	 * @throws CoreException if unable to read or parse the file
+	 * @since 3.21
+	 */
+	public ExecutionEnvironmentDescription(Map<String, String> eeProperties) {
+		this.fProperties = resolveEEHome(new LinkedHashMap<>(eeProperties)); // defensive copy
 	}
 
 	/**
@@ -375,10 +387,13 @@ public final class ExecutionEnvironmentDescription {
 					eeFile.getPath() }), e));
 		}
 		properties.putIfAbsent(EE_HOME, eeFile.getParentFile().getAbsolutePath());
-		// resolve things with ${ee.home} in them
-		String eeHome = properties.get(EE_HOME);
-		properties.replaceAll((k, value) -> value != null ? resolveHome(value, eeHome) : ""); //$NON-NLS-1$
 		return properties;
+	}
+
+	private static Map<String, String> resolveEEHome(Map<String, String> eeProperties) { // resolve things with ${ee.home} in them
+		String eeHome = eeProperties.get(EE_HOME);
+		eeProperties.replaceAll((k, value) -> value != null ? resolveHome(value, eeHome) : ""); //$NON-NLS-1$
+		return eeProperties;
 	}
 
 	/**

org.eclipse.jdt.launching/META-INF/MANIFEST.MF
@@ -2,7 +2,7 @@ Manifest-Version: 1.0
 Bundle-ManifestVersion: 2
 Bundle-Name: %pluginName
 Bundle-SymbolicName: org.eclipse.jdt.launching; singleton:=true
-Bundle-Version: 3.20.200.qualifier
+Bundle-Version: 3.21.0.qualifier
 Bundle-Activator: org.eclipse.jdt.internal.launching.LaunchingPlugin
 Bundle-Vendor: %providerName
 Bundle-Localization: plugin

org.eclipse.jdt.launching/pom.xml
@@ -18,7 +18,7 @@
   </parent>
   <groupId>org.eclipse.jdt</groupId>
   <artifactId>org.eclipse.jdt.launching</artifactId>
-  <version>3.20.200-SNAPSHOT</version>
+  <version>3.21.0-SNAPSHOT</version>
   <packaging>eclipse-plugin</packaging>
   
   <build>

commit c5fb9277b10d027a84f7e63ee9fbed5f5a26bccf

Javadoc error fix for new ExecutionEnvironmentDescription constructor
See https://download.eclipse.org/eclipse/downloads/drops4/I20230925-1800/compilelogs/jdt.doc.isv.javadoc.txt

../../../eclipse.jdt.debug/org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/environments/ExecutionEnvironmentDescription.java:206:
error: exception not thrown: org.eclipse.core.runtime.CoreException
	 * @throws CoreException if unable to read or parse the file
	           ^

Regression from cc2c11c5c55636a71ab20d1d0805dbd7c5baece5

org.eclipse.jdt.launching/launching/org/eclipse/jdt/launching/environments/ExecutionEnvironmentDescription.java
@@ -203,7 +203,6 @@ public final class ExecutionEnvironmentDescription {
 	 * defined in <code>https://wiki.eclipse.org/Execution_Environment_Descriptions</code>.
 	 *
 	 * @param eeProperties execution environment description properties
-	 * @throws CoreException if unable to read or parse the file
 	 * @since 3.21
 	 */
 	public ExecutionEnvironmentDescription(Map<String, String> eeProperties) {