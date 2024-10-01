8133adf5cad79bf443be847da5da71bd9d51a2d0
Christian Schima <eclipse@schima24.de>
2024-10-01 15:22:55 +0200

    Bugfix for #518 (Setting a breakpoint inside lambda with object).

--- org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/ValidBreakpointLocationLocator.java
+++ org.eclipse.jdt.debug/model/org/eclipse/jdt/internal/debug/core/breakpoints/ValidBreakpointLocationLocator.java
@@ -1066,9 +1066,15 @@ public class ValidBreakpointLocationLocator extends ASTVisitor {
 
 						}
 					}
+
+				// Lambda body can be a block (which is handled above) or an (arbitrary) expression. So maybe the
+				// following cases are insufficient and should be replaced by handling the general
+				// org.eclipse.jdt.core.dom.Expression.
 				} else if (body instanceof LambdaExpression) {
 					body.accept(this);
 				} else if (body instanceof MethodInvocation) {
+					body.accept(this);
+				} else if (body instanceof ClassInstanceCreation) {
 					body.accept(this);
 				}
 			}
