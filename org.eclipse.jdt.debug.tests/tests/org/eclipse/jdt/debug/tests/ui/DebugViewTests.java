7d77f37b4e19b995b48f50daab60693771382716
Zsombor Gegesy <gzsombor@gmail.com>
2025-01-03 16:12:09 +0100

    Add new UI test for stack frame grouping

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/ui/DebugViewTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/ui/DebugViewTests.java
@@ -19,6 +19,7 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.model.IStackFrame;
 import org.eclipse.debug.ui.IDebugUIConstants;
 import org.eclipse.jdt.debug.core.IJavaStackFrame;
+import org.eclipse.jdt.debug.core.IJavaStackFrame.Category;
 import org.eclipse.jdt.debug.core.IJavaThread;
 import org.eclipse.jdt.debug.ui.IJavaDebugUIConstants;
 import org.eclipse.jdt.internal.debug.ui.JDIDebugUIPlugin;
@@ -224,6 +225,34 @@ public class DebugViewTests extends AbstractDebugViewTests {
 		String breakpointMethodName = "breakpointMethod";
 		int expectedBreakpointHitsCount = 1;
 		doTestWrongSelection(iterations, typeName, breakpointMethodName, expectedBreakpointHitsCount);
+	}
+
+	public void testStackFrameGrouppingAndColors() throws Exception {
+		IJavaThread thread = null;
+		try {
+			thread = runCodeUntilBreakpoint("StackFrameColoring", "breakpointMethod");
+			assertNotNull("thread", thread);
+			var selectedStackFrame = assertStackFrameIsSelected("breakpointMethod");
+			var parentItem = selectedStackFrame.getParentItem();
+			var allFrames = parentItem.getItems();
+			assertNotNull("all frames", allFrames);
+			assertEquals("frame[0]", "StackFrameColoring.breakpointMethod() line: 34", allFrames[0].getText());
+			assertEquals("frame[0] - production", IJavaStackFrame.Category.PRODUCTION, getStackFrameCategory(allFrames, 0));
+			assertEquals("frame[1]", "1 collapsed frames", allFrames[1].getText());
+			assertTrue("frame[2]", allFrames[2].getText().contains("apply(Object) line: not available"));
+			assertEquals("frame[2] - production", IJavaStackFrame.Category.PRODUCTION, getStackFrameCategory(allFrames, 2));
+			assertEquals("frame[3]", "7 collapsed frames", allFrames[3].getText());
+			assertEquals("frame[4]", "StackFrameColoring.run() line: 29", allFrames[4].getText());
+			assertEquals("frame[4] - production", IJavaStackFrame.Category.PRODUCTION, getStackFrameCategory(allFrames, 4));
+			assertEquals("frame[5]", "StackFrameColoring.main(String[]) line: 22", allFrames[5].getText());
+			assertEquals("frame[5] - production", IJavaStackFrame.Category.PRODUCTION, getStackFrameCategory(allFrames, 5));
+		} finally {
+			terminateAndCleanUp(thread);
+		}
+	}
+
+	private Category getStackFrameCategory(TreeItem[] allFrames, int idx) {
+		return ((IJavaStackFrame) allFrames[idx].getData()).getCategory();
 	}
 
 	/**
