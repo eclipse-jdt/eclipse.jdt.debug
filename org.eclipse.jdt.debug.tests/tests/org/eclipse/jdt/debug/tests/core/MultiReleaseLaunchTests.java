4bfb13baa2a10569210089be0b36b967c9d14395
Christoph Läubrich <laeubi@laeubi-soft.de>
2025-08-06 08:09:30 +0200

    Move the search from the dialog to the DetectVMInstallationsJob
    and use that search method in the test to discover and dispose required
    jvms

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/MultiReleaseLaunchTests.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/core/MultiReleaseLaunchTests.java
@@ -13,13 +13,18 @@
  *******************************************************************************/
 package org.eclipse.jdt.debug.tests.core;
 
+import java.io.File;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.UUID;
 
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
@@ -27,23 +32,28 @@ import org.eclipse.debug.ui.DebugUITools;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.debug.core.IJavaDebugTarget;
 import org.eclipse.jdt.debug.tests.ui.AbstractDebugUiTests;
+import org.eclipse.jdt.internal.launching.DetectVMInstallationsJob;
 import org.eclipse.jdt.launching.IVMInstall;
 import org.eclipse.jdt.launching.IVMInstall2;
 import org.eclipse.jdt.launching.IVMInstallType;
 import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.VMStandin;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.TextConsole;
 
 /**
  * <b>IMPORTANT</b> This test requires some different JVM installs to be present (see {@link #JAVA_11}, {@link #JAVA_17}, {@link #JAVA_21})) if such
- * JVMs can not be found, the test will fail!
+ * JVMs can not be found, the test will fail! One can specify a basedir to search for such jvms with the {@link #JVM_SEARCH_BASE} system property.
  */
 public class MultiReleaseLaunchTests extends AbstractDebugUiTests {
 
+	private static final String JVM_SEARCH_BASE = "MultiReleaseLaunchTests.rootDir";
 	private static final RequiredJavaVersion JAVA_11 = new RequiredJavaVersion(11, 16);
 	private static final RequiredJavaVersion JAVA_17 = new RequiredJavaVersion(17, 20);
 	private static final RequiredJavaVersion JAVA_21 = new RequiredJavaVersion(21, Integer.MAX_VALUE);
+
+	private List<Runnable> disposeVms = new ArrayList<>();
 
 	public MultiReleaseLaunchTests(String name) {
 		super(name);
@@ -52,10 +62,36 @@ public class MultiReleaseLaunchTests extends AbstractDebugUiTests {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
+		final Set<File> existingLocations = new HashSet<>();
 		List<RequiredJavaVersion> requiredJavaVersions = new ArrayList<>(List.of(JAVA_11, JAVA_17, JAVA_21));
-		removeExistingJavaVersions(requiredJavaVersions);
+		removeExistingJavaVersions(requiredJavaVersions, existingLocations);
+		if (!requiredJavaVersions.isEmpty()) {
+			final File rootDir = new File(System.getProperty(JVM_SEARCH_BASE, "/opt/tools/java/openjdk/"));
+			final List<File> locations = new ArrayList<>();
+			final List<IVMInstallType> types = new ArrayList<>();
+			DetectVMInstallationsJob.search(rootDir, locations, types, existingLocations, new NullProgressMonitor());
+			for (int i = 0; i < locations.size(); i++) {
+				File location = locations.get(i);
+				IVMInstallType type = types.get(i);
+				String id = "MultiReleaseLaunchTests-" + UUID.randomUUID() + "-" + i;
+				VMStandin workingCopy = new VMStandin(type, id);
+				workingCopy.setInstallLocation(location);
+				IVMInstall install = workingCopy.convertToRealVM();
+				if (removeIfMatch(requiredJavaVersions, install)) {
+					disposeVms.add(() -> type.disposeVMInstall(id));
+				} else {
+					type.disposeVMInstall(id);
+				}
+			}
+		}
 		assertTrue("The following java versions are required by this test but can not be found: "
 				+ requiredJavaVersions, requiredJavaVersions.isEmpty());
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		disposeVms.forEach(Runnable::run);
 	}
 
 	@Override
@@ -110,7 +146,7 @@ public class MultiReleaseLaunchTests extends AbstractDebugUiTests {
 		return -1;
 	}
 
-	private static void removeExistingJavaVersions(Collection<RequiredJavaVersion> requiredJavaVersions) {
+	private static void removeExistingJavaVersions(Collection<RequiredJavaVersion> requiredJavaVersions, Set<File> existingLocations) {
 		IVMInstallType[] installTypes = JavaRuntime.getVMInstallTypes();
 		for (IVMInstallType installType : installTypes) {
 			IVMInstall[] vmInstalls = installType.getVMInstalls();
@@ -118,15 +154,21 @@ public class MultiReleaseLaunchTests extends AbstractDebugUiTests {
 				if (requiredJavaVersions.isEmpty()) {
 					return;
 				}
-				int javaVersion = getJavaVersion(install);
-				for (Iterator<RequiredJavaVersion> iterator = requiredJavaVersions.iterator(); iterator.hasNext();) {
-					if (iterator.next().matches(javaVersion)) {
-						iterator.remove();
-						break;
-					}
-				}
+				existingLocations.add(install.getInstallLocation());
+				removeIfMatch(requiredJavaVersions, install);
 			}
 		}
+	}
+
+	protected static boolean removeIfMatch(Collection<RequiredJavaVersion> requiredJavaVersions, IVMInstall install) {
+		int javaVersion = getJavaVersion(install);
+		for (Iterator<RequiredJavaVersion> iterator = requiredJavaVersions.iterator(); iterator.hasNext();) {
+			if (iterator.next().matches(javaVersion)) {
+				iterator.remove();
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private static record RequiredJavaVersion(int from, int to) {
