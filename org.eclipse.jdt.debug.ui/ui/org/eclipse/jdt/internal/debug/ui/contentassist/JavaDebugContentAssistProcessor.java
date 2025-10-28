068962bef59b5f7fd753924cacc720ae411077f1
Andrey Loskutov <loskutov@gmx.de>
2025-10-28 11:35:24 +0100

    Remove @SuppressWarnings annotation no longer raised by ecj, take 2
    Fixes remaining `@SuppressWarnings("deprecation")` annotations flagged
    as unnecessary by
    https://github.com/eclipse-jdt/eclipse.jdt.core/pull/4564
    
    See https://github.com/eclipse-jdt/eclipse.jdt.core/issues/4553

--- org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/contentassist/JavaDebugContentAssistProcessor.java
+++ org.eclipse.jdt.debug.ui/ui/org/eclipse/jdt/internal/debug/ui/contentassist/JavaDebugContentAssistProcessor.java
@@ -56,8 +56,6 @@ public class JavaDebugContentAssistProcessor implements IContentAssistProcessor 
 	private final IJavaDebugContentAssistContext fContext;
 	private ContentAssistant fAssistant;
 
-
-	@SuppressWarnings("deprecation")
 	public JavaDebugContentAssistProcessor(IJavaDebugContentAssistContext context) {
 		fContext = context;
 		TemplateContextType contextType= JavaPlugin.getDefault().getTemplateContextRegistry().getContextType(JavaContextType.ID_ALL);
