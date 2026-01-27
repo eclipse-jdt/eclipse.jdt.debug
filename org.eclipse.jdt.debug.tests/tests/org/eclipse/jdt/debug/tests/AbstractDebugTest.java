3ccc1e82f89e57591c0d3564348ea06e0756bdf6
sougandhs <sougandh.s@ibm.com>
2026-01-27 06:02:57 +0530

    Fix Stale Captured Variable on lambda re-entry
    This commit fixes an issue where the debugger showed stale values for
    captured variables when the same lambda was entered multiple
    times from different calls.
    
    Fixes : https://github.com/eclipse-jdt/eclipse.jdt.debug/issues/795

--- org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
+++ org.eclipse.jdt.debug.tests/tests/org/eclipse/jdt/debug/tests/AbstractDebugTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2025 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -523,6 +523,7 @@ public abstract class AbstractDebugTest extends TestCase implements  IEvaluation
 				cfgs.add(createLaunchConfiguration(jp, "Bug578145LambdaOnChainCalls"));
 				cfgs.add(createLaunchConfiguration(jp, "LambdaBreakpoints1"));
 				cfgs.add(createLaunchConfiguration(jp, "GH275"));
+				cfgs.add(createLaunchConfiguration(jp, "LambdaTest"));
 	    		loaded18 = true;
 	    		waitForBuild();
 	        }
