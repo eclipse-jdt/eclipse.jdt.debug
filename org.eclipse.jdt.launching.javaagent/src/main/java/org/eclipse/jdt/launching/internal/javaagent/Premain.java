b7e07545c1409ef6ea10d8768de40aaaf9a5a446
Sougandh S <sougandh.s@ibm.com>
2026-09-07 14:17:14 +0530

    Replace ASM with JVM Classfile API in Advanced Source Lookup agent for Java 25+
    Add javaagent25 jar for Class-File API based advanced source lookup. ASM
    must be updated for each new class file version. Debuggees on Java 25+
    can use the JDK's built-in java.lang.classfile API instead, which always
    matches its own JDK's class format. The existing ASM-based javaagent jar
    is unchanged and still used for debuggees below Java 25

--- org.eclipse.jdt.launching.javaagent/src/main/java/org/eclipse/jdt/launching/internal/javaagent/Premain.java
+++ org.eclipse.jdt.launching.javaagent/src/main/java/org/eclipse/jdt/launching/internal/javaagent/Premain.java
@@ -14,7 +14,6 @@
  *******************************************************************************/
 package org.eclipse.jdt.launching.internal.javaagent;
 
-import java.io.InputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
@@ -22,23 +21,13 @@ import java.net.URL;
 import java.security.CodeSource;
 import java.security.ProtectionDomain;
 
-import org.eclipse.jdt.launching.internal.weaving.ClassfileTransformer;
+import org.eclipse.jdt.launching.internal.weaving.ClassfileTransformer2;
 
 public class Premain {
-	private static final ClassfileTransformer transformer = new ClassfileTransformer();
+	private static final ClassfileTransformer2 transformer = new ClassfileTransformer2();
 
 	public static void premain(final String agentArgs, final Instrumentation inst) {
 		final boolean debuglog = "debuglog".equals(agentArgs); //$NON-NLS-1$
-
-		// disable instrumentation if Object.class class format is not supported
-		short major = readJavaLangObjectMajor(debuglog);
-		if (major < 0 || major > ClassfileTransformer.MAX_CLASS_MAJOR) {
-			String vendor = System.getProperty("java.vendor"); //$NON-NLS-1$
-			String version = System.getProperty("java.version"); //$NON-NLS-1$
-			System.err.printf("JRE %s/%s is not supported, advanced source lookup disabled.\n Eclipse debugger will use less precise source lookup implementation for this debug session, but everything else will continue to work otherwise.\n" //$NON-NLS-1$
-						+ "Upgrading Eclipse to the latest version will likely make this warning go away.", vendor, version); //$NON-NLS-1$
-			return;
-		}
 
 		inst.addTransformer(new ClassFileTransformer() {
 			@Override
@@ -77,37 +66,6 @@ public class Premain {
 		});
 
 		printErrorMessage("Advanced source lookup enabled.", null, debuglog);//$NON-NLS-1$
-	}
-
-	private static short readJavaLangObjectMajor(boolean debuglog) {
-		// https://docs.oracle.com/javase/specs/jvms/se10/html/jvms-4.html
-		// We need class major_version, i.e. the u2 field starting at offset 6
-
-		final int offset = 6;
-
-		try (InputStream is = ClassLoader.getSystemResourceAsStream("java/lang/Object.class")) {//$NON-NLS-1$ )
-			if (is == null) {
-				printErrorMessage("Could not open java/lang/Object.class system resource stream.", null, debuglog);//$NON-NLS-1$
-				return -1;
-			}
-			byte[] bytes = new byte[offset + 2];
-			if (is.read(bytes) < bytes.length) {
-				printErrorMessage("Could not read java/lang/Object.class system resource stream.", null, debuglog);//$NON-NLS-1$
-				return -1;
-			}
-
-			int magic = ((bytes[0] & 0xFF) << 24) | ((bytes[1] & 0xFF) << 16) | ((bytes[2] & 0xFF) << 8) | (bytes[3] & 0xFF);
-			if (magic != 0xCAFEBABE) {
-				printErrorMessage("Invalid java/lang/Object.class magic.", null, debuglog);//$NON-NLS-1$
-				return -1;
-			}
-
-			return (short) (((bytes[offset] & 0xFF) << 8) | (bytes[offset + 1] & 0xFF));
-		}
-		catch (Exception ex) {
-			printErrorMessage("Could not open java/lang/Object.class system resource stream.", ex, debuglog);//$NON-NLS-1$
-			return -1;
-		}
 	}
 
 	private static void printErrorMessage(String errorMessage, Exception ex, boolean debuglog) {
